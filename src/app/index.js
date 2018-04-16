060586c24300e647a3593e6535a35473fc418d91
Richard Westenra <rjwestenra@gmail.com>
2018-04-16 17:37:12 +0100

    Reorder directories and update default settings etc

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
