6b167deb4a3a8d6d93f79fe41f74aff907bdf03a
Richard Westenra <rjwestenra@gmail.com>
2018-04-17 13:04:17 +0100

    Remove unnecessary markup/styles from app component

--- src/app/index.js
+++ src/app/index.js
@@ -5,12 +5,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
