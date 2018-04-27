84446ce84e7be90f56c11e3f7549a06c90c696e4
Richard Westenra <rjwestenra@gmail.com>
2018-04-27 13:55:09 +0100

    Add new dagre-d3 chart

--- src/flowchart/index.js
+++ src/flowchart/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { select } from 'd3-selection';
-import { linkVertical, stack } from 'd3-shape';
+import { select, event } from 'd3-selection';
+import { curveBasis, linkVertical } from 'd3-shape';
 import { scaleLinear, scaleOrdinal, scalePoint } from 'd3-scale';
+import { zoom } from 'd3-zoom';
+import DagreD3 from 'dagre-d3';
 import './flowchart.css';
 
 const getArray = n => Array.from(Array(n).keys());
@@ -16,7 +18,12 @@ class FlowChart extends Component {
     this.svg = select(this._svg);
     this.setChartHeight();
     window.addEventListener('resize', this.setChartHeight.bind(this));
-    this.makeChart();
+    this.generateRandomData();
+    this.calculatePaths();
+    this.setScales();
+    // this.populateDataTables();
+    // this.makeChart();
+    this.makeChart2();
   }
 
   setChartHeight() {
@@ -44,7 +51,7 @@ class FlowChart extends Component {
     const links = nodes.map((d, i) => {
       const source = d;
       const targets = nodes.filter(
-        dd => dd.name !== source.name && dd.layer >= source.layer
+        dd => dd.name !== source.name && dd.layer > source.layer
       );
       if (targets.length) {
         return {
@@ -58,7 +65,7 @@ class FlowChart extends Component {
       };
     });
 
-    return {
+    this.data = {
       layers,
       nodes,
       links
@@ -68,9 +75,9 @@ class FlowChart extends Component {
   /**
    * Experiment
    */
-  calculatePaths(data) {
+  calculatePaths() {
     // Convert each link into a path
-    const paths = data.links.map(link => [link.source, link.target]);
+    const paths = this.data.links.map(link => [link.source, link.target]);
 
     // Calculate all the possible paths through the flowchart,
     // and make each one a path array of nodes inside the paths array
@@ -89,17 +96,17 @@ class FlowChart extends Component {
     }
 
     // Get the length of the longest path for each layer
-    data.yMax = 0;
-    data.layers.forEach(layer => {
+    let yMax = 0;
+    this.data.layers.forEach(layer => {
       // Get the y index for each node, corresponding to its relevant layer
       let layerY = 0;
       paths.forEach((path, i) => {
         let pathY = 0;
         path.forEach(node => {
           if (node.layer === layer.id) {
-            const nodeLevel = pathY + data.yMax;
+            const nodeLevel = pathY + yMax;
             if (typeof node.level === 'undefined' || nodeLevel > node.level) {
-              node.level = pathY + data.yMax;
+              node.level = pathY + yMax;
             }
             pathY++;
             if (pathY > layerY) {
@@ -109,135 +116,193 @@ class FlowChart extends Component {
         });
       });
       layer.length = layerY;
-      data.yMax += layerY;
+      yMax += layerY;
     });
 
     // Calculate start/end of each layer band, for stack viz
-    data.layers.reduce((a, b) => {
+    this.data.layers.reduce((a, b) => {
       b.y0 = a;
       b.y1 = a + b.length;
       return b.y1;
     }, 0);
 
-    data.paths = paths;
-
-    return data;
+    this.data.paths = paths;
+    this.data.yMax = yMax;
   }
 
-  setScales(data) {
-    const scale = {
+  setScales() {
+    this.scale = {
       x: scalePoint()
-        .domain(data.nodes.map(d => d.name))
+        .domain(this.data.nodes.map(d => d.name))
         .range([this.width * 0.2, this.width * 0.8]),
 
       y: scaleLinear()
-        .domain([0, data.yMax])
+        .domain([0, this.data.yMax])
         .range([0, this.height]),
 
       link: linkVertical()
-        .x(d => scale.x(d.name))
-        .y(d => scale.y(d.level + 0.5)),
+        .x(d => this.scale.x(d.name))
+        .y(d => this.scale.y(d.level + 0.5)),
 
       colour: scaleOrdinal()
-        .domain(data.layers.map(d => d.id))
+        .domain(this.data.layers.map(d => d.id))
         .range(
-          data.layers.map(
-            (d, i) => `hsl(${i * (360 / data.layers.length)}, 50%, 70%)`
+          this.data.layers.map(
+            (d, i) => `hsl(${i * (360 / this.data.layers.length)}, 50%, 70%)`
           )
         )
     };
-
-    return scale;
   }
 
   makeChart() {
-    // Generate data
-    const data = this.generateRandomData();
-    this.calculatePaths(data);
-    const scale = this.setScales(data);
-
-    // Add objects to the DOM
-
-    // TODO remove this
-    select('#pathlist')
-      .selectAll('tr')
-      .data(data.paths)
-      .enter()
-      .append('tr')
-      .selectAll('td')
-      .data(d => d)
-      .enter()
-      .append('td')
-      .style('background', d => scale.colour(d.layer))
-      .text(d => d.name || '');
-
-    select('#nodelist')
-      .selectAll('li')
-      .data(data.nodes.sort((a, b) => a.level - b.level))
-      .enter()
-      .append('li')
-      .style('background', d => scale.colour(d.layer))
-      .text(d => `${d.name} - ${d.level}` || '');
-
-    const layerRect = this.svg
+    this.svg
       .append('g')
       .attr('class', 'layers')
       .selectAll('rect')
-      .data(data.layers)
+      .data(this.data.layers)
       .enter()
       .append('rect')
-      .attr('fill', d => scale.colour(d.id))
+      .attr('fill', d => this.scale.colour(d.id))
       .attr('fill-opacity', 0.3)
       .attr('x', 0)
       .attr('width', this.width)
-      .attr('height', d => scale.y(d.length))
-      .attr('y', d => scale.y(d.y0));
+      .attr('height', d => this.scale.y(d.length))
+      .attr('y', d => this.scale.y(d.y0));
 
-    const link = this.svg
+    this.svg
       .append('g')
       .attr('class', 'links')
       .selectAll('path')
-      .data(data.links)
+      .data(this.data.links)
       .enter()
       .append('path')
       .attr('fill', 'none')
       .attr('stroke-width', 2)
       .attr('stroke', 'rgba(0,0,0,0.15)')
-      .attr('d', scale.link);
+      .attr('d', this.scale.link);
 
-    const node = this.svg
+    this.svg
       .append('g')
       .attr('class', 'nodes')
       .selectAll('circle')
-      .data(data.nodes)
+      .data(this.data.nodes)
       .enter()
       .append('circle')
       .attr('r', 15)
-      .attr('fill', d => scale.colour(d.layer))
+      .attr('fill', d => this.scale.colour(d.layer))
       .attr('title', d => d.name)
-      .attr('cx', d => scale.x(d.name))
-      .attr('cy', d => scale.y(d.level + 0.5));
+      .attr('cx', d => this.scale.x(d.name))
+      .attr('cy', d => this.scale.y(d.level + 0.5));
 
-    const text = this.svg
+    this.svg
       .append('g')
       .attr('class', 'text')
       .selectAll('text')
-      .data(data.nodes)
+      .data(this.data.nodes)
       .enter()
       .append('text')
       .attr('fill', '#fff')
       .attr('text-anchor', 'middle')
       .attr('dy', '5')
       .text(d => d.name)
-      .attr('x', d => scale.x(d.name))
-      .attr('y', d => scale.y(d.level + 0.5));
+      .attr('x', d => this.scale.x(d.name))
+      .attr('y', d => this.scale.y(d.level + 0.5));
+  }
+
+  makeChart2() {
+    // Create the input graph
+    this.graph = new DagreD3.graphlib.Graph({ compound: true })
+      .setGraph({})
+      .setDefaultEdgeLabel(() => ({}));
+
+    // this.data.layers.forEach(d => {
+    //   g.setNode(d.id, {
+    //     label: d.name,
+    //     clusterLabelPos: 'top',
+    //     style: `fill: ${this.scale.colour(d.id)}`
+    //   });
+    // });
+
+    this.data.nodes.forEach(d => {
+      this.graph.setNode(d.name, {
+        label: () => {
+          var icon = document.createElement('img');
+          icon.setAttribute('src', '/database.svg');
+          icon.setAttribute('width', 25);
+          icon.setAttribute('height', 25);
+          icon.setAttribute('transform', 'translateY(4px)');
+          icon.setAttribute('alt', d.name);
+          return icon;
+        },
+        shape: 'circle',
+        style: `fill: ${this.scale.colour(d.layer)}`
+      });
+      // this.graph.setParent(d.name, d.layer);
+    });
+
+    this.data.links.forEach(d => {
+      this.graph.setEdge(d.source.name, d.target.name, {
+        arrowHead: 'vee',
+        curve: curveBasis
+      });
+    });
+
+    // Create the renderer
+    const render = new DagreD3.render();
+
+    // Set up an SVG group so that we can translate the final graph.
+    const svg = select(this._svg);
+    const zoomGroup = svg.append('g');
+    const graphGroup = zoomGroup.append('g');
+
+    this.graph.graph().transition = selection =>
+      selection.transition().duration(500);
+
+    // Run the renderer. This is what draws the final graph.
+    render(graphGroup, this.graph);
+
+    // Center the graph
+    const offset = {
+      x: (this.width - this.graph.graph().width) / 2,
+      y: (this.height - this.graph.graph().height) / 2
+    };
+    graphGroup.attr('transform', `translate(${offset.x}, ${offset.y})`);
+
+    // Initialize zoom behaviour
+    const zoomBehaviour = zoom().on('zoom', () => {
+      zoomGroup.attr('transform', event.transform);
+    });
+    svg.call(zoomBehaviour);
+  }
+
+  populateDataTables() {
+    // TODO remove this
+    select('#pathlist')
+      .selectAll('tr')
+      .data(this.data.paths)
+      .enter()
+      .append('tr')
+      .selectAll('td')
+      .data(d => d)
+      .enter()
+      .append('td')
+      .style('background', d => this.scale.colour(d.layer))
+      .text(d => d.name || '');
+
+    select('#nodelist')
+      .selectAll('li')
+      .data(this.data.nodes.sort((a, b) => a.level - b.level))
+      .enter()
+      .append('li')
+      .style('background', d => this.scale.colour(d.layer))
+      .text(d => `${d.name} - ${d.level}` || '');
   }
 
   render() {
     return (
       <div className="FlowChart">
-        <table id="pathlist" />
-        <ul id="nodelist" />
+        {/* <table id="pathlist" />
+        <ul id="nodelist" /> */}
         <svg ref={el => (this._svg = el)} width="960" height="600" />
       </div>
     );
