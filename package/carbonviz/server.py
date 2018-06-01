d3cec681eddb0c66e427dbca50ddf30bfb21a7c6
Ivan Danov <idanov@users.noreply.github.com>
2018-06-01 15:38:44 +0100

    Read real log files (#3)
    * Copy html files to package/carbonviz/html
    
    * Read pipeline.log file format
    
    * Add a new line at the end of .gitignore
    
    * Take out pipeline.log into a top-level constant

--- package/carbonviz/server.py
+++ package/carbonviz/server.py
@@ -1,15 +1,16 @@
 import getopt
 import json
 import sys
-from os.path import dirname, join, abspath
+from os.path import dirname, join, abspath, exists
 
 from flask import Flask, jsonify
 
 port = 4141
 logdir = abspath("logs")
+FNAME = 'pipeline.log'
 
 app = Flask(__name__,
-            static_folder=join(abspath(dirname(__file__)), 'build'),
+            static_folder=join(abspath(dirname(__file__)), 'html'),
             static_url_path='')
 
 
@@ -20,8 +21,18 @@ def root():
 
 @app.route('/logs/nodes.json')
 def nodes():
-    data = json.load(open(join(logdir, 'nodes.json'), 'r'))
-    return jsonify(data)
+    with open(join(logdir, FNAME), "r") as f:
+        last = f.readline()
+        for last in f:
+            pass
+
+    if last:
+        log = json.loads(last)
+        log['message'] = json.loads(log['message'])
+        pipeline = log['message']['pipeline']
+        return jsonify(pipeline)
+    else:
+        return jsonify([])
 
 
 def main():
@@ -37,13 +48,17 @@ def main():
             print('{} --logdir <log-directory> --port <web-server-port>'
                   .format(sys.argv[0]))
             sys.exit()
-        elif opt == "--logdir":
-            logdir = abspath(arg)
-        elif opt == "--port" and arg.isdigit():
-            port = int(arg)
+        elif opt == "--logdir" and not exists(join(abspath(arg), FNAME)):
+            print('--logdir should point to a folder with a {} file'
+                  .format(FNAME))
+            sys.exit(2)
         elif opt == "--port" and not arg.isdigit():
             print('--port should be an integer')
             sys.exit(2)
+        elif opt == "--port":
+            port = int(arg)
+        elif opt == "--logdir":
+            logdir = abspath(arg)
 
     app.run(port=port)
 
