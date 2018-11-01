bd524a7e19dc9b8f9516c54fbd5d5a3ddb0774f7
Richard Westenra <rjwestenra@gmail.com>
2018-11-01 19:19:43 +0000

    Use a React onClick handler instead of document.onClick for closing nav

--- src/components/chart-wrapper/index.js
+++ src/components/chart-wrapper/index.js
@@ -14,25 +14,16 @@ class ChartWrappper extends Component {
 
     // Pre-bind these methods to prevent the 'removeEventListener and bind(this) gotcha'
     // (See https://gist.github.com/Restuta/e400a555ba24daa396cc)
-    this.handleDocumentClick = this.handleDocumentClick.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.closeNav = this.closeNav.bind(this);
   }
 
   componentWillMount() {
-    document.addEventListener('click', this.handleDocumentClick, false);
     document.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('click', this.handleDocumentClick, false);
     document.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleDocumentClick(e) {
-    if (this.state.visibleNav && !this.nav.contains(e.target)) {
-      this.closeNav();
-    }
   }
 
   handleKeyDown(e) {
@@ -81,7 +72,9 @@ class ChartWrappper extends Component {
           </button>
           <ChartUI {...this.props} />
         </nav>
-        { children }
+        <div onClick={this.closeNav}>
+          { children }
+        </div>
       </div>
     );
   }
