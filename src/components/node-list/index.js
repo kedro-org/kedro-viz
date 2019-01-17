62bd00f4a1576152ea5214993591106161bcfcf5
Richard Westenra <rjwestenra@gmail.com>
2019-01-17 17:49:46 +0000

    Add toggle all nodes buttons

--- src/components/node-list/index.js
+++ src/components/node-list/index.js
@@ -107,6 +107,33 @@ class NodeList extends React.Component {
           style={{ width: 'auto' }}
           autoHide
           hideTracksWhenNotNeeded>
+          <div className='carbon'>
+            <h2 className='pipeline-node-list__toggle-title'>Nodes</h2>
+            <div className='pipeline-node-list__toggle-container'>
+              <button
+                onClick={this.toggleAllNodes.bind(this, false)}
+                className='pipeline-node-list__toggle'>
+                <svg
+                  className='pipeline-node-list__icon pipeline-node-list__icon--check'
+                  width='24'
+                  height='24'>
+                  <polygon points='9.923 14.362 7.385 11.944 6 13.263 7.33384369 14.5336026 9.923 17 18 9.32 16.615 8' />
+                </svg>
+                Check all
+              </button>
+              <button
+                onClick={this.toggleAllNodes.bind(this, true)}
+                className='pipeline-node-list__toggle'>
+                <svg
+                  className='pipeline-node-list__icon pipeline-node-list__icon--uncheck'
+                  width='24'
+                  height='24'>
+                  <path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z' />
+                </svg>
+                Uncheck all
+              </button>
+            </div>
+          </div>
           <ul className='pipeline-node-list'>
             { formattedNodes.map(node => (
               <li
