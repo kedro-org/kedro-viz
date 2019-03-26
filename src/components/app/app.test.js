a7533a5dd7f60a1af981372831d3664440640caa
Richard Westenra <rjwestenra@gmail.com>
2019-03-26 16:43:20 +0000

    Install Enzyme and use shallow for App smoketest

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <App
-      allowHistoryDeletion={true}
-      allowUploads={true}
-      data="random"
-      showHistory={true}
-    />,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('renders without crashing', () => {
+    shallow(<App data="random" />);
+  });
 });
