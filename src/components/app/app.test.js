5abf37aae15c20e2bc09f9975560461df7605503
Richard Westenra <rjwestenra@gmail.com>
2019-03-26 19:36:08 +0000

    Add unit tests for App component

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './index';
+import { fakeData } from '../../utils/test-data';
 
 describe('App', () => {
-  it('renders without crashing', () => {
-    shallow(<App data="random" />);
+  describe('renders without crashing', () => {
+    it('when loading random data', () => {
+      shallow(<App data="random" />);
+    });
+
+    it('when loading json data', () => {
+      shallow(<App data="json" />);
+    });
+
+    it('when being passed data as a prop', () => {
+      shallow(<App data={fakeData} />);
+    });
+  });
+
+  describe('updates the store', () => {
+    const getSnapshotIDs = wrapper =>
+      wrapper.instance().store.getState().snapshotIDs;
+
+    it('when data prop is set on first load', () => {
+      const wrapper = shallow(<App data={fakeData} />);
+      expect(getSnapshotIDs(wrapper)).toHaveLength(1);
+    });
+
+    it('when data prop is updated', () => {
+      const wrapper = shallow(<App data={fakeData} />);
+      const newFakeData = fakeData.concat(fakeData[0]);
+      wrapper.setProps({ data: newFakeData });
+      expect(getSnapshotIDs(wrapper)).toHaveLength(2);
+    });
+  });
+
+  describe('throws an error', () => {
+    it('when data prop is empty', () => {
+      expect(() => shallow(<App />)).toThrow();
+    });
   });
 });
---
+++ src/utils/test-data.js
@@ -0,0 +1,14 @@
+export const fakeData = [
+  {
+    created_ts: '1551452832000',
+    kernel_ai_schema_id: '310750827599783',
+    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    json_schema: [
+      {
+        inputs: ['Lorem', 'ipsum', 'dolor', 'sit', 'amet'],
+        name: 'consectetur',
+        outputs: ['Aliquam', 'eu', 'accumsan', 'mauris']
+      }
+    ]
+  }
+];
