ab0342ec28d8d53146d4af9a7110b644870c1e2d
Richard Westenra <rjwestenra@gmail.com>
2019-03-28 14:53:15 +0000

    Improve formatTime tests

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -13,14 +13,30 @@ import {
 describe('utils', () => {
   describe('formatTime', () => {
     const timestamp = 1553708618164;
-    const expectedTime = 'Wed Mar 27 2019 5:43:38 PM';
+    const date = new Date(timestamp);
+
+    it('returns a properly-formatted date from a numerical timestamp', () => {
+      expect(formatTime(timestamp)).toEqual(
+        expect.stringContaining(date.toDateString())
+      );
+    });
 
     it('returns a properly-formatted time from a numerical timestamp', () => {
-      expect(formatTime(timestamp)).toEqual(expectedTime);
+      expect(formatTime(timestamp)).toEqual(
+        expect.stringContaining(date.toLocaleTimeString())
+      );
+    });
+
+    it('returns a properly-formatted date from a stringified timestamp', () => {
+      expect(formatTime(String(timestamp))).toEqual(
+        expect.stringContaining(date.toDateString())
+      );
     });
 
     it('returns a properly-formatted time from a stringified timestamp', () => {
-      expect(formatTime(timestamp + '')).toEqual(expectedTime);
+      expect(formatTime(String(timestamp))).toEqual(
+        expect.stringContaining(date.toLocaleTimeString())
+      );
     });
   });
 });
