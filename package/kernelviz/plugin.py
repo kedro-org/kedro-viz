2d33473c8b66488a968a710c47665cacc8e23f2e
Gordon <gordon.wrigley@quantumblack.com>
2019-04-10 14:23:36 +0100

    plugin

---
+++ package/kernelviz/plugin.py
@@ -0,0 +1,22 @@
+import click
+from kedro.cli.utils import python_call, forward_command
+from kedro.cli import get_project_context
+import tempfile
+import json
+from pathlib import Path
+
+
+@click.group(name="KernelViz")
+def commands():
+    pass
+
+
+@forward_command(commands, forward_help=True)
+def viz(args):
+    """Visualize the pipeline using kernelviz"""
+    pipeline = get_project_context("create_pipeline")()
+    data = json.dumps({"message": pipeline.to_json()})
+
+    with tempfile.TemporaryDirectory() as directory:
+        (Path(directory) / "pipeline.log").write_text(data, "utf-8")
+        python_call("kernelviz", ["--logdir", directory] + list(args))
--- package/setup.py
+++ package/setup.py
@@ -3,38 +3,37 @@ from os import path
 
 from setuptools import setup, find_packages
 
-name = 'kernelviz'
+name = "kernelviz"
 here = path.abspath(path.dirname(__file__))
 
 import os
 
-jsbuild = [os.path.join(dirpath, f)
-           for dirpath, dirnames, files in os.walk('kernelviz/html/')
-           for f in files]
+jsbuild = [
+    os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk("kernelviz/html/")
+    for f in files
+]
 
 files = map(lambda x: x.replace("kernelviz/", "", 1), jsbuild)
 
-with open(path.join(here, path.pardir, 'package.json')) as data:
+with open(path.join(here, path.pardir, "package.json")) as data:
     obj = json.load(data)
-    version = obj['version']
+    version = obj["version"]
 
 setup(
     name=name,
     version=version,
-    description='KernelViz is the visualisation tool used by KernelAI',
-    url='https://github.com/quantumblack/asset-kernel-viz',
+    description="KernelViz is the visualisation tool used by KernelAI",
+    url="https://github.com/quantumblack/asset-kernel-viz",
     packages=find_packages(),
-    package_data={
-        'kernelviz': list(files)
-    },
-    install_requires=[
-        'Flask>=1.0, <2.0'
-    ],
-    setup_requires=['pytest-runner==4.2'],
-    tests_require=['pytest==4.2.0', 'pytest-cov==2.6.1'],
-    author='QuantumBlack',
-    author_email='feedback@quantumblack.com',
+    package_data={"kernelviz": list(files)},
+    install_requires=["Flask>=1.0, <2.0"],
+    setup_requires=["pytest-runner==4.2"],
+    tests_require=["pytest==4.2.0", "pytest-cov==2.6.1"],
+    author="QuantumBlack",
+    author_email="feedback@quantumblack.com",
     entry_points={
-        'console_scripts': ['kernelviz = kernelviz.server:main']
-    }
+        "console_scripts": ["kernelviz = kernelviz.server:main"],
+        "kedro.project_commands": ["kernelviz = kernelviz.plugin:commands"],
+    },
 )
