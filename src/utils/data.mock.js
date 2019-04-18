ed48fe3da7b4ecb00944ff017059cf429951e5df
Richard Westenra <rjwestenra@gmail.com>
2019-04-18 10:31:30 +0100

    Add reusable setup functions to data mock file

--- src/utils/data.mock.js
+++ src/utils/data.mock.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { mount, shallow } from 'enzyme';
 import store from '../store';
 import { getInitialState } from '../components/app/load-data';
 import formatSnapshots from './format-data';
 
-// Example data for use in tests
+/**
+ * Example data for use in tests
+ */
 export const mockData = [
   {
     created_ts: '1551452832000',
@@ -44,7 +47,9 @@ export const mockData = [
   }
 ];
 
-// Example state object for use in tests of redux-enabled components
+/**
+ * Example state object for use in tests of redux-enabled components
+ */
 export const mockState = getInitialState(formatSnapshots(mockData), {
   allowHistoryDeletion: true,
   allowUploads: true,
@@ -52,8 +57,33 @@ export const mockState = getInitialState(formatSnapshots(mockData), {
   showHistory: true
 });
 
+// Redux store based on mock data
 export const mockStore = store(mockState);
 
-export const MockProvider = ({ children }) => (
-  <Provider store={mockStore}>{children}</Provider>
+/**
+ * React-Redux Provider wrapper for testing connected components
+ * @param {Object} children A React component
+ * @param {Object} state Redux state object for creating the store
+ */
+export const MockProvider = ({ children, state = mockState }) => (
+  <Provider store={store(state)}>{children}</Provider>
 );
+
+/**
+ * Set up mounted/shallow Enzyme wrappers
+ */
+export const setup = {
+  /**
+   * Mount a React-Redux Provider wrapper for testing connected components
+   * @param {Object} children React component(s)
+   * @param {Object} state Redux state object for creating the store
+   */
+  mount: (children, state) =>
+    mount(<MockProvider state={state}>{children}</MockProvider>),
+  /**
+   * Render a pure React component in a shallow wrapper
+   * @param {Object} Component A React component
+   * @param {Object} props React component props
+   */
+  shallow: (Component, props = {}) => shallow(<Component {...props} />)
+};
