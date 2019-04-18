ba1dd97fe37aaaaecd01a72e8037117c7480ada2
Richard Westenra <rjwestenra@gmail.com>
2019-04-18 12:01:37 +0100

    Improve tests for Description component

--- src/components/description/description.test.js
+++ src/components/description/description.test.js
@@ -1,36 +1,47 @@
-import React from 'react';
-import { mount } from 'enzyme';
 import { Description, mapStateToProps } from './index';
-import { mockState } from '../../utils/data.mock';
+import { mockState, setup } from '../../utils/data.mock';
 import {
   getActiveSnapshotMessage,
   getActiveSnapshotTimestamp
 } from '../../selectors';
 
-function setup() {
-  const props = {
-    message: getActiveSnapshotMessage(mockState),
-    showDescription: true,
-    timestamp: getActiveSnapshotTimestamp(mockState),
-    visibleNav: true
-  };
-
-  const wrapper = mount(<Description {...props} />);
-
-  return {
-    props,
-    wrapper
-  };
-}
+const props = {
+  message: getActiveSnapshotMessage(mockState),
+  showDescription: true,
+  timestamp: getActiveSnapshotTimestamp(mockState),
+  visibleNav: true
+};
 
 describe('Description', () => {
   it('renders without crashing', () => {
-    const { wrapper } = setup();
+    const wrapper = setup.shallow(Description, props);
+    expect(wrapper.find('.snapshot-description').length).toBe(1);
+  });
+
+  it('adds a menu-visible class when menu is visible', () => {
+    const wrapper = setup.shallow(Description, props);
     expect(
       wrapper
         .find('.snapshot-description')
         .hasClass('snapshot-description--menu-visible')
     ).toBe(true);
+  });
+
+  it('removes menu-visible class when menu is hidden', () => {
+    const wrapper = setup.shallow(
+      Description,
+      Object.assign({}, props, { visibleNav: false })
+    );
+    expect(
+      wrapper
+        .find('.snapshot-description')
+        .hasClass('snapshot-description--menu-visible')
+    ).toBe(false);
+  });
+
+  it('returns null when showDescription is false', () => {
+    const wrapper = setup.shallow(Description, { showDescription: false });
+    expect(wrapper.html()).toBe(null);
   });
 
   it('maps state to props', () => {
