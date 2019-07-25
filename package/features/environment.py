d8fc49fac2977ff78b2935b8733140201ff47eac
Kiyo <921kiyo@users.noreply.github.com>
2019-07-25 10:58:46 +0100

    Update inline comment about pinning pip

--- package/features/environment.py
+++ package/features/environment.py
@@ -81,7 +81,7 @@ def before_all(context):
     # install this plugin by resolving the requirements using pip-compile
     # from pip-tools due to this bug in pip: https://github.com/pypa/pip/issues/988
     #
-    # pip<19.2 can be unpinned when 19.3 is released.
+    # pip<19.2 can be unpinned when pip-tools>3.9.0 is released.
     call([context.python, "-m", "pip", "install", "-U", "pip<19.2", "pip-tools"])
     pip_compile = str(bin_dir / "pip-compile")
     with tempfile.TemporaryDirectory() as tmpdirname:
