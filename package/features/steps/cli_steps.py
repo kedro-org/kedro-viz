29c9085776d25b4419b13e7beb48cc3407b808d0
Kiyo <921kiyo@users.noreply.github.com>
2019-07-26 16:45:30 +0100

    Undo breakline change

--- package/features/steps/cli_steps.py
+++ package/features/steps/cli_steps.py
@@ -85,6 +85,7 @@ def exec_viz_command(context, command):
 @then("kedro-viz should start successfully")
 def check_kedroviz_up(context):
     """Check that kedro-viz is up and responding to requests"""
+
     wait_for(
         _check_service_up, expected_result=None, print_error=False, context=context
     )
@@ -98,6 +99,7 @@ def _check_service_up(context):
         context (behave.runner.Context): Test context
     """
     data_json = json.loads(download_url("http://localhost:4141/api/nodes.json"))
+
     try:
         assert context.result.poll() is None
         assert data_json["snapshots"][0]["nodes"][0]["name"] == "predict"
