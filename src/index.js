7cfa18f449b2e63ff770b2224e44f8a7eb14648c
Richard Westenra <rjwestenra@gmail.com>
2019-08-30 10:31:09 +0100

    Add Konami code easter egg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import EasterEgg from './components/easter-egg';
 import config from './config';
 import './styles/index.css';
 
@@ -8,10 +9,13 @@ const { dataSource } = config();
 const showHistory = dataSource === 'random' || dataSource === 'mock';
 
 ReactDOM.render(
-  <App
-    allowHistoryDeletion={showHistory}
-    data={dataSource}
-    showHistory={showHistory}
-  />,
+  <>
+    <App
+      allowHistoryDeletion={showHistory}
+      data={dataSource}
+      showHistory={showHistory}
+    />
+    <EasterEgg />
+  </>,
   document.getElementById('root')
 );
