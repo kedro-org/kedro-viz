7cc0b2ef674cc30e740b9e747ea5ca04ac87246a
Richard Westenra <rjwestenra@gmail.com>
2019-09-07 18:03:34 +0100

    Refactor data formatter

--- src/utils/format-data.js
+++ src/utils/format-data.js
@@ -15,109 +15,99 @@ const validateInput = data => {
  * @param {Object} source - Name and type of the source node
  * @param {Object} target - Name and type of the target node
  */
-export const getEdgeID = (source, target) => [source, target].join('|');
+const getEdgeID = (source, target) => [source, target].join('|');
+
+/**
+ * Generate a new default pipeline state instance
+ * @return {Object} Initial state
+ */
+const createPipelineState = () => ({
+  id: null,
+  // Nodes
+  nodes: [],
+  nodeID: {},
+  nodeName: {},
+  nodeFullName: {},
+  nodeType: {},
+  nodeIsParam: {},
+  nodeTags: {},
+  nodeActive: {},
+  nodeDisabled: {},
+  nodeFocused: null,
+  // Edges
+  edges: [],
+  edgeSources: {},
+  edgeTargets: {},
+  edgeActive: {},
+  edgeDisabled: {},
+  // Tags
+  tags: [],
+  tagName: {},
+  tagActive: {},
+  tagEnabled: {}
+});
+
+/**
+ * Add a new node if it doesn't already exist
+ * @param {string} name - Default node name
+ * @param {string} type - 'data' or 'task'
+ * @param {Array} tags - List of associated tags
+ */
+const addNode = state => node => {
+  const { id } = node;
+  if (state.nodeName[id]) {
+    return;
+  }
+  state.nodes.push(id);
+  state.nodeID[id] = id;
+  state.nodeName[id] = node.name;
+  state.nodeFullName[id] = node.full_name;
+  state.nodeType[id] = node.type;
+  state.nodeIsParam[id] = Boolean(node.is_parameters);
+  state.nodeTags[id] = node.tags || [];
+};
+
+/**
+ * Create a new link between two nodes and add it to the edges array
+ * @param {Object} source - Parent node
+ * @param {Object} target - Child node
+ */
+const addEdge = state => ({ source, target }) => {
+  const id = getEdgeID(source, target);
+  if (state.edges.includes(id)) {
+    return;
+  }
+  state.edges.push(id);
+  state.edgeSources[id] = source;
+  state.edgeTargets[id] = target;
+};
+
+/**
+ * Add a new Tag if it doesn't already exist
+ * @param {string} name - Default node name
+ */
+const addTag = state => tag => {
+  const { id } = tag;
+  state.tags.push(id);
+  state.tagName[id] = tag.name;
+};
 
 /**
  * Format the full list of snapshot data
- * @param {Array} data
- * @eturn {Object}
+ * @param {Object} data Raw unformatted data input
+ * @return {Object} Formatted state
  */
 const formatData = data => {
-  if (!validateInput(data) && !Array.isArray(data.snapshots)) {
-    return {};
+  const state = createPipelineState();
+
+  if (validateInput(data)) {
+    state.id = data.schema_id;
+    data.nodes.forEach(addNode(state));
+    data.edges.forEach(addEdge(state));
+    data.tags.forEach(addTag(state));
   }
 
-  // Nodes
-  const nodes = [];
-  const nodeID = {};
-  const nodeName = {};
-  const nodeFullName = {};
-  const nodeType = {};
-  const nodeIsParam = {};
-  const nodeTags = {};
-  // Edges
-  const edges = [];
-  const edgeSources = {};
-  const edgeTargets = {};
-  // Tags
-  const tags = [];
-  const tagName = {};
-
-  /**
-   * Add a new node if it doesn't already exist
-   * @param {string} name - Default node name
-   * @param {string} type - 'data' or 'task'
-   * @param {Array} tags - List of associated tags
-   */
-  const addNode = node => {
-    const { id } = node;
-    if (nodeName[id]) {
-      return;
-    }
-    nodes.push(id);
-    nodeID[id] = id;
-    nodeName[id] = node.name;
-    nodeFullName[id] = node.full_name;
-    nodeType[id] = node.type;
-    nodeIsParam[id] = Boolean(node.is_parameters);
-    nodeTags[id] = node.tags || [];
-  };
-
-  /**
-   * Create a new link between two nodes and add it to the edges array
-   * @param {Object} source - Parent node
-   * @param {Object} target - Child node
-   */
-  const addEdge = ({ source, target }) => {
-    const id = getEdgeID(source, target);
-    if (edges.includes(id)) {
-      return;
-    }
-    edges.push(id);
-    edgeSources[id] = source;
-    edgeTargets[id] = target;
-  };
-
-  /**
-   * Add a new Tag if it doesn't already exist
-   * @param {string} name - Default node name
-   */
-  const addTag = tag => {
-    const { id } = tag;
-    tags.push(id);
-    tagName[id] = tag.name;
-  };
-
-  const rawData = data.snapshots ? data.snapshots[0] : data;
-
-  if (validateInput(rawData)) {
-    // Begin formatting
-    rawData.nodes.forEach(addNode);
-    rawData.edges.forEach(addEdge);
-    rawData.tags.forEach(addTag);
-  }
-
-  return {
-    id: rawData ? rawData.schema_id : '',
-    nodes,
-    nodeName,
-    nodeFullName,
-    nodeActive: {},
-    nodeDisabled: {},
-    nodeFocused: null,
-    nodeType,
-    nodeIsParam,
-    nodeTags,
-    edges,
-    edgeActive: {},
-    edgeSources,
-    edgeTargets,
-    edgeDisabled: {},
-    tags,
-    tagName,
-    tagActive: {},
-    tagEnabled: {}
-  };
+  return state;
 };
 
 export default formatData;
