5c50dd885dc56950305666a04a3494e5cd7d5452
Richard Westenra <rjwestenra@gmail.com>
2019-09-23 14:02:39 +0100

    Improve naming

--- src/selectors/layout.js
+++ src/selectors/layout.js
@@ -104,12 +104,12 @@ export const getGraph = createSelector(
 export const getLayout = createSelector(
   [getGraph, getNodeType, getNodeActive],
   (graph, nodeType, nodeActive) => ({
-    nodes: graph.nodes().map(node => {
-      const graphNode = graph.node(node);
-      return Object.assign({}, graphNode, {
-        type: nodeType[node],
-        order: graphNode.x + graphNode.y * 9999,
-        active: nodeActive[node]
+    nodes: graph.nodes().map(nodeID => {
+      const node = graph.node(nodeID);
+      return Object.assign({}, node, {
+        type: nodeType[nodeID],
+        order: node.x + node.y * 9999,
+        active: nodeActive[nodeID]
       });
     }),
     edges: graph.edges().map(edge => graph.edge(edge))
