bb5d702e33964d1ddf9a7366d083c0755024599a
Richard Westenra <rjwestenra@gmail.com>
2020-02-17 13:50:51 +0000

    Move pipeline state init to initial-state.js

--- src/store/initial-state.js
+++ src/store/initial-state.js
@@ -38,15 +38,54 @@ export const getPipelineData = data => {
 };
 
 /**
+ * Generate a new default pipeline state instance
+ * @return {Object} Initial state
+ */
+export const getInitialPipelineState = () => ({
+  id: null,
+  // Nodes
+  nodes: [],
+  nodeName: {},
+  nodeFullName: {},
+  nodeType: {},
+  nodeIsParam: {},
+  nodeTags: {},
+  nodeDisabled: {},
+  nodeClicked: null,
+  nodeHovered: null,
+  // Edges
+  edges: [],
+  edgeSources: {},
+  edgeTargets: {},
+  edgeActive: {},
+  edgeDisabled: {},
+  // Tags
+  tags: [],
+  tagName: {},
+  tagActive: {},
+  tagEnabled: {},
+  // Node types
+  types: ['task', 'data', 'parameters'],
+  typeName: {
+    data: 'Datasets',
+    task: 'Nodes',
+    parameters: 'Parameters'
+  },
+  typeDisabled: {}
+});
+
+/**
  * Configure the redux store's initial state
- * @param {Object}   pipelineData Formatted pipeline data
- * @param {Object}   props App component props
+ * @param {Object} props App component props
+ * @return {Object} Initial state
  */
 const getInitialState = (props = {}) => {
   const pipelineData = normalizeData(getPipelineData(props.data));
 
   // Load properties from localStorage if defined, else use defaults
-  const { textLabels = true, theme = 'dark', typeDisabled = {} } = loadState();
+  const localStorageState = loadState();
+  const { textLabels = true, typeDisabled = {} } = localStorageState;
+  const theme = props.theme || localStorageState.theme || 'dark';
 
   const visible = Object.assign(
     { exportBtn: true, labelBtn: true, themeBtn: true },
@@ -59,7 +98,7 @@ const getInitialState = (props = {}) => {
     fontLoaded: false,
     textLabels,
     visible,
-    theme: props.theme || theme,
+    theme,
     typeDisabled
   };
 };
