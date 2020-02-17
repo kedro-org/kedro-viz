c312fbc094cfd8a116b7c6a8af662804e0d837d2
Richard Westenra <rjwestenra@gmail.com>
2020-02-17 12:05:26 +0000

    Add JSDoc comment for configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,11 @@ import getInitialState from './initial-state';
 import loadData from './load-data';
 import { saveState } from './helpers';
 
+/**
+ * Configure initial state and create the Redux store
+ * @param {Object} props Initial React props passed to App component
+ * @return {Object} Redux store
+ */
 export default function configureStore(props) {
   /**
    * Dispatch an action to update the store with new pipeline data
