kedro-org/kedro-viz

commit 4d6f05c130766bbfab72038e741a064db04077fc

[KED-1450] Move visibleNav/navWidth calculations into redux store (#124)
The visibleNav calculation affects the flowchart width and is currently a class method used in multiple places, which is getting difficult to maintain. It's handled in this way because the boolean flag is stored in the Wrapper component state, so it can only be passed down as a prop. Moving this prop to the redux store and using selectors to manage the navWidth calculations will save time and make things more maintainable.

src/selectors/layout.js
@@ -4,7 +4,7 @@ import { getNodeActive, getVisibleNodes } from './nodes';
 import { getVisibleEdges } from './edges';
 
 const getNodeType = state => state.node.type;
-const getChartSize = state => state.chartSize;
+const getVisibleSidebar = state => state.visible.sidebar;
 
 /**
  * Calculate chart layout with Dagre.js.
@@ -69,34 +69,63 @@ export const getGraphSize = createSelector(
 );
 
 /**
+ * Return the displayed width of the sidebar
+ */
+export const getSidebarWidth = (visibleSidebar, outerChartWidth) => {
+  const defaultSidebarWidth = 300; // from _variables.scss
+  const breakpointSmall = 480; // from _variables.scss
+  if (visibleSidebar && outerChartWidth > breakpointSmall) {
+    return defaultSidebarWidth;
+  }
+  return 0;
+};
+
+/**
+ * Convert the DOMRect into an Object, mutate some of the properties,
+ * and add some useful new ones
+ */
+export const getChartSize = createSelector(
+  [getVisibleSidebar, state => state.chartSize],
+  (visibleSidebar, chartSize) => {
+    const { left, top, width, height } = chartSize;
+    if (!width || !height) {
+      return {};
+    }
+    const sidebarWidth = getSidebarWidth(visibleSidebar, width);
+    return {
+      left,
+      top,
+      outerWidth: width,
+      outerHeight: height,
+      width: width - sidebarWidth,
+      height,
+      sidebarWidth
+    };
+  }
+);
+
+/**
  * Get chart zoom translation/scale,
  * by comparing native graph width/height to container width/height
  */
 export const getZoomPosition = createSelector(
   [getGraphSize, getChartSize],
-  (graph, container) => {
-    const validDimensions = [
-      container.width,
-      container.height,
-      graph.width,
-      graph.height
-    ].every(n => !isNaN(n) && Number.isFinite(n));
-
-    if (validDimensions) {
-      const scale = Math.min(
-        container.width / graph.width,
-        container.height / graph.height
-      );
-      return {
-        scale,
-        translateX: container.width / 2 - (graph.width * scale) / 2,
-        translateY: container.height / 2 - (graph.height * scale) / 2
-      };
+  (graph, chart) => {
+    if (!Object.keys(chart).length) {
+      return {};
     }
+
+    const scale = Math.min(
+      chart.width / graph.width,
+      chart.height / graph.height
+    );
+    const translateX = chart.width / 2 - (graph.width * scale) / 2;
+    const translateY = chart.height / 2 - (graph.height * scale) / 2;
+
     return {
-      scale: 1,
-      translateX: 0,
-      translateY: 0
+      scale,
+      translateX: translateX + chart.sidebarWidth,
+      translateY
     };
   }
 );

src/selectors/layout.test.js
@@ -1,8 +1,11 @@
 import { mockState } from '../utils/state.mock';
 import {
+  getChartSize,
   getGraph,
+  getGraphSize,
   getLayoutNodes,
   getLayoutEdges,
+  getSidebarWidth,
   getZoomPosition
 } from './layout';
 import { getVisibleNodes } from './nodes';
@@ -13,6 +16,7 @@ import reducer from '../reducers';
 describe('Selectors', () => {
   describe('getGraph', () => {
     const graph = getGraph(mockState.lorem);
+
     it('calculates chart layout and returns a Dagre object', () => {
       expect(graph).toEqual(
         expect.objectContaining({
@@ -76,16 +80,81 @@ describe('Selectors', () => {
     });
   });
 
-  describe('getZoomPosition', () => {
-    it('returns a default chart zoom translation/scale if none is specified', () => {
-      expect(getZoomPosition(mockState.lorem)).toEqual({
-        scale: 1,
-        translateX: 0,
-        translateY: 0
+  describe('getGraphSize', () => {
+    it('returns width, height and margin of the graph', () => {
+      const graphSize = getGraphSize(mockState.lorem);
+      expect(graphSize).toEqual(
+        expect.objectContaining({
+          height: expect.any(Number),
+          marginx: expect.any(Number),
+          marginy: expect.any(Number),
+          width: expect.any(Number)
+        })
+      );
+    });
+  });
+
+  describe('getSidebarWidth', () => {
+    describe('if sidebar is visible', () => {
+      it('reduces the chart width by 300 on wider screens', () => {
+        expect(getSidebarWidth(true, 1000)).toEqual(300);
+        expect(getSidebarWidth(true, 500)).toEqual(300);
+      });
+
+      it('sets sidebar width to zero on mobile', () => {
+        expect(getSidebarWidth(true, 480)).toEqual(0);
+        expect(getSidebarWidth(true, 320)).toEqual(0);
       });
     });
 
-    it('returns the updated chart zoom translation/scale if set', () => {
+    describe('if sidebar is hidden', () => {
+      it('sets sidebar width to zero on desktop', () => {
+        expect(getSidebarWidth(false, 1000)).toEqual(0);
+      });
+
+      it('sets sidebar width to zero on mobile', () => {
+        expect(getSidebarWidth(false, 480)).toEqual(0);
+        expect(getSidebarWidth(false, 320)).toEqual(0);
+      });
+    });
+  });
+
+  describe('getChartSize', () => {
+    it('returns a set of undefined properties if chartSize DOMRect is not supplied', () => {
+      expect(getChartSize(mockState.lorem)).toEqual({
+        height: undefined,
+        left: undefined,
+        outerHeight: undefined,
+        outerWidth: undefined,
+        sidebarWidth: undefined,
+        top: undefined,
+        width: undefined
+      });
+    });
+
+    it('returns a DOMRect converted into an Object, with some extra properties', () => {
+      const newMockState = {
+        ...mockState.lorem,
+        chartSize: { left: 100, top: 100, width: 1000, height: 1000 }
+      };
+      expect(getChartSize(newMockState)).toEqual({
+        height: expect.any(Number),
+        left: expect.any(Number),
+        outerHeight: expect.any(Number),
+        outerWidth: expect.any(Number),
+        sidebarWidth: expect.any(Number),
+        top: expect.any(Number),
+        width: expect.any(Number)
+      });
+    });
+  });
+
+  describe('getZoomPosition', () => {
+    it('returns an empty object if chartSize is unset', () => {
+      expect(getZoomPosition(mockState.lorem)).toEqual({});
+    });
+
+    it('returns the updated chart zoom translation/scale if chartSize is set', () => {
       const newMockState = reducer(
         mockState.lorem,
         updateChartSize({ width: 100, height: 100 })

src/actions/index.js
@@ -39,6 +39,19 @@ export function toggleTagFilter(tagID, enabled) {
   };
 }
 
+export const TOGGLE_SIDEBAR = 'TOGGLE_SIDEBAR';
+
+/**
+ * Toggle sidebar visible/hidden
+ * @param {boolean} visible Whether sidebar nav is shown
+ */
+export function toggleSidebar(visible) {
+  return {
+    type: TOGGLE_SIDEBAR,
+    visible
+  };
+}
+
 export const TOGGLE_THEME = 'TOGGLE_THEME';
 
 /**

src/components/flowchart/flowchart.test.js
@@ -59,55 +59,16 @@ describe('FlowChart', () => {
     expect(spy2).not.toHaveBeenCalled();
   });
 
-  describe('getNavOffset', () => {
-    describe('if nav is visible', () => {
-      it('reduces the chart width by 300 on wider screens', () => {
-        const wrapper = setup.mount(<FlowChart visibleNav={true} />);
-        const instance = wrapper.find('FlowChart').instance();
-        expect(instance.getNavOffset(1000)).toEqual(300);
-        expect(instance.getNavOffset(500)).toEqual(300);
-      });
-
-      it('sets nav offset to zero on mobile', () => {
-        const instance = setup
-          .mount(<FlowChart visibleNav={true} />)
-          .find('FlowChart')
-          .instance();
-        expect(instance.getNavOffset(480)).toEqual(0);
-        expect(instance.getNavOffset(320)).toEqual(0);
-      });
-    });
-
-    describe('if nav is hidden', () => {
-      const instance = setup
-        .mount(<FlowChart visibleNav={false} />)
-        .find('FlowChart')
-        .instance();
-
-      it('sets nav offset to zero on desktop', () => {
-        expect(instance.getNavOffset(1000)).toEqual(0);
-      });
-
-      it('sets nav offset to zero on mobile', () => {
-        expect(instance.getNavOffset(480)).toEqual(0);
-        expect(instance.getNavOffset(320)).toEqual(0);
-      });
-    });
-  });
-
   it('maps state to props', () => {
     const expectedResult = {
+      centralNode: null,
       chartSize: expect.any(Object),
       edges: expect.any(Array),
-      nodes: expect.any(Array),
       linkedNodes: expect.any(Object),
-      centralNode: null,
+      nodes: expect.any(Array),
       textLabels: expect.any(Boolean),
-      zoom: expect.objectContaining({
-        scale: expect.any(Number),
-        translateX: expect.any(Number),
-        translateY: expect.any(Number)
-      })
+      visibleSidebar: expect.any(Boolean),
+      zoom: expect.any(Object)
     };
     expect(mapStateToProps(mockState.lorem)).toEqual(expectedResult);
   });

src/components/flowchart/index.js
@@ -7,6 +7,7 @@ import { zoom, zoomIdentity } from 'd3-zoom';
 import { updateChartSize } from '../../actions';
 import { toggleNodeClicked, toggleNodeHovered } from '../../actions/nodes';
 import {
+  getChartSize,
   getLayoutNodes,
   getLayoutEdges,
   getZoomPosition
@@ -53,7 +54,7 @@ export class FlowChart extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    if (prevProps.visibleNav !== this.props.visibleNav) {
+    if (prevProps.visibleSidebar !== this.props.visibleSidebar) {
       this.updateChartSize();
     }
     if (prevProps.zoom !== this.props.zoom) {
@@ -79,31 +80,9 @@ export class FlowChart extends Component {
    * and apply them to the chart SVG
    */
   updateChartSize() {
-    const {
-      left,
-      top,
-      width,
-      height
-    } = this.containerRef.current.getBoundingClientRect();
-    const navOffset = this.getNavOffset(width);
-    this.props.onUpdateChartSize({
-      x: left,
-      y: top,
-      outerWidth: width,
-      outerHeight: height,
-      width: width - navOffset,
-      height,
-      navOffset
-    });
-  }
-
-  getNavOffset(width) {
-    const navWidth = 300; // from _variables.scss
-    const breakpointSmall = 480; // from _variables.scss
-    if (this.props.visibleNav && width > breakpointSmall) {
-      return navWidth;
-    }
-    return 0;
+    this.props.onUpdateChartSize(
+      this.containerRef.current.getBoundingClientRect()
+    );
   }
 
   /**
@@ -154,15 +133,13 @@ export class FlowChart extends Component {
    * Zoom and scale to fit
    */
   zoomChart() {
-    const { chartSize, zoom } = this.props;
-    const { scale, translateX, translateY } = zoom;
-    const navOffset = this.getNavOffset(chartSize.outerWidth);
+    const { scale = 1, translateX = 0, translateY = 0 } = this.props.zoom;
     this.el.svg
       .transition()
       .duration(this.DURATION)
       .call(
         this.zoomBehaviour.transform,
-        zoomIdentity.translate(translateX + navOffset, translateY).scale(scale)
+        zoomIdentity.translate(translateX, translateY).scale(scale)
       );
   }
 
@@ -228,19 +205,16 @@ export class FlowChart extends Component {
    * @param {Object} node A node datum
    */
   showTooltip(node) {
-    const { chartSize } = this.props;
+    const { left, top, width, outerWidth, sidebarWidth } = this.props.chartSize;
     const eventOffset = event.target.getBoundingClientRect();
-    const navOffset = this.getNavOffset(chartSize.outerWidth);
-    const isRight = eventOffset.left - navOffset > chartSize.width / 2;
-    const xOffset = isRight
-      ? eventOffset.left - (chartSize.width + navOffset)
-      : eventOffset.left;
+    const isRight = eventOffset.left - sidebarWidth > width / 2;
+    const xOffset = isRight ? eventOffset.left - outerWidth : eventOffset.left;
     this.setState({
       tooltipVisible: true,
       tooltipIsRight: isRight,
       tooltipText: node.fullName,
-      tooltipX: xOffset - chartSize.x + eventOffset.width / 2,
-      tooltipY: eventOffset.top - chartSize.y
+      tooltipX: xOffset - left + eventOffset.width / 2,
+      tooltipY: eventOffset.top - top
     });
   }
 
@@ -317,12 +291,12 @@ export class FlowChart extends Component {
 
 export const mapStateToProps = state => ({
   centralNode: getCentralNode(state),
-  chartSize: state.chartSize,
+  chartSize: getChartSize(state),
   edges: getLayoutEdges(state),
   linkedNodes: getLinkedNodes(state),
   nodes: getLayoutNodes(state),
   textLabels: state.textLabels,
-  view: state.view,
+  visibleSidebar: state.visible.sidebar,
   zoom: getZoomPosition(state)
 });
 

src/components/sidebar/index.js
@@ -1,57 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import classnames from 'classnames';
-import Icon from '@quantumblack/kedro-ui/lib/components/icon';
+import { toggleSidebar } from '../../actions';
 import TagList from '../tag-list';
 import NodeList from '../node-list';
-import MenuIcon from '../icons/menu';
+import { ShowMenuButton, HideMenuButton } from './menu-buttons';
 import './sidebar.css';
-
-/**
- * Hamburger menu button
- * @param {Function} props.onToggle Show menu on click
- * @param {Boolean} props.visible Whether nav is visible
- */
-export const ShowMenuButton = ({ onToggle, visible }) => (
-  <button
-    aria-label="Show menu"
-    className={classnames(
-      'pipeline-sidebar__show-menu pipeline-sidebar__icon-button',
-      {
-        'pipeline-sidebar__icon-button--visible': visible
-      }
-    )}
-    onClick={onToggle}>
-    <MenuIcon className="pipeline-icon" />
-  </button>
-);
-
-/**
- * ⨉-shaped button to close the menu. Hidden when menu is closed.
- * @param {Function} props.onToggle Show menu on click
- * @param {string} props.theme Kedro-UI theme: 'light' or 'dark'
- * @param {Boolean} props.visible Whether nav is visible
- */
-export const HideMenuButton = ({ onToggle, theme, visible }) => (
-  <button
-    aria-label="Hide menu"
-    className={classnames(
-      'pipeline-sidebar__hide-menu pipeline-sidebar__icon-button',
-      {
-        'pipeline-sidebar__icon-button--visible': visible
-      }
-    )}
-    onClick={onToggle}>
-    <Icon type="close" title="Close" theme={theme} />
-  </button>
-);
 
 /**
  * Main app container. Handles showing/hiding the sidebar nav, and theme classes.
  * @param {Object} props onToggle, theme, and visible
  */
-const Sidebar = props => (
+export const Sidebar = props => (
   <>
-    <ShowMenuButton onToggle={props.onToggle} visible={!props.visible} />
+    <ShowMenuButton {...props} />
     <nav
       className={classnames('pipeline-sidebar', {
         'pipeline-sidebar--visible': props.visible
@@ -65,4 +27,18 @@ const Sidebar = props => (
   </>
 );
 
-export default Sidebar;
+const mapStateToProps = state => ({
+  theme: state.theme,
+  visible: state.visible.sidebar
+});
+
+const mapDispatchToProps = dispatch => ({
+  onToggle: visible => {
+    dispatch(toggleSidebar(visible));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Sidebar);

src/components/sidebar/menu-buttons.js
@@ -1,0 +1,47 @@
+import React from 'react';
+import classnames from 'classnames';
+import Icon from '@quantumblack/kedro-ui/lib/components/icon';
+import MenuIcon from '../icons/menu';
+
+/**
+ * Hamburger menu button
+ * @param {Function} props.onToggle Show menu on click
+ * @param {Boolean} props.sidebarVisible Whether the sidebar is visible
+ */
+export const ShowMenuButton = ({ onToggle, visible: sidebarVisible }) => (
+  <button
+    aria-label="Show menu"
+    className={classnames(
+      'pipeline-sidebar__show-menu pipeline-sidebar__icon-button',
+      {
+        'pipeline-sidebar__icon-button--visible': !sidebarVisible
+      }
+    )}
+    onClick={() => onToggle(true)}>
+    <MenuIcon className="pipeline-icon" />
+  </button>
+);
+
+/**
+ * ⨉-shaped button to close the menu. Hidden when menu is closed.
+ * @param {Function} props.onToggle Show menu on click
+ * @param {string} props.theme Kedro-UI theme: 'light' or 'dark'
+ * @param {Boolean} props.sidebarVisible Whether the sidebar is visible
+ */
+export const HideMenuButton = ({
+  onToggle,
+  theme,
+  visible: sidebarVisible
+}) => (
+  <button
+    aria-label="Hide menu"
+    className={classnames(
+      'pipeline-sidebar__hide-menu pipeline-sidebar__icon-button',
+      {
+        'pipeline-sidebar__icon-button--visible': sidebarVisible
+      }
+    )}
+    onClick={() => onToggle(false)}>
+    <Icon type="close" title="Close" theme={theme} />
+  </button>
+);

src/components/sidebar/sidebar.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Sidebar, { ShowMenuButton, HideMenuButton } from './index';
+import MountSidebar, { Sidebar } from './index';
+import { ShowMenuButton, HideMenuButton } from './menu-buttons';
 import { mockState, setup } from '../../utils/state.mock';
 
 const mockProps = {
@@ -7,6 +8,37 @@ const mockProps = {
   onToggle: () => {},
   visible: true
 };
+
+describe('Sidebar', () => {
+  it('renders without crashing', () => {
+    const wrapper = setup.shallow(Sidebar, mockProps);
+    const container = wrapper.find('.pipeline-sidebar');
+    expect(container.length).toBe(1);
+  });
+
+  it('is open by default', () => {
+    const sidebar = setup.shallow(Sidebar, mockProps).find('.pipeline-sidebar');
+    expect(sidebar.hasClass('pipeline-sidebar--visible')).toBe(true);
+  });
+
+  it('hides when clicking the hide menu button', () => {
+    const wrapper = setup.mount(<MountSidebar />, {
+      visible: { sidebar: true }
+    });
+    wrapper.find('.pipeline-sidebar__hide-menu').simulate('click');
+    const sidebar = wrapper.find('.pipeline-sidebar');
+    expect(sidebar.hasClass('pipeline-sidebar--visible')).toBe(false);
+  });
+
+  it('shows when clicking the show menu button', () => {
+    const wrapper = setup.mount(<MountSidebar />, {
+      visible: { sidebar: false }
+    });
+    wrapper.find('.pipeline-sidebar__show-menu').simulate('click');
+    const sidebar = wrapper.find('.pipeline-sidebar');
+    expect(sidebar.hasClass('pipeline-sidebar--visible')).toBe(true);
+  });
+});
 
 describe('ShowMenuButton', () => {
   it('renders without crashing', () => {
@@ -21,20 +53,5 @@ describe('HideMenuButton', () => {
     const wrapper = setup.shallow(HideMenuButton, mockProps);
     const container = wrapper.find('button');
     expect(container.length).toBe(1);
-  });
-});
-
-describe('Sidebar', () => {
-  it('renders without crashing', () => {
-    const wrapper = setup.shallow(Sidebar, mockProps);
-    const container = wrapper.find('.pipeline-sidebar');
-    expect(container.length).toBe(1);
-  });
-
-  it('has an open sidebar by default', () => {
-    const wrapper = setup.shallow(Sidebar, mockProps);
-    expect(
-      wrapper.find('.pipeline-sidebar').hasClass('pipeline-sidebar--visible')
-    ).toBe(true);
   });
 });

commit 1ce21f280fb7be41bb76385211382eb14b750453

Prevent errors when no nodes are present
Fixes bug introduced in #124

src/selectors/layout.js
@@ -118,8 +118,16 @@ export const getChartSize = createSelector(
 export const getZoomPosition = createSelector(
   [getGraphSize, getChartSize],
   (graph, chart) => {
-    if (!Object.keys(chart).length) {
-      return {};
+    if (
+      !Object.keys(chart).length ||
+      !Number.isFinite(graph.width) ||
+      !Number.isFinite(graph.width)
+    ) {
+      return {
+        scale: 1,
+        translateX: 0,
+        translateY: 0
+      };
     }
 
     const scale = Math.min(

src/selectors/layout.test.js
@@ -11,6 +11,7 @@ import {
 import { getVisibleNodes } from './nodes';
 import { getVisibleEdges } from './edges';
 import { updateChartSize } from '../actions';
+import getInitialState from '../store/initial-state';
 import reducer from '../reducers';
 
 describe('Selectors', () => {
@@ -150,8 +151,22 @@ describe('Selectors', () => {
   });
 
   describe('getZoomPosition', () => {
-    it('returns an empty object if chartSize is unset', () => {
-      expect(getZoomPosition(mockState.lorem)).toEqual({});
+    const defaultZoom = {
+      scale: 1,
+      translateX: 0,
+      translateY: 0
+    };
+
+    it('returns default values if chartSize is unset', () => {
+      expect(getZoomPosition(mockState.lorem)).toEqual(defaultZoom);
+    });
+
+    it('returns default values if graph width/height is infinite (i.e. when no nodes are visible)', () => {
+      const newMockState = reducer(
+        getInitialState({ data: [] }),
+        updateChartSize({ width: 100, height: 100 })
+      );
+      expect(getZoomPosition(newMockState)).toEqual(defaultZoom);
     });
 
     it('returns the updated chart zoom translation/scale if chartSize is set', () => {