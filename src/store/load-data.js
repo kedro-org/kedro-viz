33e2e4e8b8c7659abdf90897f806b108288a255d
Richard Westenra <rjwestenra@gmail.com>
2020-04-27 15:38:01 +0100

    Make dataPath relative again
    Resolves KED-1564

--- src/store/load-data.js
+++ src/store/load-data.js
@@ -6,9 +6,11 @@ import config from '../config';
  */
 const loadJsonData = () => {
   const { dataPath } = config();
+  const fullPath = `/public${dataPath.substr(1)}`;
+
   return json(dataPath).catch(() => {
     throw new Error(
-      `Unable to load pipeline data from ${dataPath}. If you're running Kedro-Viz as a standalone (e.g. for JavaScript development), please check that you have placed a data file at /public${dataPath}.`
+      `Unable to load pipeline data from ${dataPath}. If you're running Kedro-Viz as a standalone (e.g. for JavaScript development), please check that you have placed a data file at ${fullPath}.`
     );
   });
 };
