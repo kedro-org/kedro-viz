95b09659f72af735eaef4184bbbf78711d5ab50b
Richard Westenra <rjwestenra@gmail.com>
2020-05-11 15:55:40 +0100

    [KED-1645] Add node-list-count to node-list group headings (#162)
    Add numerical counters to show the total number of nodes (and visible nodes) for each node-list group

--- src/selectors/node-types.js
+++ src/selectors/node-types.js
@@ -1,18 +1,42 @@
 import { createSelector } from 'reselect';
+import { getNodeDisabled } from './disabled';
+import { arrayToObject } from '../utils';
 
+const getNodeIDs = state => state.node.ids;
+const getNodeType = state => state.node.type;
 const getNodeTypeIDs = state => state.nodeType.ids;
 const getNodeTypeName = state => state.nodeType.name;
 const getNodeTypeDisabled = state => state.nodeType.disabled;
 
 /**
+ * Calculate the total number of nodes (and the number of visible nodes)
+ * for each node-type
+ */
+export const getTypeNodeCount = createSelector(
+  [getNodeTypeIDs, getNodeIDs, getNodeType, getNodeDisabled],
+  (types, nodeIDs, nodeType, nodeDisabled) =>
+    arrayToObject(types, type => {
+      const typeNodeIDs = nodeIDs.filter(nodeID => nodeType[nodeID] === type);
+      const visibleTypeNodeIDs = typeNodeIDs.filter(
+        nodeID => !nodeDisabled[nodeID]
+      );
+      return {
+        total: typeNodeIDs.length,
+        visible: visibleTypeNodeIDs.length
+      };
+    })
+);
+
+/**
  * Get formatted list of node type objects
  */
 export const getNodeTypes = createSelector(
-  [getNodeTypeIDs, getNodeTypeName, getNodeTypeDisabled],
-  (types, typeName, typeDisabled) =>
+  [getNodeTypeIDs, getNodeTypeName, getNodeTypeDisabled, getTypeNodeCount],
+  (types, typeName, typeDisabled, typeNodeCount) =>
     types.map(id => ({
       id,
       name: typeName[id],
-      disabled: Boolean(typeDisabled[id])
+      disabled: Boolean(typeDisabled[id]),
+      nodeCount: typeNodeCount[id]
     }))
 );
