35f389dbcb37c52da3c45f24af4f77b81f837895
Richard Westenra <rjwestenra@gmail.com>
2020-05-15 14:22:13 +0100

    Fix icon-toolbar tests

--- src/components/export-modal/export-modal.test.js
+++ src/components/export-modal/export-modal.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ExportModal, { mapStateToProps } from './index';
 import { mockState, setup } from '../../utils/state.mock';
 
-describe('IconToolbar', () => {
+describe('ExportModal', () => {
   it('renders without crashing', () => {
     const wrapper = setup.mount(<ExportModal />);
     expect(wrapper.find('.pipeline-icon-modal').length).toBe(1);
@@ -16,7 +16,16 @@ describe('IconToolbar', () => {
         marginx: expect.any(Number),
         marginy: expect.any(Number)
       }),
-      theme: expect.stringMatching(/light|dark/)
+      theme: expect.stringMatching(/light|dark/),
+      visible: expect.objectContaining({
+        exportBtn: expect.any(Boolean),
+        exportModal: expect.any(Boolean),
+        labelBtn: expect.any(Boolean),
+        layerBtn: expect.any(Boolean),
+        layers: expect.any(Boolean),
+        themeBtn: expect.any(Boolean),
+        sidebar: expect.any(Boolean)
+      })
     };
     expect(mapStateToProps(mockState.lorem)).toEqual(expectedResult);
   });
