41aa0adda0dae5173bdf5e5dd181121c87563206
Richard Westenra <rjwestenra@gmail.com>
2020-05-19 12:06:23 +0100

    Change wording on node-list-toggle buttons
    This is because they don't make as much sense anymore now that it's using eye icons, not checkboxes

--- src/components/node-list/node-list-toggle.js
+++ src/components/node-list/node-list-toggle.js
@@ -15,7 +15,7 @@ export const NodeListToggleAll = ({ onToggleNodesDisabled, nodeIDs }) => (
           height="24">
           <polygon points="9.923 14.362 7.385 11.944 6 13.263 7.33384369 14.5336026 9.923 17 18 9.32 16.615 8" />
         </svg>
-        Check all
+        Show all
       </button>
       <button
         onClick={() => onToggleNodesDisabled(nodeIDs, true)}
@@ -26,7 +26,7 @@ export const NodeListToggleAll = ({ onToggleNodesDisabled, nodeIDs }) => (
           height="24">
           <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
         </svg>
-        Uncheck all
+        Hide all
       </button>
     </div>
   </div>
