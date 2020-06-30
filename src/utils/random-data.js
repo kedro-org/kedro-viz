5839e79779647b96726be2aa9e4b4eb49f101b48
bru5 <45365769+bru5@users.noreply.github.com>
2020-06-30 12:09:28 +0100

    [KED-1650] Improve random generator variations (#187)
    * [KED-1650] Improve random generator variations
    
    * [KED-1650] Improve random generator variations
    
    * [KED-1650] Always use seeded random in generator
    
    * Improve random pipeline generator
    
    * Improve random pipeline generator
    
    * Improve random pipeline generator

--- src/utils/random-data.js
+++ src/utils/random-data.js
@@ -1,7 +1,10 @@
+import batchingToposort from 'batching-toposort';
+
 import { arrayToObject, unique } from './index';
 import {
   getNumberArray,
   random,
+  randomIndex,
   randomNumber,
   randomNumberBetween,
   getRandom,
@@ -11,15 +14,16 @@ import {
 
 //--- Config variables ---//
 
-const MIN_CONNECTED_NODES = 1;
-const MAX_CONNECTED_NODES = 3;
-const MAX_RANK_COUNT = 40;
-const MIN_RANK_COUNT = 5;
+const MAX_EDGE_COUNT = 200;
+const MIN_EDGE_COUNT = 100;
+const MAX_RANK_COUNT = 50;
+const MIN_RANK_COUNT = 10;
 const MAX_RANK_NODE_COUNT = 10;
 const MIN_RANK_NODE_COUNT = 1;
+const MIN_NODE_DEGREE = 2;
 const MAX_NODE_TAG_COUNT = 5;
 const MAX_TAG_COUNT = 20;
-const PARAMETERS_FREQUENCY = 0.05;
+const PARAMETERS_FREQUENCY = 0.2;
 const LAYERS = [
   'Raw',
   'Intermediate',
@@ -39,6 +43,9 @@ class Pipeline {
     this.tags = this.generateTags();
     this.nodes = this.generateNodes();
     this.edges = this.generateEdges();
+
+    this.update();
+    this.finalise();
   }
 
   /**
@@ -96,10 +103,8 @@ class Pipeline {
     const nodes = [];
     for (let rank = 0; rank < this.rankCount; rank++) {
       const rankNodeCount = this.getRankNodeCount(rank);
-      const type = this.getType(rank);
-
       for (let i = 0; i < rankNodeCount; i++) {
-        const node = this.createNode(i, rank, type);
+        const node = this.createNode(i, rank);
         nodes.push(node);
       }
     }
@@ -107,15 +112,11 @@ class Pipeline {
   }
 
   /**
-   * Randomly calculate the number of nodes in a rank
-   * @param {number} rank Rank number
+   * Return a random count of nodes for a rank
    * @returns {number} rank node count
    */
-  getRankNodeCount(rank) {
-    const max = MAX_RANK_NODE_COUNT;
-    const min = MIN_RANK_NODE_COUNT;
-    const p = (this.rankCount - rank) / this.rankCount;
-    return randomNumber(p * (max - min) + min);
+  getRankNodeCount() {
+    return Math.min(MIN_RANK_NODE_COUNT / random(), MAX_RANK_NODE_COUNT);
   }
 
   /**
@@ -123,33 +124,36 @@ class Pipeline {
    * @param {number} rank Rank number
    * @returns {string} Node type (task/data/parameters)
    */
-  getType(rank) {
-    if (rank % 2) {
+  getType(node) {
+    if (node.rank % 2) {
       return 'task';
     }
-    if (random() < PARAMETERS_FREQUENCY) {
+
+    if (node._sources.length === 0 && random() < PARAMETERS_FREQUENCY) {
       return 'parameters';
     }
+
     return 'data';
   }
 
   /**
-   * Create a node datum object
+   * Create a node datum object.
    * @param {number} i Node index within its rank
-   * @param {number} rank Rank index
-   * @param {number} type
+   * @param {number} initialRank Rank index
    * @returns {object} Node object
    */
-  createNode(i, rank, type) {
-    const name = this.getNodeName(type);
+  createNode(i, initialRank) {
+    const layer = this.rankLayers[initialRank];
     const node = {
-      id: `${type}/${name}(${rank}-${i})`,
-      name,
-      full_name: getRandomName(randomNumber(40)),
-      type,
-      rank,
-      layer: this.rankLayers[rank],
-      tags: this.getRandomTags()
+      id: `${layer}_${initialRank}_${i}`,
+      name: null,
+      full_name: null,
+      type: null,
+      rank: initialRank,
+      layer: layer,
+      tags: this.getRandomTags(),
+      _sources: [],
+      _targets: []
     };
     return node;
   }
@@ -173,76 +177,102 @@ class Pipeline {
   }
 
   /**
+   * Gets a map of ranks to lists of nodes at that rank
+   * @returns {array} List of nodes
+   */
+  getNodesByRank() {
+    const nodesByRank = {};
+
+    for (const node of this.nodes) {
+      nodesByRank[node.rank] = nodesByRank[node.rank] || [];
+      nodesByRank[node.rank].push(node);
+    }
+
+    return nodesByRank;
+  }
+
+  /**
    * Create list of edges
    * @returns {array} Edge objects
    */
   generateEdges() {
     const edges = [];
-    const dataNodes = this.nodes.filter(node => node.type === 'data');
-    const taskNodes = this.nodes.filter(node => node.type !== 'data');
-    const remainingEdgeCount = arrayToObject(
-      dataNodes.map(node => node.id),
-      () => MAX_CONNECTED_NODES
-    );
+    const edgeCount = randomNumberBetween(MIN_EDGE_COUNT, MAX_EDGE_COUNT);
+    const nodesByRank = this.getNodesByRank();
 
-    taskNodes.forEach(node => {
-      const ancestors = dataNodes.filter(
-        d => d.rank < node.rank && remainingEdgeCount[d.id]
-      );
-      this.getRandomNodes(ancestors).forEach(source => {
-        edges.push({
-          source: source.id,
-          target: node.id
-        });
-        remainingEdgeCount[source.id]--;
-      });
+    // Find the sorted list of node ranks
+    const ranks = Object.keys(nodesByRank)
+      .map(rank => parseFloat(rank))
+      .sort((a, b) => a - b);
 
-      const descendants = dataNodes.filter(
-        d => d.rank > node.rank && remainingEdgeCount[d.id]
-      );
-      this.getRandomNodes(descendants).forEach(target => {
-        edges.push({
-          source: node.id,
-          target: target.id
-        });
-        remainingEdgeCount[target.id]--;
-      });
-    });
+    // Gets a random node with the given rank index
+    const getRandomNodeAtRank = rankIndex => {
+      const rankValue = ranks[rankIndex];
+      const rankNodes = nodesByRank[rankValue];
+      const rankNodeIndex = randomIndex(rankNodes.length);
+      return rankNodes[rankNodeIndex];
+    };
+
+    // For the desired amount of edges
+    for (let i = 0; i < edgeCount; i += 1) {
+      // Choose a random source node excluding the last rank
+      const sourceRankIndex = randomIndex(ranks.length - 1);
+      const source = getRandomNodeAtRank(sourceRankIndex);
+
+      // Choose a random target node after the source rank prefering nearby
+      const remainingRankCount = ranks.length - 1 - sourceRankIndex;
+      const biasedRandom = Math.round(0.5 / random());
+      const targetRankIndex =
+        sourceRankIndex + Math.min(biasedRandom, remainingRankCount);
+      const target = getRandomNodeAtRank(targetRankIndex);
+
+      // Build the edge
+      const edge = {
+        source: source.id,
+        target: target.id,
+        _sourceNode: source,
+        _targetNode: target
+      };
+
+      edges.push(edge);
+
+      // Keep track of edges on nodes for later convenience
+      source._targets.push(edge);
+      target._sources.push(edge);
+    }
 
     return edges;
   }
 
   /**
-   * Get a random list of nodes to link to
-   * @param {array} nodes List of nodes from which to choose
-   * @returns {array} Randomly-selected nodes
-   */
-  getRandomNodes(nodes) {
-    const connections = randomNumberBetween(
-      MIN_CONNECTED_NODES,
-      MAX_CONNECTED_NODES
-    );
-    return getRandomSelection(nodes, connections);
-  }
-
-  /**
-   * Select only nodes that are connected to others
+   * Select only nodes with at least the minimum required connected nodes
    * @returns {object} Filtered nodes
    */
-  filterUnconnectedNodes() {
-    const findMatchingEdge = node => edge =>
-      node.id === edge.source || node.id === edge.target;
+  activeNodes() {
+    const nodes = {};
 
-    return this.nodes.filter(
-      node => this.edges.findIndex(findMatchingEdge(node)) >= 0
-    );
+    // Gets the total number of edges for the given node
+    const degree = node => node._sources.length + node._targets.length;
+
+    for (const edge of this.edges) {
+      // Keep both nodes if they have enough combined connections
+      if (
+        degree(edge._sourceNode) + degree(edge._targetNode) >
+        MIN_NODE_DEGREE
+      ) {
+        nodes[edge._sourceNode.id] = edge._sourceNode;
+        nodes[edge._targetNode.id] = edge._targetNode;
+      }
+    }
+
+    return Object.values(nodes);
   }
 
   /**
    * Select only used tags
    * @returns {object} Filtered tags
    */
-  filterUnusedTags() {
+  activeTags() {
     return this.nodes
       .reduce((tags, node) => (node.tags ? tags.concat(node.tags) : tags), [])
       .filter(unique)
@@ -250,19 +280,83 @@ class Pipeline {
   }
 
   /**
-   * Get a complete JSON schema
-   * @returns {object} Pipeline schema
+   * Select only used edges
+   * @returns {object} Filtered edges
    */
-  getSchema() {
+  activeEdges() {
+    const visibleNodes = arrayToObject(
+      this.nodes.map(node => node.id),
+      () => true
+    );
+
+    return this.edges.filter(
+      edge => visibleNodes[edge.target] && visibleNodes[edge.source]
+    );
+  }
+
+  /**
+   * Updates node properties including rank, type and name based on the current graph
+   */
+  update() {
+    const graph = {};
+
+    for (const node of this.nodes) {
+      graph[node.id] = [];
+    }
+
+    for (const edge of this.edges) {
+      graph[edge.source].push(edge.target);
+    }
+
+    // Use toposort to find actual ranks
+    const sortedNodes = batchingToposort(graph);
+
+    for (let rank = 0; rank < sortedNodes.length; rank++) {
+      for (const id of sortedNodes[rank]) {
+        const node = this.nodes.find(node => node.id === id);
+        node.rank = rank;
+        node.type = this.getType(node);
+        node.name = this.getNodeName(node.type);
+        node.full_name = `${node.layer}_${node.type}_${node.rank}_${
+          node.name
+        }`.replace(/\s/g, '_');
+      }
+    }
+  }
+
+  /**
+   * Removes unused elements and cleans up temporary properties
+   */
+  finalise() {
+    this.nodes = this.activeNodes();
+    this.tags = this.activeTags();
+    this.edges = this.activeEdges();
+
+    for (const node of this.nodes) {
+      delete node._sources;
+      delete node._targets;
+    }
+
+    for (const edge of this.edges) {
+      delete edge._targetNode;
+      delete edge._sourceNode;
+    }
+  }
+
+  /**
+   * Gets the complete pipeline data
+   * @returns {object} The pipeline data
+   */
+  all() {
     return {
       edges: this.edges,
-      layers: LAYERS,
-      nodes: this.filterUnconnectedNodes(),
-      tags: this.filterUnusedTags()
+      nodes: this.nodes,
+      tags: this.tags,
+      layers: LAYERS
     };
   }
 }
 
-const generateRandomPipeline = () => new Pipeline().getSchema();
+const generateRandomPipeline = () => new Pipeline().all();
 
 export default generateRandomPipeline;
