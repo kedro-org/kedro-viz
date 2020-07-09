dc79e5264ea89da6b084c0191d8e52e4f6239a41
Kiyo <921kiyo@users.noreply.github.com>
2020-07-09 16:03:10 +0100

    Remove ungrouped-imports

--- package/kedro_viz/server.py
+++ package/kedro_viz/server.py
@@ -25,7 +25,6 @@
 #
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# pylint: disable=ungrouped-imports
 """ Kedro-Viz plugin and webserver """
 
 import hashlib
