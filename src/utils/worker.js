7781f2d68a20bc12fb24ed19ec1dc641ea7740a8
Richard Westenra <rjwestenra@gmail.com>
2020-07-21 11:28:34 +0100

    Mock the worker for Jest testing purposes

--- src/utils/worker.js
+++ src/utils/worker.js
@@ -1,9 +1,29 @@
+// This file contains any web-workers used in the app, which are inlined by
+// webpack + workerize-loader, so that they can be used in the exported library
+// without needing any special configuration on the part of the consumer.
+// Web workers don't work in Jest, so in a test environment we directly import
+// them instead, and then mock up a faux-worker function
+
 /* eslint-disable import/no-webpack-loader-syntax */
-import graphWorker from 'workerize-loader?inline!./graph';
 
-// This file contains any web-workers used in the app,
-// which are inlined by webpack + workerize-loader, so that
-// they can be used in the exported library without needing
-// any special configuration on the part of the consumer
+const isTest = typeof jest !== 'undefined';
 
-export const graph = graphWorker;
+const graphWorker = isTest
+  ? require('./graph')
+  : require('workerize-loader?inline!./graph');
+
+/**
+ * Emulate a web worker for testing purposes
+ */
+const createMockWorker = worker => () => {
+  const mockWorker = {
+    terminate: () => {}
+  };
+  Object.keys(worker).forEach(name => {
+    mockWorker[name] = payload =>
+      new Promise(resolve => resolve(worker[name](payload)));
+  });
+  return mockWorker;
+};
+
+export const graph = isTest ? createMockWorker(graphWorker) : graphWorker;
