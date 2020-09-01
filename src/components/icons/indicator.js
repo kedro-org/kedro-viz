78e3699813ccb160f8e40b6920c51eb751a098fe
bru5 <45365769+bru5@users.noreply.github.com>
2020-09-01 18:25:50 +0100

    [KED-1887] Further work on sidebar updates

--- src/components/icons/indicator.js
+++ src/components/icons/indicator.js
@@ -5,6 +5,6 @@ export default ({ className }) => (
     className={className}
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24">
-    <rect x="7" y="9" width="5.5" height="5.5" rx="1" />
+    <rect x="8.5" y="9" width="5" height="5" rx="1" strokeWidth="1.5" />
   </svg>
 );
