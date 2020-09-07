bddbbb7217a1833997de459ab025cfeb2b49a995
Richard Westenra <rjwestenra@gmail.com>
2020-09-07 12:14:01 +0100

    Add tests for keyboard events

--- src/components/node-list/node-list-search.test.js
+++ src/components/node-list/node-list-search.test.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import { NodeListSearch, mapStateToProps } from './node-list-search';
 import { mockState, setup } from '../../utils/state.mock';
 
@@ -14,6 +16,29 @@ describe('NodeListSearch', () => {
     const search = wrapper.find('.pipeline-nodelist-search');
     search.simulate('keydown', { keyCode: 27 });
     expect(onUpdateSearchValue.mock.calls.length).toBe(1);
+  });
+
+  it('focuses when the user types Ctrl+F', () => {
+    const { container } = render(<NodeListSearch />);
+    const input = container.querySelector('input');
+    fireEvent.keyDown(container, { key: 'f', keyCode: 70, ctrlKey: true });
+    expect(input).toHaveFocus();
+  });
+
+  it('focuses when the user types Cmd+F', () => {
+    const { container } = render(<NodeListSearch />);
+    const input = container.querySelector('input');
+    fireEvent.keyDown(container, { key: 'f', keyCode: 70, metaKey: true });
+    expect(input).toHaveFocus();
+  });
+
+  it('does not prevent default browser find event if input is already focused', () => {
+    const { container } = render(<NodeListSearch />);
+    const event = { key: 'f', keyCode: 70, metaKey: true };
+    let allowsDefaultEvent = fireEvent.keyDown(container, event);
+    expect(allowsDefaultEvent).toBe(false);
+    allowsDefaultEvent = fireEvent.keyDown(container, event);
+    expect(allowsDefaultEvent).toBe(true);
   });
 
   it('maps state to props', () => {
