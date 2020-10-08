f497da50610b0f4c006278291a2a458dae99552c
studioswong <studioswong@gmail.com>
2020-10-08 10:08:35 +0100

    initial setup of input file (tests are failing)

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import App from './index';
 import getRandomPipeline from '../../utils/random-data';
 import animals from '../../utils/data/animals.mock';
+import input from '../../utils/data/input.json';
 import demo from '../../utils/data/demo.mock';
 import { mockState } from '../../utils/state.mock';
 import { Flags } from '../../utils/flags';
