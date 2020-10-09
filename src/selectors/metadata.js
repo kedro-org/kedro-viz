60fcbf501e3984e38f35b0858700eb9051e0c72f
bru5 <45365769+bru5@users.noreply.github.com>
2020-10-09 17:41:26 +0100

    [KED-1423] Show tag names on metadata panel

--- src/selectors/metadata.js
+++ src/selectors/metadata.js
@@ -22,25 +22,31 @@ export const getClickedNodeMetaData = createSelector(
     getClickedNode,
     getNodesById,
     state => state.node.tags,
+    state => state.tag.name,
     state => state.pipeline
   ],
-  (nodeId, nodes = {}, nodeTags, pipeline) => {
+  (nodeId, nodes = {}, nodeTags, tagNames, pipeline) => {
     const node = nodes[nodeId];
 
     if (node) {
-      // Note: node.sources node.targets require newgraph enabled
-      return {
+      const metadata = {
         node,
-        inputs:
-          node.sources &&
-          node.sources.map(edge => nodes[edge.source]).sort(sortAlpha),
-        outputs:
-          node.targets &&
-          node.targets.map(edge => nodes[edge.target]).sort(sortAlpha),
-        tags: [...nodeTags[node.id]].sort(sortAlpha),
+        tags: [...nodeTags[node.id]]
+          .map(tagId => tagNames[tagId])
+          .sort(sortAlpha),
         pipeline: pipeline.name[pipeline.active],
         runCommand: `kedro run --to-nodes ${node.fullName}`
       };
+
+      // Note: node.sources node.targets require newgraph enabled
+      if (node.sources && node.targets) {
+        Object.assign(metadata, {
+          inputs: node.sources.map(edge => nodes[edge.source]).sort(sortAlpha),
+          outputs: node.targets.map(edge => nodes[edge.target]).sort(sortAlpha)
+        });
+      }
+
+      return metadata;
     }
 
     return null;
