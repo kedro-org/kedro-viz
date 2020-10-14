2a0f325a386c1b9368d7c3afe290976cbb6b2393
Richard Westenra <rjwestenra@gmail.com>
2020-10-14 12:58:35 +0100

    Add a short timeout in load-data mock
    This will simulate real world use and should help catch race conditions

--- src/store/__mocks__/load-data.js
+++ src/store/__mocks__/load-data.js
@@ -17,20 +17,31 @@ export const mockAPIFeatureSupport = data => {
 };
 
 /**
+ * Create a promise that resolves after a timeout
+ * @param {number} ms Timeout in milliseconds
+ */
+const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+/**
  * Mock asynchronously loading/parsing data
  * @param {string} path JSON file location. Defaults to main data url from config.js
  * @return {function} A promise that will return when the file is loaded and parsed
  */
 const loadJsonData = async (path = getUrl('main')) => {
-  await Promise.resolve();
+  // Add a short timeout to simulate real world use,
+  // which should help catch race conditions
+  await timeout(50);
+
   // Use animals dataset in place of 'main' endpoint
   if (path.includes('main')) {
     return mockAPIFeatureSupport(animals);
   }
+
   // Use demo dataset for 'pipelines' endpoints
   if (path.includes('pipelines')) {
     return mockAPIFeatureSupport(demo);
   }
+
   const fullPath = `/public${path.substr(1)}`;
   throw new Error(
     `Unable to load pipeline data from ${path}. If you're running Kedro-Viz as a standalone (e.g. for JavaScript development), please check that you have placed a data file at ${fullPath}.`
