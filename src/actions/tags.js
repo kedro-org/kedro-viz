1e89a34268e1d702c2f3cff3b1a8ec9cf82cd718
bru5 <45365769+bru5@users.noreply.github.com>
2020-11-20 16:50:37 +0000

    Add batching to tag actions (#308)
    Tag actions now accept an array of ids as well as a single id.

--- src/actions/tags.js
+++ src/actions/tags.js
@@ -1,14 +1,14 @@
 export const TOGGLE_TAG_ACTIVE = 'TOGGLE_TAG_ACTIVE';
 
 /**
- * Toggle a tag's highlighting on/off
- * @param {string} tagID Tag id
- * @param {Boolean} active True if tag is active
+ * Toggle a tag's highlighting on/off (or array of tags)
+ * @param {string|Array} tagIDs Tag id(s)
+ * @param {Boolean} active True if tag(s) active
  */
-export function toggleTagActive(tagID, active) {
+export function toggleTagActive(tagIDs, active) {
   return {
     type: TOGGLE_TAG_ACTIVE,
-    tagID,
+    tagIDs: Array.isArray(tagIDs) ? tagIDs : [tagIDs],
     active
   };
 }
@@ -16,14 +16,14 @@ export function toggleTagActive(tagID, active) {
 export const TOGGLE_TAG_FILTER = 'TOGGLE_TAG_FILTER';
 
 /**
- * Toggle a tag on/off
- * @param {string} tagID Tag id
- * @param {Boolean} enabled True if tag is enabled
+ * Toggle a tag's filtering on/off (or array of tags)
+ * @param {string|Array} tagIDs Tag id(s)
+ * @param {Boolean} enabled True if tag(s) enabled
  */
-export function toggleTagFilter(tagID, enabled) {
+export function toggleTagFilter(tagIDs, enabled) {
   return {
     type: TOGGLE_TAG_FILTER,
-    tagID,
+    tagIDs: Array.isArray(tagIDs) ? tagIDs : [tagIDs],
     enabled
   };
 }
