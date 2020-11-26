f5ef68a25147d4c19b72b97038d764568d4142fe
studioswong <studioswong@gmail.com>
2020-11-26 18:28:08 +0000

    updated naming of docstring field within codebase to match API fieldname

--- src/store/normalize-data.js
+++ src/store/normalize-data.js
@@ -22,7 +22,7 @@ export const createInitialPipelineState = () => ({
     hovered: null,
     fetched: {},
     code: {},
-    docString: {},
+    docstring: {},
     parameters: {},
     filepath: {},
     datasetType: {}
@@ -121,7 +121,7 @@ const addNode = state => node => {
   state.node.tags[id] = node.tags || [];
   // supports for metadata in case it exists on initial load
   state.node.code[id] = node.code;
-  state.node.docString[id] = node.docString;
+  state.node.docstring[id] = node.docstring;
   state.node.parameters[id] = node.parameters;
   state.node.filepath[id] = node.filepath;
   state.node.datasetType[id] = node.datasetType;
