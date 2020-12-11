135d2d0d4dff6f7eff82dd492e1d3dd90742242b
bru5 <45365769+bru5@users.noreply.github.com>
2020-12-11 11:28:01 +0000

    [KED-2166] Further tests on metadata panel

--- src/components/metadata/metadata.test.js
+++ src/components/metadata/metadata.test.js
@@ -4,7 +4,9 @@ import { getClickedNodeMetaData } from '../../selectors/metadata';
 import { setup, mockState } from '../../utils/state.mock';
 import { addEdgeLinks } from '../../utils/graph/graph';
 
-const salmonNodeId = '443cf06a';
+const salmonTaskNodeId = '443cf06a';
+const catDatasetNodeId = '9d989e8d';
+const rabbitParamsNodeId = 'c38d4c6a';
 
 describe('MetaData', () => {
   // Add edge links, can be removed when new graph is default
@@ -28,71 +30,194 @@ describe('MetaData', () => {
     // Using attribute since traversal by sibling not supported
     wrapper.find(`.pipeline-metadata__row[data-label="${label}"]`);
 
-  it('shows the node type as an icon', () => {
-    const wrapper = mount({ nodeId: salmonNodeId });
-    expect(rowIcon(wrapper).hasClass('pipeline-node-icon--type-task')).toBe(
-      true
-    );
+  describe('Task nodes', () => {
+    it('shows the node type as an icon', () => {
+      const wrapper = mount({ nodeId: salmonTaskNodeId });
+      expect(rowIcon(wrapper).hasClass('pipeline-node-icon--type-task')).toBe(
+        true
+      );
+    });
+
+    it('shows the node name as the title', () => {
+      const wrapper = mount({ nodeId: salmonTaskNodeId });
+      expect(textOf(title(wrapper))).toEqual(['salmon']);
+    });
+
+    it('shows the node type as text', () => {
+      const wrapper = mount({ nodeId: salmonTaskNodeId });
+      const row = rowByLabel(wrapper, 'Type:');
+      expect(textOf(rowValue(row))).toEqual(['task']);
+    });
+
+    it('shows the node parameters', () => {
+      const wrapper = mount({ nodeId: salmonTaskNodeId });
+      const row = rowByLabel(wrapper, 'Parameters (-):');
+      expect(textOf(rowValue(row))).toEqual(['-']);
+    });
+
+    it('shows the node inputs', () => {
+      const wrapper = mount({ nodeId: salmonTaskNodeId });
+      const row = rowByLabel(wrapper, 'Inputs:');
+      expect(textOf(rowValue(row))).toEqual([
+        'Cat',
+        'Dog',
+        'Parameters',
+        'Params:rabbit'
+      ]);
+    });
+
+    it('shows the node outputs', () => {
+      const wrapper = mount({ nodeId: salmonTaskNodeId });
+      const row = rowByLabel(wrapper, 'Outputs:');
+      expect(textOf(rowValue(row))).toEqual(['Horse', 'Sheep']);
+    });
+
+    it('shows the node tags', () => {
+      const wrapper = mount({ nodeId: salmonTaskNodeId });
+      const row = rowByLabel(wrapper, 'Tags:');
+      expect(textOf(rowValue(row))).toEqual(['Small']);
+    });
+
+    it('shows the node pipeline', () => {
+      const wrapper = mount({ nodeId: salmonTaskNodeId });
+      const row = rowByLabel(wrapper, 'Pipeline:');
+      expect(textOf(rowValue(row))).toEqual(['Default']);
+    });
+
+    it('shows the node run command', () => {
+      const wrapper = mount({ nodeId: salmonTaskNodeId });
+      const row = rowByLabel(wrapper, 'Run Command:');
+      expect(textOf(rowValue(row))).toEqual(['kedro run --to-nodes salmon']);
+    });
+
+    it('shows the node docstring', () => {
+      const wrapper = mount({ nodeId: salmonTaskNodeId });
+      const row = rowByLabel(wrapper, 'Description (docstring):');
+      expect(textOf(rowValue(row))).toEqual(['-']);
+    });
+
+    it('copies run command when button clicked', () => {
+      window.navigator.clipboard = {
+        writeText: jest.fn()
+      };
+
+      const wrapper = mount({ nodeId: salmonTaskNodeId });
+      const copyButton = wrapper.find('button.pipeline-metadata__copy-button');
+
+      copyButton.simulate('click');
+
+      expect(window.navigator.clipboard.writeText).toHaveBeenCalledWith(
+        'kedro run --to-nodes salmon'
+      );
+    });
   });
 
-  it('shows the node name as the title', () => {
-    const wrapper = mount({ nodeId: salmonNodeId });
-    expect(textOf(title(wrapper))).toEqual(['salmon']);
+  describe('Dataset nodes', () => {
+    it('shows the node type as an icon', () => {
+      const wrapper = mount({ nodeId: catDatasetNodeId });
+      expect(rowIcon(wrapper).hasClass('pipeline-node-icon--type-data')).toBe(
+        true
+      );
+    });
+
+    it('shows the node name as the title', () => {
+      const wrapper = mount({ nodeId: catDatasetNodeId });
+      expect(textOf(title(wrapper))).toEqual(['Cat']);
+    });
+
+    it('shows the node type as text', () => {
+      const wrapper = mount({ nodeId: catDatasetNodeId });
+      const row = rowByLabel(wrapper, 'Type:');
+      expect(textOf(rowValue(row))).toEqual(['data']);
+    });
+
+    it('shows the node dataset type', () => {
+      const wrapper = mount({ nodeId: catDatasetNodeId });
+      const row = rowByLabel(wrapper, 'Dataset Type:');
+      expect(textOf(rowValue(row))).toEqual(['-']);
+    });
+
+    it('shows the node filepath', () => {
+      const wrapper = mount({ nodeId: catDatasetNodeId });
+      const row = rowByLabel(wrapper, 'File Path:');
+      expect(textOf(rowValue(row))).toEqual(['-']);
+    });
+
+    it('shows the node tags', () => {
+      const wrapper = mount({ nodeId: catDatasetNodeId });
+      const row = rowByLabel(wrapper, 'Tags:');
+      expect(textOf(rowValue(row))).toEqual(['Large', 'Medium', 'Small']);
+    });
+
+    it('shows the node pipeline', () => {
+      const wrapper = mount({ nodeId: catDatasetNodeId });
+      const row = rowByLabel(wrapper, 'Pipeline:');
+      expect(textOf(rowValue(row))).toEqual(['Default']);
+    });
+
+    it('shows the node run command', () => {
+      const wrapper = mount({ nodeId: catDatasetNodeId });
+      const row = rowByLabel(wrapper, 'Run Command:');
+      expect(textOf(rowValue(row))).toEqual(['kedro run --to-inputs cat']);
+    });
+
+    it('copies run command when button clicked', () => {
+      window.navigator.clipboard = {
+        writeText: jest.fn()
+      };
+
+      const wrapper = mount({ nodeId: catDatasetNodeId });
+      const copyButton = wrapper.find('button.pipeline-metadata__copy-button');
+
+      copyButton.simulate('click');
+
+      expect(window.navigator.clipboard.writeText).toHaveBeenCalledWith(
+        'kedro run --to-inputs cat'
+      );
+    });
   });
 
-  it('shows the node type as text', () => {
-    const wrapper = mount({ nodeId: salmonNodeId });
-    const row = rowByLabel(wrapper, 'Type:');
-    expect(textOf(rowValue(row))).toEqual(['task']);
-  });
+  describe('Parameter nodes', () => {
+    it('shows the node type as an icon', () => {
+      const wrapper = mount({ nodeId: rabbitParamsNodeId });
+      expect(
+        rowIcon(wrapper).hasClass('pipeline-node-icon--type-parameters')
+      ).toBe(true);
+    });
 
-  it('shows the node inputs', () => {
-    const wrapper = mount({ nodeId: salmonNodeId });
-    const row = rowByLabel(wrapper, 'Inputs:');
-    expect(textOf(rowValue(row))).toEqual([
-      'Cat',
-      'Dog',
-      'Parameters',
-      'Params:rabbit'
-    ]);
-  });
+    it('shows the node name as the title', () => {
+      const wrapper = mount({ nodeId: rabbitParamsNodeId });
+      expect(textOf(title(wrapper))).toEqual(['Params:rabbit']);
+    });
 
-  it('shows the node outputs', () => {
-    const wrapper = mount({ nodeId: salmonNodeId });
-    const row = rowByLabel(wrapper, 'Outputs:');
-    expect(textOf(rowValue(row))).toEqual(['Horse', 'Sheep']);
-  });
+    it('shows the node type as text', () => {
+      const wrapper = mount({ nodeId: rabbitParamsNodeId });
+      const row = rowByLabel(wrapper, 'Type:');
+      expect(textOf(rowValue(row))).toEqual(['parameters']);
+    });
 
-  it('shows the node tags', () => {
-    const wrapper = mount({ nodeId: salmonNodeId });
-    const row = rowByLabel(wrapper, 'Tags:');
-    expect(textOf(rowValue(row))).toEqual(['Small']);
-  });
+    it('shows the node filepath', () => {
+      const wrapper = mount({ nodeId: rabbitParamsNodeId });
+      const row = rowByLabel(wrapper, 'File Path:');
+      expect(textOf(rowValue(row))).toEqual(['-']);
+    });
 
-  it('shows the node pipeline', () => {
-    const wrapper = mount({ nodeId: salmonNodeId });
-    const row = rowByLabel(wrapper, 'Pipeline:');
-    expect(textOf(rowValue(row))).toEqual(['Default']);
-  });
+    it('shows the node parameters', () => {
+      const wrapper = mount({ nodeId: rabbitParamsNodeId });
+      const row = rowByLabel(wrapper, 'Parameters (-):');
+      expect(textOf(rowValue(row))).toEqual(['-']);
+    });
 
-  it('shows the node run command', () => {
-    const wrapper = mount({ nodeId: salmonNodeId });
-    const row = rowByLabel(wrapper, 'Run Command:');
-    expect(textOf(rowValue(row))).toEqual(['kedro run --to-nodes salmon']);
-  });
+    it('shows the node tags', () => {
+      const wrapper = mount({ nodeId: rabbitParamsNodeId });
+      const row = rowByLabel(wrapper, 'Tags:');
+      expect(textOf(rowValue(row))).toEqual(['Small']);
+    });
 
-  it('copies run command when button clicked', () => {
-    window.navigator.clipboard = {
-      writeText: jest.fn()
-    };
-
-    const wrapper = mount({ nodeId: salmonNodeId });
-    const copyButton = wrapper.find('button.pipeline-metadata__copy-button');
-
-    copyButton.simulate('click');
-
-    expect(window.navigator.clipboard.writeText).toHaveBeenCalledWith(
-      'kedro run --to-nodes salmon'
-    );
+    it('shows the node pipeline', () => {
+      const wrapper = mount({ nodeId: rabbitParamsNodeId });
+      const row = rowByLabel(wrapper, 'Pipeline:');
+      expect(textOf(rowValue(row))).toEqual(['Default']);
+    });
   });
 });
