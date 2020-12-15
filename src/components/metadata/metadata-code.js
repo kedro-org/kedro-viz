2569ac5fbcfd80ca41bb4a402618ea56534dcf10
bru5 <45365769+bru5@users.noreply.github.com>
2020-12-15 18:18:34 +0000

    [KED-2328] Add MetaDataCode and MetaCodeToggle components

---
+++ src/components/metadata/metadata-code.js
@@ -0,0 +1,46 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import hljs from 'highlight.js/lib/core';
+import python from 'highlight.js/lib/languages/python';
+import modifiers from '../../utils/modifiers';
+import './styles/metadata-code.css';
+
+hljs.registerLanguage('python', python);
+
+/**
+ * A highlighted code panel
+ */
+export const MetaDataCode = ({
+  visible = true,
+  value = '',
+  sidebarVisible
+}) => {
+  const codeRef = useRef();
+  const [highlighted, setHighlighted] = useState();
+
+  useEffect(() => {
+    const detected = hljs.highlightAuto(value);
+    const language = detected.language || detected.second_best.language;
+    setHighlighted(hljs.highlight(language, value).value);
+  }, [codeRef, value]);
+
+  return (
+    <div
+      className={modifiers(
+        'pipeline-metadata-code',
+        { visible, wide: !sidebarVisible },
+        'kedro'
+      )}>
+      <h2 className="pipeline-metadata-code__title">Code block</h2>
+      <code className="pipeline-metadata-code__code">
+        <pre ref={codeRef} dangerouslySetInnerHTML={{ __html: highlighted }} />
+      </code>
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  sidebarVisible: state.visible.sidebar
+});
+
+export default connect(mapStateToProps)(MetaDataCode);
