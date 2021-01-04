1393089ac6a69c6060bf72f05001508f16c2557c
bru5 <45365769+bru5@users.noreply.github.com>
2021-01-04 10:56:45 +0000

    [KED-2328] Add yaml language to code panel

--- src/components/metadata/metadata-code.js
+++ src/components/metadata/metadata-code.js
@@ -2,10 +2,12 @@ import React, { useRef, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import hljs from 'highlight.js/lib/core';
 import python from 'highlight.js/lib/languages/python';
+import yaml from 'highlight.js/lib/languages/yaml';
 import modifiers from '../../utils/modifiers';
 import './styles/metadata-code.css';
 
 hljs.registerLanguage('python', python);
+hljs.registerLanguage('yaml', yaml);
 
 /**
  * A highlighted code panel
