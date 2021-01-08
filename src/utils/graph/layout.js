a2f18a9d518d9ed7fcb43e06d46451c468ad841d
bru5 <45365769+bru5@users.noreply.github.com>
2021-01-08 11:50:18 +0000

    [KED-2212] Improve graph performance (#343)
    * [KED-2212]  Add constraint base to solver
    
    * [KED-2212] Refactor base constraints on graph
    
    * [KED-2212] Remove defaults for constraints in solver for performance
    
    * [KED-2212] Refactor constraint operators
    
    * [KED-2212] Add docs note for constraints
    
    * [KED-2212] Refactor constants for constraints
    
    * [KED-2212] Rename constraint delta to difference
    
    * [KED-2212] Rename constraint key to property
    
    * [KED-2212] Update solver tests

--- src/utils/graph/layout.js
+++ src/utils/graph/layout.js
@@ -1,5 +1,13 @@
 import { halfPI, snap, angle, compare, groupByRow } from './common';
-import { solve, greaterOrEqual, equalTo } from './solver';
+import { solve } from './solver';
+import {
+  rowConstraint,
+  layerConstraint,
+  parallelConstraint,
+  crossingConstraint,
+  separationConstraint,
+  separationStrictConstraint
+} from './constraints';
 
 /**
  * Finds positions for the given nodes relative to their edges.
@@ -32,29 +40,31 @@ export const layout = ({
   const parallelSingleConstraints = [];
   const parallelDoubleConstraints = [];
   const separationConstraints = [];
-  const snapConstraints = [];
+  const separationStrictConstraints = [];
 
   for (const node of nodes) {
     node.x = 0;
     node.y = 0;
   }
 
+  // Constraint constants passed to solver
+  const constants = {
+    spaceX,
+    spaceY,
+    basisX,
+    layerSpace: (spaceY + layerSpaceY) * 0.5
+  };
+
   // Constraints in Y formed by the edges of the graph
   const rowConstraints = edges.map(edge => ({
+    base: rowConstraint,
     a: edge.targetNode,
-    b: edge.sourceNode,
-    key: 'y',
-    operator: greaterOrEqual,
-    target: () => spaceY,
-    weightA: () => 0,
-    weightB: () => 1,
-    required: true
+    b: edge.sourceNode
   }));
 
   // Constraints in Y separating nodes into layers if specified
   if (layers) {
     const layerNames = Object.values(layers);
-    const layerSpace = (spaceY + layerSpaceY) * 0.5;
     let layerNodes = nodes.filter(node => node.nearestLayer === layerNames[0]);
 
     // For each defined layer
@@ -71,26 +81,18 @@ export const layout = ({
       // Constraints in Y for each node such that node.y <= layerNode.y - spaceY
       for (const node of layerNodes) {
         layerConstraints.push({
+          base: layerConstraint,
           a: layerNode,
-          b: node,
-          key: 'y',
-          operator: greaterOrEqual,
-          target: () => layerSpace,
-          weightA: () => 0,
-          weightB: () => 1
+          b: node
         });
       }
 
       // Constraints in Y for each node on the next layer such that node.y >= layerNode.y
       for (const node of nextLayerNodes) {
         layerConstraints.push({
+          base: layerConstraint,
           a: node,
-          b: layerNode,
-          key: 'y',
-          operator: greaterOrEqual,
-          target: () => layerSpace,
-          weightA: () => 0,
-          weightB: () => 1
+          b: layerNode
         });
       }
 
@@ -99,27 +101,10 @@ export const layout = ({
   }
 
   // Find the positions of each node in Y given the constraints exactly
-  solve([...rowConstraints, ...layerConstraints], 1, true);
+  solve([...rowConstraints, ...layerConstraints], constants, 1, true);
 
   // Find the rows formed by the nodes
   const rows = groupByRow(nodes);
-
-  // Constraints in X to prevent a pair of edges crossing
-  const crossingConstraint = {
-    basisX,
-    key: 'x',
-    operator: (distance, target, delta) =>
-      target >= 0 ? delta >= target : delta <= target,
-    target: (a, b, co) => {
-      // Find the minimal target position that separates both nodes
-      const sourceDelta = co.edgeA.sourceNode.x - co.edgeB.sourceNode.x;
-      const targetDelta = co.edgeA.targetNode.x - co.edgeB.targetNode.x;
-      return sourceDelta + targetDelta < 0 ? -co.basisX : co.basisX;
-    },
-    strength: co => 1 / co.basisX,
-    weightA: () => 0.5,
-    weightB: () => 0.5
-  };
 
   // For every pair of edges
   for (let i = 0; i < edges.length; i += 1) {
@@ -131,22 +116,22 @@ export const layout = ({
       // Add crossing constraint between edge source nodes, where different
       if (edgeA.source !== edgeB.source) {
         crossingConstraints.push({
-          ...crossingConstraint,
+          base: crossingConstraint,
           a: edgeA.sourceNode,
           b: edgeB.sourceNode,
-          edgeA,
-          edgeB
+          edgeA: edgeA,
+          edgeB: edgeB
         });
       }
 
       // Add crossing constraint between edge target nodes, where different
       if (edgeA.target !== edgeB.target) {
         crossingConstraints.push({
-          ...crossingConstraint,
+          base: crossingConstraint,
           a: edgeA.targetNode,
           b: edgeB.targetNode,
-          edgeA,
-          edgeB
+          edgeA: edgeA,
+          edgeB: edgeB
         });
       }
     }
@@ -154,32 +139,25 @@ export const layout = ({
 
   // Constraints in X to minimise edge length thereby prioritising straight parallel edges in Y
   for (const edge of edges) {
-    const parallelConstraint = {
+    const constraint = {
+      base: parallelConstraint,
       a: edge.sourceNode,
-      b: edge.targetNode,
-      key: 'x',
-      operator: equalTo,
-      target: () => 0,
-      // Lower degree nodes can be moved more freely than higher
-      strength: co =>
-        1 / Math.max(1, 0.5 * (co.a.targets.length + co.b.sources.length)),
-      weightA: () => 0.5,
-      weightB: () => 0.5
+      b: edge.targetNode
     };
 
-    parallelConstraints.push(parallelConstraint);
+    parallelConstraints.push(constraint);
 
     const sourceHasOneTarget = edge.sourceNode.targets.length === 1;
     const targetHasOneSource = edge.targetNode.sources.length === 1;
 
     // Collect edges connected to single-degree nodes at either end
     if (sourceHasOneTarget || targetHasOneSource) {
-      parallelSingleConstraints.push(parallelConstraint);
+      parallelSingleConstraints.push(constraint);
     }
 
     // Collect edges connected to single-degree at both ends
     if (sourceHasOneTarget && targetHasOneSource) {
-      parallelDoubleConstraints.push(parallelConstraint);
+      parallelDoubleConstraints.push(constraint);
     }
   }
 
@@ -188,14 +166,14 @@ export const layout = ({
 
   for (let i = 0; i < iterations; i += 1) {
     // Minimise crossing
-    solve(crossingConstraints, 1);
+    solve(crossingConstraints, constants, 1);
 
     // Minimise edge length
-    solve(parallelConstraints, 1);
+    solve(parallelConstraints, constants, 1);
 
     // Minimise edge length specifically for low-degree edges more strongly
-    solve(parallelSingleConstraints, halfIterations);
-    solve(parallelDoubleConstraints, halfIterations);
+    solve(parallelSingleConstraints, constants, halfIterations);
+    solve(parallelDoubleConstraints, constants, halfIterations);
 
     // Clear separation constraints from previous iteration
     separationConstraints.length = 0;
@@ -210,20 +188,15 @@ export const layout = ({
       // Constraints in X to maintain minimum node separation
       for (let j = 0; j < rowNodes.length - 1; j += 1) {
         separationConstraints.push({
+          base: separationConstraint,
           a: rowNodes[j],
-          b: rowNodes[j + 1],
-          key: 'x',
-          operator: (distance, target, delta) => delta <= target,
-          target: (ax, bx, co) => -spaceX - co.a.width * 0.5 - co.b.width * 0.5,
-          strength: () => 1,
-          weightA: () => 0.5,
-          weightB: () => 0.5
+          b: rowNodes[j + 1]
         });
       }
     }
 
     // Minimise node separation overlap
-    solve(separationConstraints, halfIterations);
+    solve(separationConstraints, constants, halfIterations);
   }
 
   // For each row already sorted in X
@@ -246,21 +219,22 @@ export const layout = ({
       );
 
       // Constraints in X to maintain target separation
-      snapConstraints.push({
+      separationStrictConstraints.push({
+        base: separationStrictConstraint,
         a: rowNodes[i + 1],
         b: rowNodes[i],
-        key: 'x',
-        operator: greaterOrEqual,
-        target: () => targetSeparation,
-        weightA: () => 0,
-        weightB: () => 1,
-        required: true
+        separation: targetSeparation
       });
     }
   }
 
   // Find final positions of each node in X under given constraints exactly
-  solve([...snapConstraints, ...parallelConstraints], 1, true);
+  solve(
+    [...separationStrictConstraints, ...parallelConstraints],
+    constants,
+    1,
+    true
+  );
 
   // Add additional spacing in Y for rows with many crossing edges
   expandDenseRows(edges, rows, spaceY);
