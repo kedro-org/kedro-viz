593cf815935bbf5d1c46436ca84d8dcfc193cc9b
Richard Westenra <rjwestenra@gmail.com>
2021-01-22 15:02:32 +0000

    Update icons with new paths

--- src/components/icons/label.js
+++ src/components/icons/label.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default ({ className }) => (
-  <svg className={className} viewBox="0 0 22 20">
-    <path d="M22 0v20H7.3L0 10 7.3 0H22zm-6.2 5h-3.6L10 15h2l.3-1.8h3.4L16 15H18L15.8 5zm-1.5 1.7l1 4.7h-2.6l1-4.7h.6z" />
+  <svg className={className} viewBox="0 0 24 24">
+    <path d="M22 3v18H8.333L2 12l6.333-9H22zm-6.155 4h-3.69L10 17h1.944l.38-1.809h3.352l.38 1.809H18L15.845 7zm-1.507 1.676l.972 4.75h-2.62l.972-4.75h.676z" />
   </svg>
 );
--- src/components/icons/map.js
+++ src/components/icons/map.js
@@ -1,15 +1,7 @@
 import React from 'react';
-import classnames from 'classnames';
 
 export default ({ className }) => (
-  <svg
-    className={classnames(className, 'pipeline-icon--stroke')}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20">
-    <path
-      transform="translate(-3 -4)"
-      strokeWidth="2"
-      d="M20.573 6.875L15.352 8.75l-6-3.75L4 6.875v12.96l5.352-2.085 6 3.75 5.221-2.736zM15.352 8v12.75M9.352 5v12.75"
-    />
+  <svg className={className} viewBox="0 0 24 24">
+    <path d="M9.5 3l6 3.7 6-2.1v13.9l-6.2 3.3-6-3.8L3 20.4V5.3L9.5 3zm10 4.4l-3.1 1.2v10.3l3.2-1.6V7.4zm-9.1-1.5v10.4l4 2.5V8.4l-4-2.5zm-2-.4L5 6.7v10.8l3.4-1.3V5.5z" />
   </svg>
 );
