4797c141826c7aa8f36f930df1cdb205a36cc307
Richard Westenra <rjwestenra@gmail.com>
2021-01-22 15:06:12 +0000

    Remove xmlns attributes from inline SVGs
    These attributes aren't necessary when SVGs are embeddded in HTML. See:
    - https://html.spec.whatwg.org/multipage/syntax.html#foreign-elements
    - https://stackoverflow.com/a/18468348/1651713

--- src/components/icons/indicator-off.js
+++ src/components/icons/indicator-off.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 export default ({ className }) => (
-  <svg
-    className={className}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24">
+  <svg className={className} viewBox="0 0 24 24">
     {/* Note: some strokeWidth values fail when zoomed in Chrome e.g. 2 */}
     <rect x="8.5" y="9" width="5" height="5" rx="1" strokeWidth="1.9" />
   </svg>
--- src/components/icons/indicator-partial.js
+++ src/components/icons/indicator-partial.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 export default ({ className }) => (
-  <svg
-    className={className}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24">
+  <svg className={className} viewBox="0 0 24 24">
     <rect x="6.5" y="10.5" width="8" height="1.5" />
   </svg>
 );
