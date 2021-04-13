687dd0fd7150103ff157bf1a6b5fbc6a9c067abc
bru5 <45365769+bru5@users.noreply.github.com>
2021-04-13 11:13:15 +0100

    [KED-2480] Refactor metadata tests to use afterLayoutActions

--- src/components/metadata/metadata.test.js
+++ src/components/metadata/metadata.test.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import MetaData from './index';
 import { getClickedNodeMetaData } from '../../selectors/metadata';
-import { setup, mockState } from '../../utils/state.mock';
-import { addEdgeLinks } from '../../utils/graph/graph';
+import { toggleNodeClicked } from '../../actions/nodes';
+import { setup, prepareState } from '../../utils/state.mock';
+import animals from '../../utils/data/animals.mock.json';
 
 const salmonTaskNodeId = '443cf06a';
 const catDatasetNodeId = '9d989e8d';
 const rabbitParamsNodeId = 'c38d4c6a';
 
 describe('MetaData', () => {
-  // Add edge links, can be removed when new graph is default
-  addEdgeLinks(mockState.animals.graph.nodes, mockState.animals.graph.edges);
-
   const mount = (props) => {
-    mockState.animals.node.clicked = props.nodeId;
-    return setup.mount(
-      <MetaData
-        visible={true}
-        metadata={getClickedNodeMetaData(mockState.animals)}
-      />
-    );
+    return setup.mount(<MetaData visible={true} />, {
+      afterLayoutActions: [
+        () => {
+          // Click the expected node
+          return toggleNodeClicked(props.nodeId);
+        },
+      ],
+    });
   };
 
   const textOf = (elements) => elements.map((element) => element.text());
@@ -33,15 +32,20 @@ describe('MetaData', () => {
   describe('All nodes', () => {
     it('limits parameters to 10 values and expands when button clicked', () => {
       // Get metadata for a sample
-      mockState.animals.node.clicked = salmonTaskNodeId;
-      const metadata = getClickedNodeMetaData(mockState.animals);
+      const metadata = getClickedNodeMetaData(prepareState({
+        data: animals,
+        afterLayoutActions: [
+          () => {
+            // Click the expected node
+            return toggleNodeClicked(salmonTaskNodeId);
+          },
+        ],
+      }));
 
       // Add extra mock parameters
       metadata.parameters = Array.from({ length: 20 }, (_, i) => `Test: ${i}`);
 
-      const wrapper = setup.mount(
-        <MetaData visible={true} metadata={metadata} />
-      );
+      const wrapper = setup.mount(<MetaData visible={true} metadata={metadata} />);
 
       const parametersRow = () => rowByLabel(wrapper, 'Parameters (20):');
       const expandButton = parametersRow().find(
