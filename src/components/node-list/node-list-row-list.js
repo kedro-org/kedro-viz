a72a3ddbf7f48e69ff0e05001393d82c9115fea3
rashidakanchwala <37628668+rashidakanchwala@users.noreply.github.com>
2021-04-13 14:13:09 +0100

    Delete lazy flag
    Lazy flag is no longer required as the feature is now going to be available by default.

--- src/components/node-list/node-list-row-list.js
+++ src/components/node-list/node-list-row-list.js
@@ -1,53 +1,9 @@
 import React from 'react';
 import modifiers from '../../utils/modifiers';
-import { connect } from 'react-redux';
 import NodeListRow, { nodeListRowHeight } from './node-list-row';
 import LazyList from '../lazy-list';
 
 const NodeRowList = ({
-  items = [],
-  group,
-  collapsed,
-  onItemClick,
-  onItemChange,
-  onItemMouseEnter,
-  onItemMouseLeave,
-}) => (
-  <ul
-    className={modifiers(
-      'pipeline-nodelist__children',
-      { closed: collapsed },
-      'pipeline-nodelist__list pipeline-nodelist__list--nested'
-    )}>
-    {items.map((item) => (
-      <NodeListRow
-        container="li"
-        key={item.id}
-        id={item.id}
-        kind={group.kind}
-        label={item.highlightedLabel}
-        name={item.name}
-        type={item.type}
-        active={item.active}
-        checked={item.checked}
-        disabled={item.disabled}
-        faded={item.faded}
-        visible={item.visible}
-        selected={item.selected}
-        unset={item.unset}
-        allUnset={group.allUnset}
-        visibleIcon={item.visibleIcon}
-        invisibleIcon={item.invisibleIcon}
-        onClick={() => onItemClick(item)}
-        onMouseEnter={() => onItemMouseEnter(item)}
-        onMouseLeave={() => onItemMouseLeave(item)}
-        onChange={(e) => onItemChange(item, !e.target.checked)}
-      />
-    ))}
-  </ul>
-);
-
-const NodeRowLazyList = ({
   items = [],
   group,
   collapsed,
@@ -122,11 +78,4 @@ const NodeRowLazyList = ({
   </LazyList>
 );
 
-export const mapStateToProps = (state, ownProps) => ({
-  lazy: state.flags.lazy,
-  ...ownProps,
-});
-
-export default connect(mapStateToProps)((props) =>
-  props.lazy ? <NodeRowLazyList {...props} /> : <NodeRowList {...props} />
-);
+export default NodeRowList;
