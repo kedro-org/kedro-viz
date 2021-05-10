a055a899a3a2f4d46d2e43998c46c0fe15cd2111
Richard Westenra <rjwestenra@gmail.com>
2021-05-10 14:41:32 +0100

    Persist localStorage when resetting pipeline state

--- src/store/reset.js
+++ src/store/reset.js
@@ -1,26 +1,30 @@
+import { mergeLocalStorage } from './initial-state';
+
 /**
- * Empty the store of all pipeline data
+ * Empty the store of all pipeline-related data entries,
+ * while maintaining stored values from localStorage
  * @param {object} state Full state snapshot
- * @returns state
+ * @returns state State with pipeline info removed
  */
 const resetPipelineState = (state) => {
   const { isArray } = Array;
   const isObject = (d) => typeof d === 'object' && d !== null && !isArray(d);
-  const keys = ['edge', 'layer', 'modularPipeline', 'node', 'pipeline', 'tag'];
+  // State data type keys that should be reset, e.g. state.edge
+  const types = ['edge', 'layer', 'modularPipeline', 'node', 'pipeline', 'tag'];
   const newState = { ...state };
-  for (const i of keys) {
-    const property = state[i];
-    const newProperty = { ...property };
-    for (const key of Object.keys(property)) {
-      if (isObject(property[key])) {
-        newProperty[key] = {};
-      } else if (isArray(property[key])) {
-        newProperty[key] = [];
+  for (const type of types) {
+    const typeData = { ...state[type] };
+    for (const key of Object.keys(typeData)) {
+      if (isObject(typeData[key])) {
+        typeData[key] = {};
+      } else if (isArray(typeData[key])) {
+        typeData[key] = [];
       }
     }
-    newState[i] = newProperty;
+    newState[type] = typeData;
   }
-  return newState;
+  // Reapply erased localStorage values:
+  return mergeLocalStorage(newState);
 };
 
 export default resetPipelineState;
