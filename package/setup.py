36cf91e74cedc5c3ae968fb9e1338f6fd525712c
Lim Hoang <limdauto@gmail.com>
2021-05-11 13:36:05 +0100

    Update setup.py to go through launchers

--- package/setup.py
+++ package/setup.py
@@ -79,7 +79,7 @@ setup(
     zip_safe=False,
     setup_requires=["pytest-runner==4.2"],
     entry_points={
-        "kedro.global_commands": ["kedro-viz = kedro_viz.server:commands"],
-        "kedro.line_magic": ["line_magic = kedro_viz.server:run_viz"],
+        "kedro.global_commands": ["kedro-viz = kedro_viz.launchers.cli:commands"],
+        "kedro.line_magic": ["line_magic = kedro_viz.launchers.jupyter:run_viz"],
     },
 )
