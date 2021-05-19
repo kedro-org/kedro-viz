551c83f58a8dedb5a3aa56f9b3590c69f8f7523e
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2021-05-19 13:11:50 +0100

    Fixing node module issue

--- src/setupTests.js
+++ src/setupTests.js
@@ -8,6 +8,3 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-
-// Require to create jest using Plotly.js library
-window.URL.createObjectURL = function () {};
