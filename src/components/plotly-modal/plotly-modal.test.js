ab406612c64e148aa02ac91d3860eeba2cf8fcd3
studioswong <studioswong@gmail.com>
2021-05-25 14:23:13 +0100

    delete describe case in plotly modal test

--- src/components/plotly-modal/plotly-modal.test.js
+++ src/components/plotly-modal/plotly-modal.test.js
@@ -10,49 +10,47 @@ import nodePlot from '../../utils/data/node_plot.mock.json';
 const bullPlotNodeID = 'c3p345ed';
 
 describe('Plotly Modal', () => {
-  describe('modal behaviour', () => {
-    // prepare mock metadata with plot data
-    const metadata = getClickedNodeMetaData(
-      prepareState({
-        data: animals,
-        afterLayoutActions: [() => toggleNodeClicked(bullPlotNodeID)],
-      })
-    );
-    metadata.plot = nodePlot.plot;
+  // prepare mock metadata with plot data
+  const metadata = getClickedNodeMetaData(
+    prepareState({
+      data: animals,
+      afterLayoutActions: [() => toggleNodeClicked(bullPlotNodeID)],
+    })
+  );
+  metadata.plot = nodePlot.plot;
 
-    const mount = (props) => {
-      return setup.mount(<PlotlyModal metadata={metadata} />, {
-        beforeLayoutActions: [() => toggleNodeClicked(props.nodeId)],
-        afterLayoutActions: [
-          () => {
-            // Click the expected node
-            return togglePlotModal(true);
-          },
-        ],
-      });
-    };
-
-    it('renders without crashing', () => {
-      const wrapper = mount({ nodeId: bullPlotNodeID });
-      expect(wrapper.find('.pipeline-plotly-modal').length).toBe(1);
+  const mount = (props) => {
+    return setup.mount(<PlotlyModal metadata={metadata} />, {
+      beforeLayoutActions: [() => toggleNodeClicked(props.nodeId)],
+      afterLayoutActions: [
+        () => {
+          // Click the expected node
+          return togglePlotModal(true);
+        },
+      ],
     });
+  };
 
-    it('modal closes when collapse button is clicked', () => {
-      const wrapper = mount({ nodeId: bullPlotNodeID });
-      wrapper.find('.pipeline-plot-modal__collapse-plot').simulate('click');
-      expect(wrapper.find('.pipeline-plotly-modal').length).toBe(0);
-    });
+  it('renders without crashing', () => {
+    const wrapper = mount({ nodeId: bullPlotNodeID });
+    expect(wrapper.find('.pipeline-plotly-modal').length).toBe(1);
+  });
 
-    it('modal closes when back button is clicked', () => {
-      const wrapper = mount({ nodeId: bullPlotNodeID });
-      wrapper.find('.pipeline-plot-modal__back').simulate('click');
-      expect(wrapper.find('.pipeline-plotly-modal').length).toBe(0);
-    });
+  it('modal closes when collapse button is clicked', () => {
+    const wrapper = mount({ nodeId: bullPlotNodeID });
+    wrapper.find('.pipeline-plot-modal__collapse-plot').simulate('click');
+    expect(wrapper.find('.pipeline-plotly-modal').length).toBe(0);
+  });
 
-    it('shows plot when a plot node is clicked', () => {
-      const wrapper = mount({ nodeId: bullPlotNodeID });
-      expect(wrapper.find('.pipeline-plot-modal__header').length).toBe(0);
-      expect(wrapper.find('.pipeline-plotly-chart').length).toBe(0);
-    });
+  it('modal closes when back button is clicked', () => {
+    const wrapper = mount({ nodeId: bullPlotNodeID });
+    wrapper.find('.pipeline-plot-modal__back').simulate('click');
+    expect(wrapper.find('.pipeline-plotly-modal').length).toBe(0);
+  });
+
+  it('shows plot when a plot node is clicked', () => {
+    const wrapper = mount({ nodeId: bullPlotNodeID });
+    expect(wrapper.find('.pipeline-plot-modal__header').length).toBe(0);
+    expect(wrapper.find('.pipeline-plotly-chart').length).toBe(0);
   });
 });
