8340c630333ad5cbfa40470b8363b559ca57bbc8
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2021-06-01 18:32:25 +0100

    test prettier

--- src/components/node-list/node-list-items.test.js
+++ src/components/node-list/node-list-items.test.js
@@ -120,7 +120,8 @@ describe('node-list-selectors', () => {
   describe('getSections', () => {
     const sections = getSections({ flags: { modularpipeline: false } });
 
-    const groupType = expect.objectContaining({
+    const groupType = 
+    expect.objectContaining({
       name: expect.any(String),
       types: expect.any(Array),
     });
