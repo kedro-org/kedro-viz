ba85e8dcacd186da0d6a952e90f15416bbca9e88
Lim Hoang <limdauto@gmail.com>
2021-06-02 17:45:21 +0100

    Remove unused import

--- package/tests/test_models/test_graph/test_graph_nodes.py
+++ package/tests/test_models/test_graph/test_graph_nodes.py
@@ -25,7 +25,6 @@
 #
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import sys
 from pathlib import Path
 from textwrap import dedent
 from unittest.mock import MagicMock, patch
