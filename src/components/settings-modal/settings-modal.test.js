f7caf87856e62be8940a93f2d99c356e64dc7fc1
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2021-06-16 13:55:47 +0100

    Added additional test cases

--- src/components/settings-modal/settings-modal.test.js
+++ src/components/settings-modal/settings-modal.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import SettingsModal, { mapStateToProps } from './index';
+import SettingsModal, { mapStateToProps, mapDispatchToProps } from './index';
 import { mockState, setup } from '../../utils/state.mock';
 
 describe('SettingsModal', () => {
   it('renders without crashing', () => {
     const wrapper = setup.mount(<SettingsModal />);
     expect(wrapper.find('.pipeline-settings-modal__content').length).toBe(1);
+  });
+
+  it('modal closes when X button is clicked', () => {
+    const wrapper = setup.mount(<SettingsModal />);
+    wrapper.find('.kui-icon--close').simulate('click');
+
+    expect(wrapper.find('.kui-modal--visible').length).toBe(0);
   });
 
   it('maps state to props', () => {
@@ -20,5 +27,22 @@ describe('SettingsModal', () => {
       flags: expect.any(Object),
     };
     expect(mapStateToProps(mockState.animals)).toEqual(expectedResult);
+  });
+
+  it('maps dispatch to props', async () => {
+    const dispatch = jest.fn();
+
+    mapDispatchToProps(dispatch).onToggle(false);
+    expect(dispatch.mock.calls[0][0]).toEqual({
+      type: 'TOGGLE_SETTINGS_MODAL',
+      visible: false,
+    });
+
+    mapDispatchToProps(dispatch).onToggleFlag('newparams', false);
+    expect(dispatch.mock.calls[1][0]).toEqual({
+      type: 'CHANGE_FLAG',
+      name: 'newparams',
+      value: false,
+    });
   });
 });
