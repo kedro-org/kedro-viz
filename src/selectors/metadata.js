34a774cf529683ed3dc7b7164f4669ff46fe2e6d
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2021-06-21 10:34:54 +0100

    Refactored metadata selector into two functions

--- src/selectors/metadata.js
+++ src/selectors/metadata.js
@@ -1,6 +1,8 @@
 import { createSelector } from 'reselect';
 import { getGraphNodes } from './nodes';
+import { arrayToObject } from '../utils';
 
+const getNodeIDs = (state) => state.node.ids;
 const getClickedNode = (state) => state.node.clicked;
 
 /**
@@ -35,11 +37,11 @@ const getRunCommand = (node) => {
 };
 
 /**
- * Gets metadata for the currently clicked node if any
+ * Gets metadata for the nodes
  */
-export const getClickedNodeMetaData = createSelector(
+export const getNodesMetaData = createSelector(
   [
-    getClickedNode,
+    getNodeIDs,
     getGraphNodes,
     (state) => state.node.tags,
     (state) => state.tag.name,
@@ -51,7 +53,7 @@ export const getClickedNodeMetaData = createSelector(
     (state) => state.node.datasetType,
   ],
   (
-    nodeId,
+    nodeIDs,
     nodes = {},
     nodeTags,
     tagNames,
@@ -61,43 +63,52 @@ export const getClickedNodeMetaData = createSelector(
     nodeParameters,
     nodePlot,
     nodeDatasetTypes
-  ) => {
-    const node = nodes[nodeId];
+  ) =>
+    arrayToObject(nodeIDs, (nodeID) => {
+      const node = nodes[nodeID];
 
-    if (!node) {
-      return null;
-    }
+      if (!node) {
+        return null;
+      }
 
-    const parameters =
-      nodeParameters[node.id] &&
-      Object.entries(nodeParameters[node.id]).map(
-        ([key, value]) => `${key}: ${value}`
-      );
+      const parameters =
+        nodeParameters[node.id] &&
+        Object.entries(nodeParameters[node.id]).map(
+          ([key, value]) => `${key}: ${value}`
+        );
 
-    const metadata = {
-      node,
-      tags: [...nodeTags[node.id]]
-        .map((tagId) => tagNames[tagId])
-        .sort(sortAlpha),
-      pipeline: pipeline.name[pipeline.active],
-      parameters,
-      runCommand: getRunCommand(node),
-      code: nodeCodes[node.id],
-      filepath: nodeFilepaths[node.id],
-      plot: nodePlot[node.id],
-      datasetType: nodeDatasetTypes[node.id],
-    };
+      const metadata = {
+        node,
+        tags: [...nodeTags[node.id]]
+          .map((tagId) => tagNames[tagId])
+          .sort(sortAlpha),
+        pipeline: pipeline.name[pipeline.active],
+        parameters,
+        runCommand: getRunCommand(node),
+        code: nodeCodes[node.id],
+        filepath: nodeFilepaths[node.id],
+        plot: nodePlot[node.id],
+        datasetType: nodeDatasetTypes[node.id],
+      };
 
-    // Note: node.sources node.targets require oldgraph enabled
-    if (node.sources && node.targets) {
-      metadata.inputs = node.sources
-        .map((edge) => nodes[edge.source])
-        .sort(sortAlpha);
-      metadata.outputs = node.targets
-        .map((edge) => nodes[edge.target])
-        .sort(sortAlpha);
-    }
+      // Note: node.sources node.targets require oldgraph enabled
+      if (node.sources && node.targets) {
+        metadata.inputs = node.sources
+          .map((edge) => nodes[edge.source])
+          .sort(sortAlpha);
+        metadata.outputs = node.targets
+          .map((edge) => nodes[edge.target])
+          .sort(sortAlpha);
+      }
 
-    return metadata;
-  }
+      return metadata;
+    })
+);
+
+/**
+ * Gets metadata for the current clicked node if any
+ */
+export const getClickedNodeMetaData = createSelector(
+  [getClickedNode, getNodesMetaData],
+  (nodeID, nodesMetaData) => nodesMetaData[nodeID]
 );
