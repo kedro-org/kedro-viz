b9451cf1e08927479b39cb84b7615bba997d7038
Lim Hoang <limdauto@gmail.com>
2021-09-13 15:49:34 +0100

    Downgrade to 3.1.4

--- package/features/steps/cli_steps.py
+++ package/features/steps/cli_steps.py
@@ -130,7 +130,7 @@ def exec_kedro_target_checked(context, command):
 @given('I have installed kedro version "{version}"')
 def install_kedro(context, version):
     """Execute Kedro command and check the status."""
-    pin_dynaconf = [context.pip, "install", "dynaconf==3.1.5"]
+    pin_dynaconf = [context.pip, "install", "'dynaconf==3.1.4'"]
     if version == "latest":
         cmd = [context.pip, "install", "-U", "kedro"]
     else:
@@ -142,7 +142,7 @@ def install_kedro(context, version):
         print(res.stderr)
         assert False
 
-    # pin dynaconf to avoid backward breaking chang ein 3.1.7
+    # pin dynaconf to avoid backward breaking change in 3.1.5
     run(pin_dynaconf, env=context.env)
 
 
