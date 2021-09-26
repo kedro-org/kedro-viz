36db11654f02cfe0c24adf17ef0bc221b3a6f423
Lim Hoang <limdauto@gmail.com>
2021-09-26 16:47:52 +0100

    Document modular pipeline type in the API response

--- package/kedro_viz/api/responses.py
+++ package/kedro_viz/api/responses.py
@@ -207,10 +207,8 @@ class ModularPipelineChildAPIResponse(BaseAPIResponse):
     type: str
 
 
-class ModularPipelinesTreeAPIResponse(BaseAPIResponse):
-    """Model the tree representation of modular pipelines in the API response.
-    The root of the tree is designated by the "__root__" key.
-    """
+class ModularPipelinesTreeNodeAPIResponse(BaseAPIResponse):
+    """Model a node in the tree representation of modular pipelines in the API response."""
 
     id: str
     name: str
@@ -218,37 +216,40 @@ class ModularPipelinesTreeAPIResponse(BaseAPIResponse):
     outputs: List[str]
     children: List[ModularPipelineChildAPIResponse]
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "__root__": {
-                    "id": "__root__",
-                    "name": "Root",
-                    "inputs": [],
-                    "outputs": [],
-                    "children": [
-                        {"id": "d577578a", "type": "parameters"},
-                        {"id": "data_science", "type": "modularPipeline"},
-                        {"id": "f1f1425b", "type": "parameters"},
-                        {"id": "data_engineering", "type": "modularPipeline"},
-                    ],
-                },
-                "data_engineering": {
-                    "id": "data_engineering",
-                    "name": "Data Engineering",
-                    "inputs": ["d577578a"],
-                    "outputs": [],
-                    "children": [],
-                },
-                "data_science": {
-                    "id": "data_science",
-                    "name": "Data Science",
-                    "inputs": ["f1f1425b"],
-                    "outputs": [],
-                    "children": [],
-                },
-            }
-        }
+
+# Represent the modular pipelines in the API response as a tree.
+# The root node is always designated with the __root__ key.
+# Example:
+# {
+#     "__root__": {
+#            "id": "__root__",
+#            "name": "Root",
+#            "inputs": [],
+#            "outputs": [],
+#            "children": [
+#                {"id": "d577578a", "type": "parameters"},
+#                {"id": "data_science", "type": "modularPipeline"},
+#                {"id": "f1f1425b", "type": "parameters"},
+#                {"id": "data_engineering", "type": "modularPipeline"},
+#            ],
+#        },
+#        "data_engineering": {
+#            "id": "data_engineering",
+#            "name": "Data Engineering",
+#            "inputs": ["d577578a"],
+#            "outputs": [],
+#            "children": [],
+#        },
+#        "data_science": {
+#            "id": "data_science",
+#            "name": "Data Science",
+#            "inputs": ["f1f1425b"],
+#            "outputs": [],
+#            "children": [],
+#        },
+#    }
+# }
+ModularPipelinesTreeAPIResponse = Dict[str, ModularPipelinesTreeNodeAPIResponse]
 
 
 class GraphAPIResponse(BaseAPIResponse):
@@ -257,7 +258,7 @@ class GraphAPIResponse(BaseAPIResponse):
     layers: List[str]
     tags: List[NamedEntityAPIResponse]
     pipelines: List[NamedEntityAPIResponse]
-    modular_pipelines: Dict[str, ModularPipelinesTreeAPIResponse]
+    modular_pipelines: ModularPipelinesTreeAPIResponse
     selected_pipeline: str
 
 
