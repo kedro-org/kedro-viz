97736313c7f217d045b0a511693159a18e3b9bab
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2021-10-06 10:46:54 +0100

    Added pyYaml to env.py

--- package/kedro_viz/data_access/__init__.py
+++ package/kedro_viz/data_access/__init__.py
@@ -28,4 +28,4 @@
 """`kedro_viz.data_access` provides an interface to save and load data for viz backend."""
 from .managers import DataAccessManager
 
-data_access_manager = DataAccessManager() 
+data_access_manager = DataAccessManager()
