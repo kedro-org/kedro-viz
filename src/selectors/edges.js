85bf3537bc3d452e27f126f823558783767155ca
Lim Hoang <limdauto@gmail.com>
2021-10-19 22:13:05 +0100

    Simplify

--- src/selectors/edges.js
+++ src/selectors/edges.js
@@ -118,16 +118,15 @@ export const getVisibleEdges = createSelector(
     getEdgeTargets,
     getTransitiveEdges,
   ],
-  (edgeIDs, edgeDisabled, edgeSources, edgeTargets, transitiveEdges) => {
-    return edgeIDs
+  (edgeIDs, edgeDisabled, edgeSources, edgeTargets, transitiveEdges) =>
+    edgeIDs
       .filter((id) => !edgeDisabled[id])
       .concat(transitiveEdges.edgeIDs)
       .map((id) => ({
         id,
         source: edgeSources[id] || transitiveEdges.sources[id],
         target: edgeTargets[id] || transitiveEdges.targets[id],
-      }));
-  }
+      }))
 );
 
 /**
