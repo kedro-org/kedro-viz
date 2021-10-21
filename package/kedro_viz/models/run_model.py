ad498ced1ac56a88b8735e3bdf0fcf116f4864b0
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2021-10-21 10:07:05 +0100

    Removed unused pylint-disabled

--- package/kedro_viz/models/run_model.py
+++ package/kedro_viz/models/run_model.py
@@ -26,11 +26,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Data model to represent run data from a Kedro Session.
-Named RunModel to avoid confusion with sqlalchemy Session
-and fastapi Session.
-"""
-# pylint: disable=missing-class-docstring
+"""Data model to represent run data from a Kedro Session."""
 # pylint: disable=too-few-public-methods
 
 from sqlalchemy import Column, String
@@ -41,10 +37,14 @@ Base = declarative_base()
 
 
 class RunModel(Base):
+    """Data model to represent run data from a Kedro Session."""
+
     __tablename__ = "runs"
 
     id = Column(String, primary_key=True, index=True)
     blob = Column(JSON)
 
     class Config:
+        """Supports data model to map to ORM objects"""
+
         orm_mode = True
