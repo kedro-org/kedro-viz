304690af4da8925b84f5a781d7e12a24569834a6
Lim Hoang <limdauto@gmail.com>
2021-10-25 17:42:45 +0100

    Fix types

--- package/kedro_viz/database.py
+++ package/kedro_viz/database.py
@@ -28,15 +28,17 @@
 
 """Database management layer based on SQLAlchemy"""
 
+from pathlib import Path
 from typing import Tuple
 
 from sqlalchemy import create_engine
 from sqlalchemy.engine.base import Engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 
 
-def create_db_engine(session_store_location: str) -> Tuple[Engine, Session]:
+def create_db_engine(
+    session_store_location: Path,
+) -> Tuple[Engine, sessionmaker]:
     """SQLAlchemy connection to a SQLite DB"""
     database_url = f"sqlite:///{session_store_location}"
     engine = create_engine(database_url, connect_args={"check_same_thread": False})
