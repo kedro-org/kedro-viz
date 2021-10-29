a8a12ba41218ea13b6cae53fe9a4d90987a8988e
studioswong <studioswong@gmail.com>
2021-10-29 22:02:53 +0100

    added fetch and createHttpLink

--- src/apollo/config.js
+++ src/apollo/config.js
@@ -1,9 +1,15 @@
-import fetch from 'cross-fetch'; // this fetch import is needed to pass the 'testJSImport' test on the CI given that there is no default installed fetch on the CI enviroment.
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import fetch from 'cross-fetch';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const link = createHttpLink({
+  /** our graphql endpoint */
+  uri: 'http://localhost:4142/graphql',
+  fetch,
+});
 
 export const client = new ApolloClient({
   connectToDevTools: true,
-  uri: 'http://localhost:4142/graphql',
+  link,
   cache: new InMemoryCache(),
   resolvers: {},
   defaultOptions: {
