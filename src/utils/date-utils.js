5a95f2ecc6880c299ee42cecd9403f3f0c388cb5
Tynan DeBold <thdebold@gmail.com>
2021-11-18 10:07:22 +0000

    update formatting for timestamp in toHumanReadableTime function

--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -4,9 +4,30 @@ const _dayJs = dayjs.extend(relativeTime);
 
 /**
  * Take a timestamp and return a meaningful length of time, e.g. 5 months ago
- * @param {ISO 8601 string} timestamp The timestamp to be converted
+ * Kedro uses this format VERSION_FORMAT = "%Y-%m-%dT%H.%M.%S.%fZ"
+ * So we need to do some string manipulation to get it to this formatted
+ * version: 2021-11-08T18:31:01.171Z
+ * @param {string} timestamp The timestamp to be converted
  * @returns A human-readable from-now date
  */
 export const toHumanReadableTime = (timestamp) => {
-  return _dayJs(timestamp).fromNow();
+  const splitTimestamp = timestamp.split('.');
+  let formattedTimestamp = '';
+
+  let i = 0;
+  const length = splitTimestamp.length;
+
+  while (i < length) {
+    if (i < 2) {
+      formattedTimestamp += `${splitTimestamp[i]}:`;
+    } else if (i === 2) {
+      formattedTimestamp += `${splitTimestamp[i]}.`;
+    } else {
+      formattedTimestamp += `${splitTimestamp[i]}`;
+    }
+
+    i++;
+  }
+
+  return _dayJs(formattedTimestamp).fromNow();
 };
