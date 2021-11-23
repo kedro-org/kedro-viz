a299aca8686efd178ebf0199356eb963b2a78573
studioswong <studioswong@gmail.com>
2021-11-23 22:42:13 +0000

    small modification to test

--- src/components/experiment-tracking/run-metadata/run-metadata.test.js
+++ src/components/experiment-tracking/run-metadata/run-metadata.test.js
@@ -11,12 +11,11 @@ const emptyRun = [
     id: '',
     author: '',
     bookmark: true,
-    timestamp: '2021-09-08T10:55:36.810Z',
-    gitSha: 'ad60192',
+    timestamp: '',
+    gitSha: '',
     gitBranch: '',
     runCommand: '',
-    notes:
-      'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.',
+    notes: '',
     title: '',
   },
 ];
