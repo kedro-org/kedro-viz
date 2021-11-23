b293930bd19e9d33c4d464e58075b005097f1d5c
studioswong <studioswong@gmail.com>
2021-11-23 20:02:17 +0000

    fixed alignment issue, added '-' to empty values and setup tests

--- src/components/experiment-tracking/run-metadata/index.js
+++ src/components/experiment-tracking/run-metadata/index.js
@@ -4,6 +4,10 @@ import { toHumanReadableTime } from '../../../utils/date-utils';
 
 import './run-metadata.css';
 
+// checks if value is an empty string, which is the default value returned
+// by the graphql endpoint for empty values
+const checkEmptyValue = (value) => (value !== '' ? value : '-');
+
 const RunMetadata = ({ isSingleRun, runs = [] }) => {
   let initialState = {};
   for (let i = 0; i < runs.length; i++) {
@@ -16,13 +20,12 @@ const RunMetadata = ({ isSingleRun, runs = [] }) => {
     setToggleNotes({ ...toggleNotes, [index]: !toggleNotes[index] });
   };
 
-  return runs.length === 0 ? (
-    <div>Loading...</div>
-  ) : (
+  return (
     <div
       className={classnames('details-metadata', {
         'details-metadata--single': isSingleRun,
-      })}>
+      })}
+    >
       {runs.map((run, i) => {
         const humanReadableTime = toHumanReadableTime(run.timestamp);
 
@@ -31,53 +34,60 @@ const RunMetadata = ({ isSingleRun, runs = [] }) => {
             className={classnames('details-metadata__run', {
               'details-metadata__run--single': isSingleRun,
             })}
-            key={run.gitSha}>
+            key={run.title}
+          >
             <table className="details-metadata__table">
               <tbody>
                 {isSingleRun ? (
                   <tr>
                     <td className="details-metadata__title" colSpan="2">
-                      {run.title}
+                      {checkEmptyValue(run.title)}
                     </td>
                   </tr>
                 ) : (
                   <tr>
                     {i === 0 ? <td></td> : null}
-                    <td className="details-metadata__title">{run.title}</td>
+                    <td className="details-metadata__title">
+                      {checkEmptyValue(run.title)}
+                    </td>
                   </tr>
                 )}
                 <tr>
                   {i === 0 ? <td>Created By</td> : null}
-                  <td>{run.author}</td>
+                  <td>{checkEmptyValue(run.author)}</td>
                 </tr>
                 <tr>
                   {i === 0 ? <td>Creation Date</td> : null}
-                  <td>{`${humanReadableTime} (${run.timestamp})`}</td>
+                  <td>{`${humanReadableTime} (${checkEmptyValue(
+                    run.timestamp
+                  )})`}</td>
                 </tr>
                 <tr>
                   {i === 0 ? <td>Git SHA</td> : null}
-                  <td>{run.gitSha}</td>
+                  <td>{checkEmptyValue(run.gitSha)}</td>
                 </tr>
                 <tr>
                   {i === 0 ? <td>Git Branch</td> : null}
-                  <td>{run.gitBranch}</td>
+                  <td>{checkEmptyValue(run.gitBranch)}</td>
                 </tr>
                 <tr>
                   {i === 0 ? <td>Run Command</td> : null}
-                  <td>{run.runCommand}</td>
+                  <td>{checkEmptyValue(run.runCommand)}</td>
                 </tr>
                 <tr>
                   {i === 0 ? <td>Notes</td> : null}
                   <td>
                     <p
                       className="details-metadata__notes"
-                      style={toggleNotes[i] ? { display: 'block' } : null}>
-                      {run.notes}
+                      style={toggleNotes[i] ? { display: 'block' } : null}
+                    >
+                      {checkEmptyValue(run.notes)}
                     </p>
                     {run.notes.length > 100 ? (
                       <button
                         className="details-metadata__show-more kedro"
-                        onClick={() => onToggleNoteExpand(i)}>
+                        onClick={() => onToggleNoteExpand(i)}
+                      >
                         {toggleNotes[i] ? 'Show less' : 'Show more'}
                       </button>
                     ) : null}
--- src/components/experiment-tracking/run-metadata/run-metadata.test.js
+++ src/components/experiment-tracking/run-metadata/run-metadata.test.js
@@ -6,6 +6,21 @@ import { configure, mount, shallow } from 'enzyme';
 
 configure({ adapter: new Adapter() });
 
+const emptyRun = [
+  {
+    id: '',
+    author: '',
+    bookmark: true,
+    timestamp: '2021-09-08T10:55:36.810Z',
+    gitSha: 'ad60192',
+    gitBranch: '',
+    runCommand: '',
+    notes:
+      'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.',
+    title: '',
+  },
+];
+
 describe('RunMetadata', () => {
   it('renders without crashing', () => {
     const wrapper = shallow(
@@ -26,6 +41,11 @@ describe('RunMetadata', () => {
 
     expect(wrapper.find('.details-metadata').length).toBe(1);
     expect(wrapper.find('.details-metadata__run--single').length).toBe(1);
+  });
+
+  it('shows a "-" for empty values ', () => {
+    const wrapper = mount(<RunMetadata isSingleRun={true} runs={emptyRun} />);
+    expect(wrapper.find('.details-metadata__title').text()).toMatch('-');
   });
 
   it('handles show more/less button click event', () => {
