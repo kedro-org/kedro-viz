5253217c0d1acce450a79f2c18a1e4de77ca841b
studioswong <studioswong@gmail.com>
2021-11-24 14:33:28 +0000

    update function naming and comments

--- src/components/experiment-tracking/run-metadata/index.js
+++ src/components/experiment-tracking/run-metadata/index.js
@@ -4,9 +4,9 @@ import { toHumanReadableTime } from '../../../utils/date-utils';
 
 import './run-metadata.css';
 
-// checks if value is an empty string, which is the default value returned
-// by the graphql endpoint for empty values
-const checkEmptyValue = (value) => (value !== '' ? value : '-');
+// We are only checking for an empty string as it is the default value
+// returned by the graphql endpoint for empty values ( not null or undefined )
+const sanitiseEmptyValue = (value) => (value !== '' ? value : '-');
 
 const RunMetadata = ({ isSingleRun, runs = [] }) => {
   let initialState = {};
@@ -34,45 +34,45 @@ const RunMetadata = ({ isSingleRun, runs = [] }) => {
             className={classnames('details-metadata__run', {
               'details-metadata__run--single': isSingleRun,
             })}
-            key={run.title}
+            key={run.title + i} // note: this should revert back to use gitSha once the BE returns the actual value
           >
             <table className="details-metadata__table">
               <tbody>
                 {isSingleRun ? (
                   <tr>
                     <td className="details-metadata__title" colSpan="2">
-                      {checkEmptyValue(run.title)}
+                      {sanitiseEmptyValue(run.title)}
                     </td>
                   </tr>
                 ) : (
                   <tr>
                     {i === 0 ? <td></td> : null}
                     <td className="details-metadata__title">
-                      {checkEmptyValue(run.title)}
+                      {sanitiseEmptyValue(run.title)}
                     </td>
                   </tr>
                 )}
                 <tr>
                   {i === 0 ? <td>Created By</td> : null}
-                  <td>{checkEmptyValue(run.author)}</td>
+                  <td>{sanitiseEmptyValue(run.author)}</td>
                 </tr>
                 <tr>
                   {i === 0 ? <td>Creation Date</td> : null}
-                  <td>{`${humanReadableTime} (${checkEmptyValue(
+                  <td>{`${humanReadableTime} (${sanitiseEmptyValue(
                     run.timestamp
                   )})`}</td>
                 </tr>
                 <tr>
                   {i === 0 ? <td>Git SHA</td> : null}
-                  <td>{checkEmptyValue(run.gitSha)}</td>
+                  <td>{sanitiseEmptyValue(run.gitSha)}</td>
                 </tr>
                 <tr>
                   {i === 0 ? <td>Git Branch</td> : null}
-                  <td>{checkEmptyValue(run.gitBranch)}</td>
+                  <td>{sanitiseEmptyValue(run.gitBranch)}</td>
                 </tr>
                 <tr>
                   {i === 0 ? <td>Run Command</td> : null}
-                  <td>{checkEmptyValue(run.runCommand)}</td>
+                  <td>{sanitiseEmptyValue(run.runCommand)}</td>
                 </tr>
                 <tr>
                   {i === 0 ? <td>Notes</td> : null}
@@ -81,7 +81,7 @@ const RunMetadata = ({ isSingleRun, runs = [] }) => {
                       className="details-metadata__notes"
                       style={toggleNotes[i] ? { display: 'block' } : null}
                     >
-                      {checkEmptyValue(run.notes)}
+                      {sanitiseEmptyValue(run.notes)}
                     </p>
                     {run.notes.length > 100 ? (
                       <button
