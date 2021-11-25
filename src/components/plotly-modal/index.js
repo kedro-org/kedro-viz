2f3e03d8b23b11d88a20a591115ae97d321f167b
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2021-11-25 17:41:39 +0000

    Fixed plotly bug

--- src/components/plotly-modal/index.js
+++ src/components/plotly-modal/index.js
@@ -19,39 +19,47 @@ const PlotlyModal = ({ metadata, onToggle, visible }) => {
     return null;
   }
   return (
-    <div className="pipeline-plotly-modal">
-      <div className="pipeline-plot-modal__top">
-        <button
-          className="pipeline-plot-modal__back"
-          onClick={onCollapsePlotClick}
-        >
-          <BackIcon className="pipeline-plot-modal__back-icon"></BackIcon>
-          <span className="pipeline-plot-modal__back-text">Back</span>
-        </button>
-        <div className="pipeline-plot-modal__header">
-          <NodeIcon className="pipeline-plot-modal__icon" icon={nodeTypeIcon} />
-          <span className="pipeline-plot-modal__title">
-            {metadata.node.name}
-          </span>
+    <>
+      {' '}
+      {metadata && (
+        <div className="pipeline-plotly-modal">
+          <div className="pipeline-plot-modal__top">
+            <button
+              className="pipeline-plot-modal__back"
+              onClick={onCollapsePlotClick}
+            >
+              <BackIcon className="pipeline-plot-modal__back-icon"></BackIcon>
+              <span className="pipeline-plot-modal__back-text">Back</span>
+            </button>
+            <div className="pipeline-plot-modal__header">
+              <NodeIcon
+                className="pipeline-plot-modal__icon"
+                icon={nodeTypeIcon}
+              />
+              <span className="pipeline-plot-modal__title">
+                {metadata.node.name}
+              </span>
+            </div>
+          </div>
+          <PlotlyChart
+            data={metadata.plot.data}
+            layout={metadata.plot.layout}
+            view="modal"
+          />
+          <div className="pipeline-plot-modal__bottom">
+            <button
+              className="pipeline-plot-modal__collapse-plot"
+              onClick={onCollapsePlotClick}
+            >
+              <CollapseIcon className="pipeline-plot-modal__collapse-plot-icon"></CollapseIcon>
+              <span className="pipeline-plot-modal__collapse-plot-text">
+                Collapse Plotly Visualization
+              </span>
+            </button>
+          </div>
         </div>
-      </div>
-      <PlotlyChart
-        data={metadata.plot.data}
-        layout={metadata.plot.layout}
-        view="modal"
-      />
-      <div className="pipeline-plot-modal__bottom">
-        <button
-          className="pipeline-plot-modal__collapse-plot"
-          onClick={onCollapsePlotClick}
-        >
-          <CollapseIcon className="pipeline-plot-modal__collapse-plot-icon"></CollapseIcon>
-          <span className="pipeline-plot-modal__collapse-plot-text">
-            Collapse Plotly Visualization
-          </span>
-        </button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
