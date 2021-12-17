5c2d54f2e856cdc1fbfcc018f878ce3bec6a918c
Lim Hoang <limdauto@gmail.com>
2021-12-17 14:53:03 +0000

    Disable pylint for a specific file

--- package/kedro_viz/models/experiments_tracking.py
+++ package/kedro_viz/models/experiments_tracking.py
@@ -1,5 +1,5 @@
 """Data model to represent run data from a Kedro Session."""
-# pylint: disable=too-few-public-methods
+# pylint: disable=too-few-public-methods,missing-class-docstring
 
 from sqlalchemy import Column
 from sqlalchemy.ext.declarative import declarative_base
