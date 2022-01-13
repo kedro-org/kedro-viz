e16b42f9c3d3617bd1f5ac5f91b4790ac4ad4383
Lim Hoang <limdauto@gmail.com>
2022-01-13 15:45:48 +0000

    Breakup repositories module into multiple modules

--- package/kedro_viz/constants.py
+++ package/kedro_viz/constants.py
@@ -1,3 +1,7 @@
 """`kedro_viz.constants` defines constants to be used throughout the application."""
+import kedro
+from semver import VersionInfo
+
 DEFAULT_REGISTERED_PIPELINE_ID = "__default__"
+KEDRO_VERSION = VersionInfo.parse(kedro.__version__)
 ROOT_MODULAR_PIPELINE_ID = "__root__"
