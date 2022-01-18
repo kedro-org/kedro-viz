4a420b9667fc6402f42f11cf68490d88599c5bc4
Tynan DeBold <thdebold@gmail.com>
2022-01-18 14:33:23 +0000

    remove duplicated id field in getRunMetadata query

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -17,7 +17,6 @@ export const GET_RUNS = gql`
 export const GET_RUN_METADATA = gql`
   query getRunMetadata($runIds: [ID!]!) {
     runMetadata(runIds: $runIds) {
-      id
       author
       bookmark
       gitBranch
