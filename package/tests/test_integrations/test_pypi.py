45cae05e5a0076c4c2833cbc6c9aa4447cb569d4
Antony Milne <antony.milne@quantumblack.com>
2022-02-03 15:46:20 +0000

    Fix types in test
    Signed-off-by: Antony Milne <antony.milne@quantumblack.com>

--- package/tests/test_integrations/test_pypi.py
+++ package/tests/test_integrations/test_pypi.py
@@ -18,4 +18,7 @@ def test_get_latest_version(mocker, mock_http_response):
     [("1.0.0", "1.2.3", True), ("1.2.3", "1.0.0", False), ("1.0.0", None, False)],
 )
 def test_is_running_outdated_version(installed_version, latest_version, is_outdated):
+    installed_version = VersionInfo.parse(installed_version)
+    if latest_version is not None:
+        latest_version = VersionInfo.parse(latest_version)
     assert is_running_outdated_version(installed_version, latest_version) == is_outdated
