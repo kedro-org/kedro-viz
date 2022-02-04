240320853116e3b1acbb5eafa8f7f2cdc91bcac8
Antony Milne <antony.milne@quantumblack.com>
2022-02-04 15:40:43 +0000

    Fix default response when --pipeline argument is used
    Signed-off-by: Antony Milne <antony.milne@quantumblack.com>

--- package/kedro_viz/api/responses.py
+++ package/kedro_viz/api/responses.py
@@ -242,24 +242,28 @@ class GraphAPIResponse(BaseAPIResponse):
 
 def get_default_response() -> GraphAPIResponse:
     """Default response for `/api/main`."""
+    default_selected_pipeline_id = (
+        data_access_manager.get_default_selected_pipeline().id
+    )
+
     modular_pipelines_tree = (
         data_access_manager.create_modular_pipelines_tree_for_registered_pipeline(
-            DEFAULT_REGISTERED_PIPELINE_ID
+            default_selected_pipeline_id
         )
     )
 
     return GraphAPIResponse(
         nodes=data_access_manager.get_nodes_for_registered_pipeline(
-            DEFAULT_REGISTERED_PIPELINE_ID
+            default_selected_pipeline_id
         ),
         edges=data_access_manager.get_edges_for_registered_pipeline(
-            DEFAULT_REGISTERED_PIPELINE_ID
+            default_selected_pipeline_id
         ),
         tags=data_access_manager.tags.as_list(),
         layers=data_access_manager.get_sorted_layers_for_registered_pipeline(
-            DEFAULT_REGISTERED_PIPELINE_ID
+            default_selected_pipeline_id
         ),
         pipelines=data_access_manager.registered_pipelines.as_list(),
         modular_pipelines=modular_pipelines_tree,
-        selected_pipeline=data_access_manager.get_default_selected_pipeline().id,
+        selected_pipeline=default_selected_pipeline_id,
     )
