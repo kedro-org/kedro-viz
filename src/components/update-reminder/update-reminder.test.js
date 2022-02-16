47a799767b7c89137ddcc1a173482f9e3f3849a9
studioswong <studioswong@gmail.com>
2022-02-16 19:27:36 +0000

    added update-reminder tests

---
+++ src/components/update-reminder/update-reminder.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import UpdateReminder from './update-reminder';
+import { mount } from 'enzyme';
+
+describe('Update Reminder', () => {
+  const versions = { latest: '4.3.1', installed: '4.2.0', isOutDated: true };
+
+  const setDismiss = jest.fn();
+
+  it('renders without crashing', () => {
+    const wrapper = mount(<UpdateReminder versions={versions} />);
+    expect(wrapper.find('.update-reminder-unexpanded').length).toBe(1);
+  });
+
+  it('dismisses when the dismiss button is clicked', () => {
+    const wrapper = mount(
+      <UpdateReminder versions={versions} setDismiss={setDismiss} />
+    );
+    const dismissButton = wrapper.find('.update-reminder-unexpanded');
+
+    dismissButton.find('button').at(1).simulate('click');
+    expect(wrapper.find('.update-reminder-unexpanded').length).toBe(0);
+  });
+
+  it('popup expands when it is clicked', () => {
+    const wrapper = mount(
+      <UpdateReminder versions={versions} setDismiss={setDismiss} />
+    );
+    const dismissButton = wrapper.find('.update-reminder-unexpanded');
+
+    dismissButton.find('button').at(0).simulate('click');
+    expect(wrapper.find('.update-reminder-expanded-header').length).toBe(1);
+  });
+});
