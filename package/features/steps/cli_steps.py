18df66d3d67e894816bbae53c31f61d083d69e8b
Deepyaman Datta <deepyaman.datta@utexas.edu>
2022-02-24 07:23:26 -0500

    Apply suggestions from code review
    Co-authored-by: Tynan DeBold <thdebold@gmail.com>
    Signed-off-by: Deepyaman Datta <deepyaman.datta@utexas.edu>

--- package/features/steps/cli_steps.py
+++ package/features/steps/cli_steps.py
@@ -87,7 +87,7 @@ def install_project_requirements(context):
 
 @given('I have installed kedro version "{version}"')
 def install_kedro(context, version):
-    """Install kedro using pip."""
+    """Install Kedro using pip."""
     if version == "latest":
         cmd = [context.pip, "install", "-U", "kedro"]
     else:
@@ -102,7 +102,7 @@ def install_kedro(context, version):
 
 @when('I execute the kedro viz command "{command}"')
 def exec_viz_command(context, command):
-    """Execute Kedro viz command."""
+    """Execute Kedro-Viz command."""
     split_command = command.split()
     make_cmd = [context.kedro] + split_command
 
@@ -113,7 +113,7 @@ def exec_viz_command(context, command):
 
 @then("kedro-viz should start successfully")
 def check_kedroviz_up(context):
-    """Check that kedro-viz is up and responding to requests."""
+    """Check that Kedro-Viz is up and responding to requests."""
     max_duration = 30  # 30 seconds
     end_by = time() + max_duration
 
