e79e40e738009c1f6c6bbb4db9666d0acf31ed32
Antony Milne <antony.milne@quantumblack.com>
2022-03-31 12:44:48 +0000

    Remove click bound
    Signed-off-by: Antony Milne <antony.milne@quantumblack.com>

--- package/features/environment.py
+++ package/features/environment.py
@@ -84,7 +84,6 @@ def _setup_context_with_venv(context, venv_dir):
             "wheel",
             "botocore",
             "PyYAML>=4.2, <6.0",
-            "click<8.0",
         ],
         env=context.env,
     )
