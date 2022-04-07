d13ba794f6fae66bf774103cd3672c6d79f7d312
Antony Milne <49395058+AntonyMilneQB@users.noreply.github.com>
2022-04-07 16:46:38 +0100

    Apply suggestions from code review

--- package/features/environment.py
+++ package/features/environment.py
@@ -84,7 +84,7 @@ def _setup_context_with_venv(context, venv_dir):
             "wheel",
             "botocore",
             "PyYAML>=4.2, <6.0",
-            "click>=8.1.0",
+            "click<9.0",
         ],
         env=context.env,
     )
