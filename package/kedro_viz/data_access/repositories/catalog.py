238a5352323cddfed31e34aeaf29c8fcee3ba0a0
Antony Milne <antony.milne@quantumblack.com>
2022-05-16 10:02:37 +0000

    Simplify dataset suggest handling code
    Signed-off-by: Antony Milne <antony.milne@quantumblack.com>

--- package/kedro_viz/data_access/repositories/catalog.py
+++ package/kedro_viz/data_access/repositories/catalog.py
@@ -14,32 +14,6 @@ from kedro.io import (
 
 from kedro_viz.constants import KEDRO_VERSION
 
-# Kedro-Viz doesn't need to know the close matches for missing datasets,
-# and `difflib.get_close_matches` is too costly if performed repeatedly.
-# Before Kedro 0.18.1, use the `_get_dataset` method without the verbose
-# error introduced in https://github.com/kedro-org/kedro/commit/f7dd247.
-if KEDRO_VERSION.match(">=0.16.0") and KEDRO_VERSION.match("<0.18.1"):
-
-    def _get_dataset(
-        self, data_set_name: str, version: Version = None
-    ) -> AbstractDataSet:
-        if data_set_name not in self._data_sets:
-            raise DataSetNotFoundError(
-                "DataSet '{}' not found in the catalog".format(data_set_name)
-            )
-
-        data_set = self._data_sets[data_set_name]
-        if version and isinstance(
-            data_set, AbstractVersionedDataSet
-        ):  # pragma: no cover
-            # we only want to return a similar-looking dataset,
-            # not modify the one stored in the current catalog
-            data_set = data_set._copy(  # pylint: disable=protected-access
-                _version=version
-            )
-
-        return data_set
-
 
 class CatalogRepository:
     _catalog: DataCatalog
@@ -83,13 +57,14 @@ class CatalogRepository:
         dataset_obj: Optional[AbstractDataSet]
         if KEDRO_VERSION.match(">=0.16.0"):
             try:
-                if KEDRO_VERSION.match(">=0.18.1"):  # pragma: no cover
-                    dataset_obj = self._catalog._get_dataset(  # type: ignore
+                # Kedro 0.18.1 introduced the `suggest` argument to disable the expensive
+                # fuzzy-matching process.
+                if KEDRO_VERSION.match(">=0.18.1"):
+                    dataset_obj = self._catalog._get_dataset(
                         dataset_name, suggest=False
                     )
-                else:
-                    with patch.object(DataCatalog, "_get_dataset", new=_get_dataset):
-                        dataset_obj = self._catalog._get_dataset(dataset_name)
+                else:  # pragma: no cover
+                    dataset_obj = self._catalog._get_dataset(dataset_name)
             except DataSetNotFoundError:  # pragma: no cover
                 dataset_obj = None
         else:
