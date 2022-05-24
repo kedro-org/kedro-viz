803f6cfaa813144ab18a2cf8d30a1c6999182016
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2022-05-24 09:55:55 +0100

    fix tests

--- src/components/pipeline-warning/pipeline-warning.test.js
+++ src/components/pipeline-warning/pipeline-warning.test.js
@@ -7,60 +7,82 @@ import {
 import { mockState, setup } from '../../utils/state.mock';
 
 describe('PipelineWarning', () => {
-  it('renders without crashing', () => {
-    const mockFn = jest.fn();
-    const props = {
-      onDisable: mockFn,
-      onHide: mockFn,
-      nodes: [],
-      visible: true,
-    };
-    const wrapper = setup.mount(<PipelineWarning {...props} />);
-    expect(wrapper.find('.pipeline-warning').length).toBe(1);
+  describe('LargePipelineWarning', () => {
+    it('renders large pipeline warning without crashing', () => {
+      const mockFn = jest.fn();
+      const props = {
+        onDisable: mockFn,
+        onHide: mockFn,
+        nodes: ['node1', 'node2'],
+        visible: true,
+      };
+      const wrapper = setup.mount(<PipelineWarning {...props} />);
+      expect(wrapper.find('.pipeline-warning__title').length).toBe(1);
+    });
+
+    it('will call onHide upon clicking the "render anyway" button', () => {
+      const mockFn = jest.fn();
+      const props = {
+        onDisable: () => {},
+        onHide: mockFn,
+        nodes: [],
+        visible: true,
+      };
+      const wrapper = setup.mount(<PipelineWarning {...props} />);
+      wrapper.find('.button__btn').at(0).simulate('click');
+      expect(mockFn.mock.calls.length).toBe(1);
+    });
+
+    it('will call onDisable upon clicking the "Don\'t show this again" button', () => {
+      const mockFn = jest.fn();
+      const props = {
+        onDisable: mockFn,
+        onHide: () => {},
+        nodes: [],
+        visible: true,
+      };
+      const wrapper = setup.mount(<PipelineWarning {...props} />);
+      wrapper.find('.button__btn').at(1).simulate('click');
+      expect(mockFn.mock.calls.length).toBe(1);
+    });
   });
 
-  it('does not render empty pipeline warning when pipeline is not empty', () => {
-    const props = {
-      nodes: ['node1', 'node2'],
-    };
-    const wrapper = setup.mount(<PipelineWarning {...props} />);
-    expect(wrapper.find('.pipeline-warning').length).toBe(0);
+  describe('EmptyPipelineWarning', () => {
+    it('renders empty pipeline warning without crashing', () => {
+      const mockFn = jest.fn();
+      const props = {
+        onDisable: mockFn,
+        onHide: mockFn,
+        nodes: [],
+        visible: false,
+      };
+      const wrapper = setup.mount(<PipelineWarning {...props} />);
+      expect(wrapper.find('.pipeline-warning__title').length).toBe(1);
+    });
+
+    it('does not render empty pipeline warning when pipeline is not empty', () => {
+      const mockFn = jest.fn();
+      const props = {
+        onDisable: mockFn,
+        onHide: mockFn,
+        nodes: ['node1', 'node2'],
+        visible: false,
+      };
+      const wrapper = setup.mount(<PipelineWarning {...props} />);
+      expect(wrapper.find('.pipeline-warning__title').length).toBe(0);
+    });
   });
 
-  it('will call onHide upon clicking the "render anyway" button', () => {
-    const mockFn = jest.fn();
-    const props = {
-      onDisable: () => {},
-      onHide: mockFn,
-      nodes: [],
-      visible: true,
-    };
-    const wrapper = setup.mount(<PipelineWarning {...props} />);
-    wrapper.find('.button__btn').at(0).simulate('click');
-    expect(mockFn.mock.calls.length).toBe(1);
-  });
-
-  it('will call onDisable upon clicking the "Don\'t show this again" button', () => {
-    const mockFn = jest.fn();
-    const props = {
-      onDisable: mockFn,
-      onHide: () => {},
-      nodes: [],
-      visible: true,
-    };
-    const wrapper = setup.mount(<PipelineWarning {...props} />);
-    wrapper.find('.button__btn').at(1).simulate('click');
-    expect(mockFn.mock.calls.length).toBe(1);
-  });
-
-  it('maps state to props', () => {
-    const expectedResult = {
-      theme: expect.any(String),
-      nodes: expect.any(Object),
-      sidebarVisible: expect.any(Boolean),
-      visible: expect.any(Boolean),
-    };
-    expect(mapStateToProps(mockState.spaceflights)).toEqual(expectedResult);
+  describe('mapStateToProps', () => {
+    it('maps state to props', () => {
+      const expectedResult = {
+        theme: expect.any(String),
+        nodes: expect.any(Object),
+        sidebarVisible: expect.any(Boolean),
+        visible: expect.any(Boolean),
+      };
+      expect(mapStateToProps(mockState.spaceflights)).toEqual(expectedResult);
+    });
   });
 
   describe('mapDispatchToProps', () => {
