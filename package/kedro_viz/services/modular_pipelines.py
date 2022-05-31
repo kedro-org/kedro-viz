2ea53c64576aa3d8f73d053afddcbf7e37278ac3
Lim Hoang <limdauto@gmail.com>
2022-05-31 13:17:53 +0100

    Fix namespace collison when two different registered pipelines have a modular pipeline with the same name (#871)
    Signed-off-by: Lim Hoang <limdauto@gmail.com>

--- package/kedro_viz/services/modular_pipelines.py
+++ package/kedro_viz/services/modular_pipelines.py
@@ -3,7 +3,7 @@ The service layer consist of pure functions operating on domain models.
 """
 from typing import Dict
 
-from kedro_viz.constants import DEFAULT_REGISTERED_PIPELINE_ID, ROOT_MODULAR_PIPELINE_ID
+from kedro_viz.constants import ROOT_MODULAR_PIPELINE_ID
 from kedro_viz.models.graph import (
     GraphNode,
     GraphNodeType,
@@ -13,8 +13,7 @@ from kedro_viz.models.graph import (
 
 
 def expand_tree(
-    modular_pipelines_tree: Dict[str, ModularPipelineNode],
-    registered_pipeline_id: str = DEFAULT_REGISTERED_PIPELINE_ID,
+    modular_pipelines_tree: Dict[str, ModularPipelineNode]
 ) -> Dict[str, ModularPipelineNode]:
     """Expand a given modular pipelines tree by adding parents for each node in the tree
     based on the node's ID. Filter out any nodes that don't belong to the given
@@ -26,7 +25,6 @@ def expand_tree(
 
     Args:
         modular_pipelines_tree: The modular pipelines tree to expand.
-        registered_pipeline_id: The registered pipeline ID to filter modular pipelines.
     Returns:
         The expanded modular pipelines tree.
     Example:
@@ -41,10 +39,7 @@ def expand_tree(
         )
     }
     for modular_pipeline_id, modular_pipeline_node in modular_pipelines_tree.items():
-        if (
-            modular_pipeline_id == ROOT_MODULAR_PIPELINE_ID
-            or not modular_pipeline_node.belongs_to_pipeline(registered_pipeline_id)
-        ):
+        if modular_pipeline_id == ROOT_MODULAR_PIPELINE_ID:
             continue
 
         if modular_pipeline_id not in expanded_tree:
