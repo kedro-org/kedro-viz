146916ed2a1a7c3ac8743bd5d88211999ae9e894
huongg <huongg1409@gmail.com>
2022-06-09 11:45:50 +0100

    run-export-modal component
    Signed-off-by: huongg <huongg1409@gmail.com>

---
+++ src/components/experiment-tracking/run-export-modal.js/run-export-modal.js
@@ -0,0 +1,81 @@
+import React, { useState, useCallback } from 'react';
+import { CSVLink } from 'react-csv';
+
+import { constructExportData } from '../../../utils/experiment-tracking-utils';
+
+import Button from '../../ui/button';
+import Modal from '../../ui/modal';
+
+import './run-export-modal.css';
+
+const RunExportModal = ({
+  theme,
+  visible,
+  setShowRunExportModal,
+  runMetadata,
+  runTrackingData,
+}) => {
+  const [exportData, setExportData] = useState([]);
+  const [isExported, setIsExported] = useState(false);
+
+  const updateExportData = useCallback(() => {
+    setExportData(constructExportData(runMetadata, runTrackingData));
+  }, [runMetadata, runTrackingData]);
+
+  const handleClick = () => {
+    const setLocalStateTimeout = setTimeout(() => {
+      setIsExported(true);
+    }, 800);
+
+    // so user is able to see the success message on the button first before the modal goes away
+    const resetTimeout = setTimeout(() => {
+      setShowRunExportModal(false);
+    }, 2000);
+
+    // so the user can't see the button text change.
+    const resetLocalStateTimeout = setTimeout(() => {
+      setIsExported(false);
+    }, 2500);
+
+    return () => {
+      clearTimeout(setLocalStateTimeout);
+      clearTimeout(resetTimeout);
+      clearTimeout(resetLocalStateTimeout);
+    };
+  };
+
+  return (
+    <div className="pipeline-run-export-modal pipeline-run-export-modal--experiment-tracking">
+      <Modal
+        closeModal={() => setShowRunExportModal(false)}
+        theme={theme}
+        title="Export experiment run"
+        visible={visible}
+      >
+        <div className="run-export-modal-button-wrapper">
+          <Button mode="secondary" onClick={() => setShowRunExportModal(false)}>
+            Cancel
+          </Button>
+          <CSVLink
+            data={exportData}
+            asyncOnClick={true}
+            onClick={updateExportData}
+            filename="run-data.csv"
+          >
+            <Button onClick={handleClick}>
+              {isExported ? (
+                <>
+                  Done <span className="success-check-mark">✅</span>
+                </>
+              ) : (
+                'Export all and close'
+              )}
+            </Button>
+          </CSVLink>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default RunExportModal;
