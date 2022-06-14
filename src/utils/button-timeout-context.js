a4caf9e06a6742dad626474d4feba7ec495224f5
huongg <huongg1409@gmail.com>
2022-06-14 23:59:35 +0100

    update description for ButtonTimeoutContext
    Signed-off-by: huongg <huongg1409@gmail.com>

--- src/utils/button-timeout-context.js
+++ src/utils/button-timeout-context.js
@@ -3,8 +3,9 @@ import React, { createContext, useState } from 'react';
 export const ButtonTimeoutContext = createContext(null);
 
 /**
- * provides a way to pass different states to button depends on whether its successful, or not
- * {@return hasNotInteracted and setHasNotInteracted} these 2 are only used for modal with editable fields
+ * Provides a way to pass different states to a button depending on whether
+ * it's successful or not.
+ * {@returns hasNotInteracted and setHasNotInteracted} these 2 are only used for modal with editable fields
  */
 export const ButtonTimeoutContextProvider = ({ children }) => {
   const [isSuccessful, setIsSuccessful] = useState(false);
