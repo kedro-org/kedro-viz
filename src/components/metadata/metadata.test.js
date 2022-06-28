34832f1057cbcee97c1851f5e4e3071c6b9c66b5
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2022-06-28 11:55:36 +0100

    added conditional

--- src/components/metadata/metadata.test.js
+++ src/components/metadata/metadata.test.js
@@ -78,7 +78,7 @@ describe('MetaData', () => {
       expect(parametersRow().find('.pipeline-metadata__value').length).toBe(20);
     });
 
-    it('when pretty name is turned off the metadata title displays the full node name', () => {
+    it('when pretty name is turned off the metadata title displays the full node names', () => {
       const props = {
         nodeId: parametersNodeId,
         mockMetadata: nodeParameters,
