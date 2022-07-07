223f79b06f2d6070df3ab8a9b174a63f90a6459d
huongg <huongg1409@gmail.com>
2022-07-07 17:15:33 +0100

    remove unused props
    Signed-off-by: huongg <huongg1409@gmail.com>

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
 import ExperimentPrimaryToolbar from '../experiment-tracking/experiment-primary-toolbar';
