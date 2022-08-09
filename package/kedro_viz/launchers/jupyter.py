7ecada7511ed71a8652688671478531f9d87afd8
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2022-08-09 12:27:01 +0100

    remove unwanted jupyter stuff-2

--- package/kedro_viz/launchers/jupyter.py
+++ package/kedro_viz/launchers/jupyter.py
@@ -110,7 +110,7 @@ def run_viz(port: int = None, line=None, local_ns=None) -> None:
         local_ns: Local namespace with local variables of the scope where the line magic is invoked.
             For more details, please visit:
             https://ipython.readthedocs.io/en/stable/config/custommagics.html
-            
+
     """
     port = port or 4141  # Default argument doesn't work in Jupyter line magic.
     port = _allocate_port(start_at=port)
