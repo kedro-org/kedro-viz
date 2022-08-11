0f73fbbbdbc9e9119b764a9a2b34b14df879cae9
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2022-08-11 10:02:07 +0100

    changes based on review
    Signed-off-by: Rashida Kanchwala <rashida.kanchwala@quantumblack.com>

--- package/kedro_viz/server.py
+++ package/kedro_viz/server.py
@@ -89,7 +89,11 @@ def run_server(
         populate_data(data_access_manager, catalog, pipelines, session_store_location)
         if save_file:
             response = get_default_response()
-            Path(save_file).write_text(response.json(indent=4))
+            try:
+                Path(save_file).write_text(response.json(indent=4, sort_keys=True)) 
+            except TypeError:
+            # Keys of incomparable types (e.g. string and int) cannot be sorted.
+                Path(save_file).write_text(response.json(indent=4, sort_keys=False))
         app = apps.create_api_app_from_project(path, autoreload)
     else:
         app = apps.create_api_app_from_file(load_file)
