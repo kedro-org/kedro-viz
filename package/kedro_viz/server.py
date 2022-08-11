9741a3e4e4eacd2de31546cf1e22198f046cbd26
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2022-08-11 17:11:41 +0100

    fix lint
    Signed-off-by: Rashida Kanchwala <rashida.kanchwala@quantumblack.com>

--- package/kedro_viz/server.py
+++ package/kedro_viz/server.py
@@ -91,9 +91,11 @@ def run_server(
             response = get_default_response()
             try:
                 Path(save_file).write_text(response.json(indent=4, sort_keys=True))
-            except TypeError: # pragma: no cover
+            except TypeError:  # pragma: no cover
                 # Keys of incomparable types (e.g. string and int) cannot be sorted.
-                Path(save_file).write_text(response.json(indent=4, sort_keys=False)) # pragma: no cover
+                Path(save_file).write_text(
+                    response.json(indent=4, sort_keys=False)
+                )  # pragma: no cover
         app = apps.create_api_app_from_project(path, autoreload)
     else:
         app = apps.create_api_app_from_file(load_file)
