cc9cf6e61fb2329a5e0f5e9d531dd808f6163a1d
huongg <huongg1409@gmail.com>
2022-10-17 23:58:57 +0100

    create shared variables in routes.config
    Signed-off-by: huongg <huongg1409@gmail.com>

--- src/routes.config.js
+++ src/routes.config.js
@@ -1,7 +1,14 @@
+export const params = {
+  focused: '?focused_id=',
+  selected: '?selected_id=',
+  expanded: '?expanded_id=',
+};
+
 export const routes = {
   flowchart: {
     main: '/',
-    focusedNode: '/?focused_id=:id',
-    selectedNode: '/?selected_id=:id',
+    focusedNode: `/${params.focused}:id`,
+    selectedNode: `/${params.selected}:id`,
+    expandedNode: `/${params.expanded}:expandedId/${params.selected}:id`,
   },
 };
