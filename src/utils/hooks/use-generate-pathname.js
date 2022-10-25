97f183b869692cb9b47fb18a61348b4eaff08a0f
huongg <huongg1409@gmail.com>
2022-10-25 10:27:51 +0100

    only get pipeline from the storage on the callback
    Signed-off-by: huongg <huongg1409@gmail.com>

--- src/utils/hooks/use-generate-pathname.js
+++ src/utils/hooks/use-generate-pathname.js
@@ -2,6 +2,11 @@ import { useCallback } from 'react';
 import { useHistory, generatePath } from 'react-router-dom';
 import { localStorageName, routes } from '../../config';
 
+const getCurrentActivePipeline = () => {
+  const localStorage = window.localStorage.getItem(localStorageName);
+  return JSON.parse(localStorage)?.pipeline?.active;
+};
+
 /**
  * To generate different pathnames based on each action
  * E.g.: click on a node, or focus on a modular pipeline
@@ -10,9 +15,6 @@ import { localStorageName, routes } from '../../config';
 export const useGeneratePathname = () => {
   const history = useHistory();
 
-  const localStorage = window.localStorage.getItem(localStorageName);
-  const activePipeline = JSON.parse(localStorage)?.pipeline?.active;
-
   const toFlowchartPage = useCallback(() => {
     const url = generatePath(routes.flowchart.main);
     history.push(url);
@@ -20,24 +22,28 @@ export const useGeneratePathname = () => {
 
   const toSelectedNode = useCallback(
     (item) => {
+      const activePipeline = getCurrentActivePipeline();
+
       const url = generatePath(routes.flowchart.selectedNode, {
         pipelineId: activePipeline,
         id: item.id,
       });
       history.push(url);
     },
-    [history, activePipeline]
+    [history]
   );
 
   const toFocusedModularPipeline = useCallback(
     (item) => {
+      const activePipeline = getCurrentActivePipeline();
+
       const url = generatePath(routes.flowchart.focusedNode, {
         pipelineId: activePipeline,
         id: item.id,
       });
       history.push(url);
     },
-    [history, activePipeline]
+    [history]
   );
 
   return {
