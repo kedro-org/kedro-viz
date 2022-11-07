b63a7c95cd8493c948de1ab4676f7ab3495876d6
huongg <huongg1409@gmail.com>
2022-11-07 21:18:01 +0700

    remove unused comment
    Signed-off-by: huongg <huongg1409@gmail.com>

--- src/components/experiment-tracking/utils/hover-state-context.js
+++ src/components/experiment-tracking/utils/hover-state-context.js
@@ -2,11 +2,6 @@ import React, { createContext, useState } from 'react';
 
 export const HoverStateContext = createContext(null);
 
-/**
- * Provides a way to pass different states to a button depending on whether
- * it's successful or not.
- * {@returns hasNotInteracted and setHasNotInteracted} these 2 are only used for modal with editable fields
- */
 export const HoverStateContextProvider = ({ children }) => {
   const [hoveredElementId, setHoveredElementId] = useState(null);
 
