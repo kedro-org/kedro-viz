5c21486eeb106b61f2230fa073bd4bb94bede586
Cvetanka <cventanka_nechevska@external.mckinsey.com>
2022-12-09 16:18:03 +0100

    First test case scenarios - wip
    Signed-off-by: Cvetanka <cventanka_nechevska@external.mckinsey.com>

---
+++ src/components/experiment-tracking/time-series/time-series.test.js
@@ -0,0 +1,10 @@
+describe('TimeSeries', () => {
+  it('renders without crashing', () => {});
+  it('constructs an svg', () => {});
+  it('draw axes', () => {});
+  it('draw metricLine', () => {});
+  it('draw runLines', () => {});
+  it('draw slectedRun with different color and symbol', () => {});
+  it('onHover highlight the line and drawn dotted horizontal line', () => {});
+  it('onHover shows tooltip', () => {});
+});
