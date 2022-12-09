f11d7df4cc086f7574f9163d237c459d7c94b0a5
Cvetanka <cventanka_nechevska@external.mckinsey.com>
2022-12-09 17:15:46 +0100

    add test for 'renders without crashing' - wip
    Signed-off-by: Cvetanka <cventanka_nechevska@external.mckinsey.com>

--- src/components/experiment-tracking/time-series/time-series.test.js
+++ src/components/experiment-tracking/time-series/time-series.test.js
@@ -1,5 +1,31 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import { TimeSeries } from './time-series';
+import { HoverStateContext } from '../utils/hover-state-context';
+import { data } from '../mock-data';
+
+const selectedRuns = ['2022-09-05T12.27.04.496Z'];
+
 describe('TimeSeries', () => {
-  it('renders without crashing', () => {});
+  it('renders without crashing', () => {
+    const mockValue = {
+      hoveredElementId: null,
+      setHoveredElementId: jest.fn(),
+    };
+
+    const width = jest
+      .spyOn(document, 'querySelector')
+      .mockImplementation(() => 100);
+
+    const wrapper = mount(
+      <HoverStateContext.Provider value={mockValue}>
+        <TimeSeries metricsData={data} selectedRuns={selectedRuns}></TimeSeries>
+      </HoverStateContext.Provider>
+    );
+
+    expect(wrapper.find('.time-series').length).toBe(1);
+  });
   it('constructs an svg', () => {});
   it('draw axes', () => {});
   it('draw metricLine', () => {});
