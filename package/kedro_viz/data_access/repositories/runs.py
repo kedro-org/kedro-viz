10057ec755530d563dc506fcec69425946c5f4c8
Rashida Kanchwala <rashida_kanchwala@mckinsey.com>
2023-03-28 14:24:57 +0100

    fixed
    Signed-off-by: Rashida Kanchwala <rashida_kanchwala@mckinsey.com>

--- package/kedro_viz/data_access/repositories/runs.py
+++ package/kedro_viz/data_access/repositories/runs.py
@@ -46,62 +46,57 @@ class RunsRepository:
     def get_all_runs(
         self, limit_amount: Optional[int] = None
     ) -> Optional[Iterable[RunModel]]:
-        all_runs = (
-            self._db_session_class()  # type: ignore
-            .query(RunModel)
-            .order_by(RunModel.id.desc())
-        )
+        with self._db_session_class() as session:  # type: ignore
+            all_runs = session.query(RunModel).order_by(RunModel.id.desc())
 
-        if limit_amount:
-            all_runs = all_runs.limit(limit_amount)
-        all_runs = all_runs.all()
+            if limit_amount:
+                all_runs = all_runs.limit(limit_amount)
+            all_runs = all_runs.all()
 
-        if all_runs:
-            self.last_run_id = all_runs[0].id
-        return all_runs
+            if all_runs:
+                self.last_run_id = all_runs[0].id
+            return all_runs
 
     @check_db_session
     def get_run_by_id(self, run_id: str) -> Optional[RunModel]:
-        return self._db_session_class().query(RunModel).get(run_id)  # type: ignore
+        with self._db_session_class() as session:  # type: ignore
+            return session.query(RunModel).get(run_id)
 
     @check_db_session
     def get_runs_by_ids(self, run_ids: List[str]) -> Optional[Iterable[RunModel]]:
-        return (
-            self._db_session_class()  # type: ignore
-            .query(RunModel)
-            .filter(RunModel.id.in_(run_ids))
-            .all()
-        )
+        with self._db_session_class() as session:  # type: ignore
+            return session.query(RunModel).filter(RunModel.id.in_(run_ids)).all()
 
     @check_db_session
     def get_user_run_details(self, run_id: str) -> Optional[UserRunDetailsModel]:
-        return (
-            self._db_session_class()  # type: ignore
-            .query(UserRunDetailsModel)
-            .filter(UserRunDetailsModel.run_id == run_id)
-            .first()
-        )
+        with self._db_session_class() as session:  # type: ignore
+            return (
+                session.query(UserRunDetailsModel)
+                .filter(UserRunDetailsModel.run_id == run_id)
+                .first()
+            )
 
     @check_db_session
     def get_new_runs(self) -> Optional[Iterable[RunModel]]:
-        query = self._db_session_class().query(RunModel)  # type: ignore
+        with self._db_session_class() as session:  # type: ignore
+            query = session.query(RunModel)
 
-        if self.last_run_id:
-            query = query.filter(RunModel.id > self.last_run_id)
+            if self.last_run_id:
+                query = query.filter(RunModel.id > self.last_run_id)
 
-        return query.order_by(RunModel.id.desc()).all()
+            return query.order_by(RunModel.id.desc()).all()
 
     @check_db_session
     def get_user_run_details_by_run_ids(
         self, run_ids: List[str]
     ) -> Optional[Dict[str, UserRunDetailsModel]]:
-        return {
-            user_run_details.run_id: user_run_details
-            for user_run_details in self._db_session_class()  # type: ignore
-            .query(UserRunDetailsModel)
-            .filter(UserRunDetailsModel.run_id.in_(run_ids))
-            .all()
-        }
+        with self._db_session_class() as session:  # type: ignore
+            return {
+                user_run_details.run_id: user_run_details
+                for user_run_details in session.query(UserRunDetailsModel)
+                .filter(UserRunDetailsModel.run_id.in_(run_ids))
+                .all()
+            }
 
     @check_db_session
     def create_or_update_user_run_details(
@@ -122,4 +117,4 @@ class RunsRepository:
                 user_run_details.title = title
                 user_run_details.bookmark = bookmark
                 user_run_details.notes = notes
-        return user_run_details
+            return user_run_details
