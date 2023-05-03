e38f13b3aa2f117792b08888e92c49928efa0c2a
huongg <huongg1409@gmail.com>
2023-05-03 11:50:27 +0100

    turn off graphiql by default
    Signed-off-by: huongg <huongg1409@gmail.com>

--- package/kedro_viz/api/graphql/router.py
+++ package/kedro_viz/api/graphql/router.py
@@ -6,6 +6,7 @@ from .schema import schema
 
 router = APIRouter()
 
-graphql_app: GraphQL = GraphQL(schema)
+# graphiql=False can be removed if you wish to use the graphiql playground locally
+graphql_app: GraphQL = GraphQL(schema, graphiql=False)
 router.add_route("/graphql", graphql_app)
 router.add_websocket_route("/graphql", graphql_app)
