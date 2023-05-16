81c4a717891b1939d15570dcad7cfb2291a22460
Antony Milne <antony.milne@quantumblack.com>
2023-05-16 10:32:42 +0100

    Add TODO
    Signed-off-by: Antony Milne <antony.milne@quantumblack.com>

--- package/kedro_viz/database.py
+++ package/kedro_viz/database.py
@@ -13,5 +13,8 @@ def make_db_session_factory(session_store_location: Path) -> sessionmaker:
     database_url = f"sqlite:///{session_store_location}"
     engine = create_engine(database_url, connect_args={"check_same_thread": False})
     session_class = sessionmaker(bind=engine)
+    # TODO: making db session factory shouldn't depend on models.
+    # So want to move the table creation elsewhere ideally.
+    # But this means returning engine as well as session class.
     Base.metadata.create_all(bind=engine)
     return session_class
