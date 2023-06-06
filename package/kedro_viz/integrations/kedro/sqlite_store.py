7e0797713b16165345531c94854f45b24be24b3e
Nok <nok.lam.chan@quantumblack.com>
2023-06-06 12:56:44 +0000

    Fix coverage
    Signed-off-by: Nok <nok.lam.chan@quantumblack.com>

--- package/kedro_viz/integrations/kedro/sqlite_store.py
+++ package/kedro_viz/integrations/kedro/sqlite_store.py
@@ -68,7 +68,7 @@ class SQLiteStore(BaseSessionStore):
                     value["branch"] = branch.name
                 except ImportError as exc:  # pragma: no cover
                     logger.warning("%s:%s", exc.__class__.__name__, exc.msg)
-                except Exception as exc:
+                except Exception as exc:  # pragma: no cover
                     logger.warning("Something went wrong when fetching git metadata.")
                     logger.warning(exc)
 
