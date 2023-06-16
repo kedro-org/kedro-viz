090d09dcb39b4e723dff46688e7ea4e730eeae40
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2023-06-16 16:31:01 -0500

    simplify e2e test execution script

--- cypress/support/cy-local.start.sh
+++ cypress/support/cy-local.start.sh
@@ -3,10 +3,17 @@
 # This file is used to run e2e tests on Kedro-Viz locally. 
 # Pre-requisite: All the Kedro-Viz dependencies need to be installed. You can find more info at https://github.com/kedro-org/kedro-viz/blob/main/CONTRIBUTING.md
 
+# logging
 e2e_process_start_time=$(date +%s)
 
 # Store the process IDs of the started processes
 process_ids=()
+
+# constants
+KEDRO_VIZ_FRONTEND_PORT=4141
+KEDRO_VIZ_BACKEND_PORT=4142
+KEDRO_VIZ_FRONTEND_TIME_OUT=30
+KEDRO_VIZ_BACKEND_TIME_OUT=60
 
 # Function to terminate processes
 terminate_processes() {
@@ -35,37 +42,57 @@ wait_for_service() {
   done
 }
 
-# Start Kedro-Viz Backend
-echo
-echo "Starting Kedro-Viz Backend..."
-echo
-make run &
-process_group=$(ps -o pgid $$ | tail -n 1)
-process_ids+=($!)
+# Function to check if a port is in use
+is_port_in_use() {
+  local port=$1
+  nc -z localhost $port >/dev/null 2>&1
+  return $?
+}
+
+
+# Check if Kedro-Viz Backend is already running
+if is_port_in_use $KEDRO_VIZ_BACKEND_PORT; then
+  echo "Kedro-Viz Backend is already running..."
+else
+  # Start Kedro-Viz Backend
+  echo
+  echo "Starting Kedro-Viz Backend..."
+  echo
+  make run &
+  process_group=$(ps -o pgid $$ | tail -n 1)
+  process_ids+=($!)
+fi
 
 # Wait for Kedro-Viz Backend to start
-wait_for_service "Kedro-Viz Backend" "localhost" 4142 60
+wait_for_service "Kedro-Viz Backend" "localhost" $KEDRO_VIZ_BACKEND_PORT $KEDRO_VIZ_BACKEND_TIME_OUT
 
-# Start Kedro-Viz frontend
-echo
-echo "Starting Kedro-Viz frontend..."
-echo
-npm start &
-process_group=$(ps -o pgid $$ | tail -n 1)
-process_ids+=($!)
+# Check if Kedro-Viz Frontend is already running
+if is_port_in_use $KEDRO_VIZ_FRONTEND_PORT; then
+  echo "Kedro-Viz Frontend is already running..."
+else
+  # Start Kedro-Viz frontend
+  echo
+  echo "Starting Kedro-Viz frontend..."
+  echo
+  npm start &
+  process_group=$(ps -o pgid $$ | tail -n 1)
+  process_ids+=($!)
+fi
 
 # Wait for Kedro-Viz Frontend to start
-wait_for_service "Kedro-Viz Frontend" "localhost" 4141 30
+wait_for_service "Kedro-Viz Frontend" "localhost" $KEDRO_VIZ_FRONTEND_PORT $KEDRO_VIZ_FRONTEND_TIME_OUT
 
 # Run Cypress E2E tests
-echo "Running Cypress E2E tests..."
+echo
+echo "Running Cypress E2E tests in headless mode ..."
 cypress run
 
-
+# logging
 e2e_process_end_time=$(date +%s)
 
 total_e2e_process_time=$((e2e_process_end_time - e2e_process_start_time))
 echo "Total E2E process execution time: $total_e2e_process_time seconds"
+
 # Terminate the processes
 terminate_processes
 
