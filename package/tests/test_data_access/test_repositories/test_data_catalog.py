9bc7046002ec1e2a97ab4f569d121c0102a8dcf8
Rashida Kanchwala <rashida_kanchwala@mckinsey.com>
2023-06-20 10:01:00 +0100

    wip
    Signed-off-by: Rashida Kanchwala <rashida_kanchwala@mckinsey.com>

--- package/tests/test_data_access/test_repositories/test_data_catalog.py
+++ package/tests/test_data_access/test_repositories/test_data_catalog.py
@@ -1,7 +1,7 @@
 from kedro.io.data_catalog import DataCatalog
 
 from kedro_viz.data_access.repositories import CatalogRepository
-
+import pytest 
 
 class TestDataCatalogRepository:
     def test_get_layer_mapping_for_transcoded_dataset(self):
@@ -18,10 +18,11 @@ class TestDataCatalogRepository:
         assert repo.get_layer_for_dataset("cars") == "raw"
         assert repo.get_layer_for_dataset("cars@pandas") == "raw"
 
-    def test_get_layer_mapping_from_metadata(self):
+
+    def test_validate_layers_error(self):
         repo = CatalogRepository()
         catalog_config = {
-            "cars": {
+            "car@pandas1": {
                 "type": "pandas.CSVDataSet",
                 "filepath": "cars.csv",
                 "metadata": {
@@ -30,10 +31,38 @@ class TestDataCatalogRepository:
                     },
                 },
             },
+            "car@pandas2": {
+                "type": "pandas.ParquetDataSet",
+                "filepath": "cars.pq",
+                "metadata": {
+                    "kedro-viz": {
+                        "layer": "intermediate",
+                    },
+                },
+            },
         }
         catalog = DataCatalog.from_config(catalog_config)
         repo.set_catalog(catalog)
-        assert repo.get_layer_for_dataset("cars") == "raw"
+        with pytest.raises(ValueError, match=r"Transcoded datasets should have the same layer. Mismatch found for"):
+           repo.get_layer_for_dataset("cars@pandas1")
+
+    def test_get_layer_mapping_from_metadata(self):
+        repo = CatalogRepository()
+        catalog_config = {
+            "car@pandas1": {
+                "type": "pandas.CSVDataSet",
+                "filepath": "cars.csv",
+                "metadata": {
+                    "kedro-viz": {
+                        "layer": "raw",
+                    },
+                },
+            },
+            
+        }
+        catalog = DataCatalog.from_config(catalog_config)
+        repo.set_catalog(catalog)
+        assert repo.get_layer_for_dataset("car") == "raw"
 
     # TODO : Update test after Kedro 19 release.
     def test_get_layer_mapping_from_metadata_and_top_level_layer(self):
