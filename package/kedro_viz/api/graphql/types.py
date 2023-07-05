204b91650fedad09016b665bc4ebb52251ac794e
Rashida Kanchwala <rashida_kanchwala@mckinsey.com>
2023-07-05 15:22:27 +0100

    fix tests final
    Signed-off-by: Rashida Kanchwala <rashida_kanchwala@mckinsey.com>

--- package/kedro_viz/api/graphql/types.py
+++ package/kedro_viz/api/graphql/types.py
@@ -14,10 +14,10 @@ from kedro_viz.models.experiment_tracking import (
     TrackingDatasetGroup as TrackingDatasetGroupModel,
 )
 
-if sys.version_info >= (3, 9):  # pragma: no cover
-    from typing import Annotated
+if sys.version_info >= (3, 9):
+    from typing import Annotated  # pragma: no cover
 else:
-    from typing_extensions import Annotated
+    from typing_extensions import Annotated  # pragma: no cover
 
 
 @strawberry.type(description="Run metadata")
