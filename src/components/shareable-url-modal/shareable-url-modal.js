462f44746a7425dd8aa63c49a0eea2ae5131e10e
Tynan DeBold <thdebold@gmail.com>
2023-08-15 11:16:05 +0100

    Update single file to S3 via file input
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>

--- src/components/shareable-url-modal/shareable-url-modal.js
+++ src/components/shareable-url-modal/shareable-url-modal.js
@@ -1,23 +1,55 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import Modal from '../ui/modal';
+import { toggleShareableUrlModal } from '../../actions';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
 import Button from '../ui/button';
 import Input from '../ui/input';
-import { toggleShareableUrlModal } from '../../actions';
+import Modal from '../ui/modal';
 
 import './shareable-url-modal.css';
 
 const ShareableUrlModal = ({ onToggle, visible }) => {
-  const [valuesToUpdate, setValuesToUpdate] = useState({});
+  const [inputValues, setInputValues] = useState({});
   const [hasNotInteracted, setHasNotInteracted] = useState(true);
 
   const onChange = (key, value) => {
-    setValuesToUpdate(
-      Object.assign({}, valuesToUpdate, {
+    setHasNotInteracted(false);
+    setInputValues(
+      Object.assign({}, inputValues, {
         [key]: value,
       })
     );
-    setHasNotInteracted(false);
+  };
+
+  const handleFileUpload = async (file) => {
+    const s3Client = new S3Client({
+      region: inputValues.awsRegion,
+      credentials: {
+        accessKeyId: inputValues.accessKey,
+        secretAccessKey: inputValues.secretAccessKey,
+      },
+    });
+
+    try {
+      const params = {
+        Bucket: inputValues.bucketName,
+        Key: file.name,
+        Body: file,
+      };
+
+      await s3Client.send(new PutObjectCommand(params));
+      console.log('File uploaded successfully.');
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
+
+  const handleFileInput = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      handleFileUpload(selectedFile);
+    }
   };
 
   return (
@@ -28,6 +60,24 @@ const ShareableUrlModal = ({ onToggle, visible }) => {
       title="Deploy and Share"
       visible={visible.shareableUrlModal}
     >
+      <div className="shareable-url-modal__input-wrapper">
+        <div className="shareable-url-modal__input-label">AWS Region</div>
+        <Input
+          onChange={(value) => onChange('awsRegion', value)}
+          placeholder="Enter details"
+          resetValueTrigger={visible}
+          size="large"
+        />
+      </div>
+      <div className="shareable-url-modal__input-wrapper">
+        <div className="shareable-url-modal__input-label">Bucket Name</div>
+        <Input
+          onChange={(value) => onChange('bucketName', value)}
+          placeholder="Enter details"
+          resetValueTrigger={visible}
+          size="large"
+        />
+      </div>
       <div className="shareable-url-modal__input-wrapper">
         <div className="shareable-url-modal__input-label">Access Key ID</div>
         <Input
@@ -52,6 +102,7 @@ const ShareableUrlModal = ({ onToggle, visible }) => {
         <Button mode="secondary" onClick={() => onToggle(false)} size="small">
           Cancel
         </Button>
+        <input type="file" accept="*" onChange={handleFileInput} />
         <Button disabled={hasNotInteracted} size="small">
           Deploy
         </Button>
