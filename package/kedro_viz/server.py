750014b3bea4e1c6447d9b0f54b1ae309cea4e7f
Pierre Godard <pierre.godard@protonmail.com>
2023-08-17 17:23:17 +0200

    Reorganize imports
    Signed-off-by: Pierre Godard <pierre.godard@protonmail.com>

--- package/kedro_viz/server.py
+++ package/kedro_viz/server.py
@@ -9,6 +9,7 @@ from fastapi.encoders import jsonable_encoder
 from kedro.framework.session.store import BaseSessionStore
 from kedro.io import DataCatalog
 from kedro.pipeline import Pipeline
+from watchgod import run_process
 from kedro_viz.api import apps
 from kedro_viz.api.rest.responses import EnhancedORJSONResponse, get_default_response
 from kedro_viz.constants import DEFAULT_HOST, DEFAULT_PORT
@@ -16,7 +17,6 @@ from kedro_viz.data_access import DataAccessManager, data_access_manager
 from kedro_viz.database import make_db_session_factory
 from kedro_viz.integrations.kedro import data_loader as kedro_data_loader
 from kedro_viz.integrations.kedro.sqlite_store import SQLiteStore
-from watchgod import run_process
 
 try:
     # kedro 0.18.11 onwards
@@ -24,6 +24,7 @@ try:
 except ImportError:  # pragma: no cover
     # older versions
     from kedro.io.core import DataSetNotFoundError as DatasetNotFoundError
+
 
 DEV_PORT = 4142
 
