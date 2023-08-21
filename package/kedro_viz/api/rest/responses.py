d044bd1f1a5d62c2219cc389a88871df7bb91aad
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2023-08-21 21:36:06 +0100

    fixing stuff

--- package/kedro_viz/api/rest/responses.py
+++ package/kedro_viz/api/rest/responses.py
@@ -357,38 +357,26 @@ def get_selected_pipeline_response(registered_pipeline_id: str):
 def save_api_responses_to_fs(filepath: str):
     protocol, path = get_protocol_and_path(filepath)
     remote_fs = fsspec.filesystem(protocol)
-    default_response = get_default_response()
-    jsonable_default_response = jsonable_encoder(default_response)
-    encoded_response = EnhancedORJSONResponse.encode_to_human_readable(
-        jsonable_default_response
-    )
+    
+    def encode_response(response):
+        jsonable_response = jsonable_encoder(response)
+        return EnhancedORJSONResponse.encode_to_human_readable(jsonable_response)
 
-    main_loc = f"{path}/api/main"
-    nodes_loc = f"{path}/api/nodes"
-    pipelines_loc = f"{path}/api/pipelines"
+    def write_to_file(location, data):
+        with remote_fs.open(location, "wb") as f:
+            f.write(data)
 
     if protocol == "file":
-        remote_fs.makedirs(path, exist_ok=True)
-        remote_fs.makedirs(nodes_loc, exist_ok=True)
-        remote_fs.makedirs(pipelines_loc, exist_ok=True)
+        for loc in [path, f"{path}/api/nodes", f"{path}/api/pipelines"]:
+            remote_fs.makedirs(loc, exist_ok=True)
 
-    with remote_fs.open(main_loc, "wb") as f:
-        f.write(encoded_response)
+    default_response = get_default_response()
+    write_to_file(f"{path}/api/main", encode_response(default_response))
 
     for node in data_access_manager.nodes.get_node_ids():
         node_response = get_node_metadata_response(node)
-        jsonable_node_response = jsonable_encoder(node_response)
-        encoded_response = EnhancedORJSONResponse.encode_to_human_readable(
-            jsonable_node_response
-        )
-        with remote_fs.open(f"{nodes_loc}/{node}", "wb") as f:
-            f.write(encoded_response)
+        write_to_file(f"{path}/api/nodes/{node}", encode_response(node_response))
 
     for pipeline in data_access_manager.registered_pipelines.get_pipeline_ids():
         pipeline_response = get_selected_pipeline_response(pipeline)
-        jsonable_pipeline_response = jsonable_encoder(pipeline_response)
-        encoded_response = EnhancedORJSONResponse.encode_to_human_readable(
-            jsonable_pipeline_response
-        )
-        with remote_fs.open(f"{pipelines_loc}/{pipeline}", "wb") as f:
-            f.write(encoded_response)
+        write_to_file(f"{path}/api/pipelines/{pipeline}", encode_response(pipeline_response))
