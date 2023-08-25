a00931bbe7830597f1a6fbdea761c8157dbc4c4f
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2023-08-25 18:03:39 -0500

    fix lint and format errors

--- package/kedro_viz/__init__.py
+++ package/kedro_viz/__init__.py
@@ -4,10 +4,9 @@ import warnings
 
 __version__ = "6.4.0"
 
+
 class KedroVizPythonVersionWarning(UserWarning):
     """Custom class for warnings about incompatibilities with Python versions."""
-
-    pass
 
 
 if not sys.warnoptions:
@@ -15,6 +14,7 @@ if not sys.warnoptions:
 
 if sys.version_info >= (3, 11):
     warnings.warn(
-        """Please be advised that Kedro Viz is not yet fully compatible with the Python version you are currently using.""",
+        """Please be advised that Kedro Viz is not yet fully
+        compatible with the Python version you are currently using.""",
         KedroVizPythonVersionWarning,
     )
--- package/tests/test_import.py
+++ package/tests/test_import.py
@@ -1,5 +1,7 @@
 import pytest
+
 import kedro_viz
+
 
 def test_import_kedro_viz_with_no_official_support_emits_warning(mocker):
     """Test importing kedro Viz with python>=3.11 and controlled warnings should work"""
@@ -10,4 +12,8 @@ def test_import_kedro_viz_with_no_official_support_emits_warning(mocker):
         kedro_viz.__loader__.exec_module(kedro_viz)
 
     assert len(record) == 1
-    assert "Please be advised that Kedro Viz is not yet fully compatible with the Python version you are currently using." in record[0].message.args[0]
+    assert (
+        """Please be advised that Kedro Viz is not yet fully
+        compatible with the Python version you are currently using."""
+        in record[0].message.args[0]
+    )
