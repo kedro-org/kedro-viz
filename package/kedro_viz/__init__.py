54d196d97978d7d91156842665ca7f2aed2360a1
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2023-08-29 11:26:41 -0500

    fix PR comments

--- package/kedro_viz/__init__.py
+++ package/kedro_viz/__init__.py
@@ -9,7 +9,7 @@ class KedroVizPythonVersionWarning(UserWarning):
     """Custom class for warnings about incompatibilities with Python versions."""
 
 
-if sys.version_info >= (3, 11):
+if sys.version_info >= (3, 12):
     warnings.warn(
         """Please be advised that Kedro Viz is not yet fully
         compatible with the Python version you are currently using.""",
