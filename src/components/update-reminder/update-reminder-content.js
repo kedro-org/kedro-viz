0f9fe54a3688415008205150a849dcb8d6674ea9
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2023-08-31 13:46:05 +0100

    update-reminder-content

--- src/components/update-reminder/update-reminder-content.js
+++ src/components/update-reminder/update-reminder-content.js
@@ -1,6 +1,13 @@
 export const updateContent = {
-  date: '18 August 2023',
+  date: '31 August 2023',
   features: [
+    {
+      title: 'Support for Python 3.11',
+      image: '',
+      copy: 'Kedro-Viz now supports Python 3.11, and we have removed upper bounds on Python versions. Please note that we will soon discontinue support for Python 3.7. If you are using Python 3.7, we recommend upgrading to Python 3.8 or newer.',
+      buttonLink: '',
+      buttonText: '',
+    },
     {
       title: 'Feature hints',
       image:
