1bba3d4d042f53000206015b58936defa12a5c98
Tynan DeBold <thdebold@gmail.com>
2023-09-14 14:09:05 +0100

    Style updates; sending updated args to the backend
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>

--- src/components/shareable-url-modal/shareable-url-modal.js
+++ src/components/shareable-url-modal/shareable-url-modal.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { toggleShareableUrlModal } from '../../actions';
+import modifiers from '../../utils/modifiers';
 
 import Button from '../ui/button';
+import CopyIcon from '../icons/copy';
+import IconButton from '../ui/icon-button';
 import Input from '../ui/input';
 import LoadingIcon from '../icons/loading';
 import Modal from '../ui/modal';
 
-import './shareable-url-modal.css';
+import './shareable-url-modal.scss';
 
 const modalMessages = {
   default:
@@ -15,7 +18,7 @@ const modalMessages = {
   failure: 'Something went wrong. Please try again later.',
   loading: 'Shooting your files through space. Sit tight...',
   success:
-    'The pipeline has successfully been deployed and the visualisation is hosted via the link below.',
+    'The current version of Kedro-Viz has been deployed and hosted via the link below.',
 };
 
 const ShareableUrlModal = ({ onToggle, visible }) => {
@@ -24,6 +27,7 @@ const ShareableUrlModal = ({ onToggle, visible }) => {
   const [hasNotInteracted, setHasNotInteracted] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [responseUrl, setResponseUrl] = useState(null);
+  const [showCopied, setShowCopied] = useState(false);
 
   const onChange = (key, value) => {
     setHasNotInteracted(false);
@@ -63,15 +67,10 @@ const ShareableUrlModal = ({ onToggle, visible }) => {
     }
   };
 
-  const copyToClipboard = (str) => {
-    if (navigator && navigator.clipboard && navigator.clipboard.writeText) {
-      return navigator.clipboard.writeText(str).then(
-        () => {},
-        (reason) => {
-          console.error("Couldn't copy the link to the clipboard: " + reason);
-        }
-      );
-    }
+  const onCopyClick = () => {
+    window.navigator.clipboard.writeText(responseUrl);
+    setShowCopied(true);
+    setTimeout(() => setShowCopied(false), 1500);
   };
 
   const handleModalClose = () => {
@@ -86,7 +85,11 @@ const ShareableUrlModal = ({ onToggle, visible }) => {
       className="shareable-url-modal"
       closeModal={() => handleModalClose()}
       message={modalMessages[deploymentState]}
-      title="Deploy and Share"
+      title={
+        deploymentState === 'success'
+          ? 'Kedro-Viz Hosted Link'
+          : 'Deploy and Share'
+      }
       visible={visible.shareableUrlModal}
     >
       {!isLoading && !responseUrl ? (
@@ -96,7 +99,7 @@ const ShareableUrlModal = ({ onToggle, visible }) => {
               AWS Bucket Region
             </div>
             <Input
-              onChange={(value) => onChange('awsRegion', value)}
+              onChange={(value) => onChange('region', value)}
               placeholder="Enter details"
               resetValueTrigger={visible}
               size="large"
@@ -105,13 +108,13 @@ const ShareableUrlModal = ({ onToggle, visible }) => {
           <div className="shareable-url-modal__input-wrapper">
             <div className="shareable-url-modal__input-label">Bucket Name</div>
             <Input
-              onChange={(value) => onChange('bucketName', value)}
+              onChange={(value) => onChange('bucket_name', value)}
               placeholder="Enter details"
               resetValueTrigger={visible}
               size="large"
             />
           </div>
-          <div className="shareable-url-modal__button-wrapper">
+          <div className="shareable-url-modal__button-wrapper shareable-url-modal__button-wrapper--right">
             <Button
               mode="secondary"
               onClick={() => handleModalClose()}
@@ -137,15 +140,50 @@ const ShareableUrlModal = ({ onToggle, visible }) => {
       {responseUrl ? (
         <>
           <div className="shareable-url-modal__result">
-            <div>Hosted link</div>
-            <a href={responseUrl} target="_blank" rel="noopener noreferrer">
-              {responseUrl}
-            </a>
+            <div className="shareable-url-modal__label">Hosted link</div>
+            <div className="shareable-url-modal__url-wrapper">
+              <a
+                className={modifiers('shareable-url-modal__result-url', {
+                  visible: !showCopied,
+                })}
+                href={responseUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {responseUrl}
+              </a>
+              {window.navigator.clipboard && (
+                <>
+                  <span
+                    className={modifiers('copy-message', {
+                      visible: showCopied,
+                    })}
+                  >
+                    Copied to clipboard.
+                  </span>
+                  <IconButton
+                    ariaLabel="Copy run command to clipboard."
+                    className="copy-button"
+                    dataHeapEvent={`clicked.run_command`}
+                    icon={CopyIcon}
+                    onClick={onCopyClick}
+                  />
+                </>
+              )}
+            </div>
           </div>
-          <Button onClick={() => copyToClipboard(responseUrl)} size="small">
-            Copy link
-          </Button>
-          <div className="shareable-url-modal__button-wrapper shareable-url-modal__button-wrapper--single">
+          <div className="shareable-url-modal__button-wrapper ">
+            <Button
+              mode="secondary"
+              onClick={() => {
+                setDeploymentState('default');
+                setIsLoading(false);
+                setResponseUrl(null);
+              }}
+              size="small"
+            >
+              Link Settings
+            </Button>
             <Button
               mode="secondary"
               onClick={() => handleModalClose()}
