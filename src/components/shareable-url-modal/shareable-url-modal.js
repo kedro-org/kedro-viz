79e33668a908b3a0cd8bd9f27b7f54ce0f6e6214
Tynan DeBold <thdebold@gmail.com>
2023-09-19 12:32:51 +0100

    Consume compatibility endpoint; use Dropdown for bucket regions
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>

--- src/components/shareable-url-modal/shareable-url-modal.js
+++ src/components/shareable-url-modal/shareable-url-modal.js
@@ -1,50 +1,109 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import classnames from 'classnames';
 import { toggleShareableUrlModal } from '../../actions';
 import modifiers from '../../utils/modifiers';
+import { isRunningLocally } from '../../utils';
 
 import Button from '../ui/button';
 import CopyIcon from '../icons/copy';
+import Dropdown from '../ui/dropdown';
 import IconButton from '../ui/icon-button';
 import Input from '../ui/input';
 import LoadingIcon from '../icons/loading';
 import Modal from '../ui/modal';
+import MenuOption from '../ui/menu-option';
 
 import './shareable-url-modal.scss';
 
-const modalMessages = {
-  default:
-    'Please enter your AWS information and a hosted link will be generated.',
-  failure: 'Something went wrong. Please try again later.',
-  loading: 'Shooting your files through space. Sit tight...',
-  success:
-    'The current version of Kedro-Viz has been deployed and hosted via the link below.',
+const s3BucketRegions = [
+  'us-east-2',
+  'us-east-1',
+  'us-west-1',
+  'us-west-2',
+  'af-south-1',
+  'ap-east-1',
+  'ap-south-2',
+  'ap-southeast-3',
+  'ap-southeast-4',
+  'ap-south-1',
+  'ap-northeast-3',
+  'ap-northeast-2',
+  'ap-southeast-1',
+  'ap-southeast-2',
+  'ap-northeast-1',
+  'ca-central-1',
+  'cn-north-1',
+  'cn-northwest-1',
+  'eu-central-1',
+  'eu-west-1',
+  'eu-west-2',
+  'eu-south-1',
+  'eu-west-3',
+  'eu-north-1',
+  'eu-south-2',
+  'eu-central-2',
+  'sa-east-1',
+  'me-south-1',
+  'me-central-1',
+  'il-central-1',
+];
+
+const modalMessages = (status, info = '') => {
+  const messages = {
+    default:
+      'Please enter your AWS information and a hosted link will be generated.',
+    failure: 'Something went wrong. Please try again later.',
+    loading: 'Shooting your files through space. Sit tight...',
+    success:
+      'The current version of Kedro-Viz has been deployed and hosted via the link below.',
+    incompatible: `Deploying and hosting Kedro-Viz is only supported with fsspec >=2023.9.0. You are currently on version ${info}.\n\nPlease upgrade fsspec to a supported version and ensure you're using Kedro 0.18.2 or above.`,
+  };
+
+  return messages[status];
 };
 
 const ShareableUrlModal = ({ onToggle, visible }) => {
   const [deploymentState, setDeploymentState] = useState('default');
   const [inputValues, setInputValues] = useState({});
+  console.log('inputValues: ', inputValues);
   const [hasNotInteracted, setHasNotInteracted] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [responseUrl, setResponseUrl] = useState(null);
   const [showCopied, setShowCopied] = useState(false);
   const [isLinkSettingsClick, setIsLinkSettingsClick] = useState(false);
-  const [isOutdated, setIsOutdated] = useState(false);
+  const [compatibilityData, setCompatibilityData] = useState({});
+  const [canUseShareableUrls, setCanUseShareableUrls] = useState(false);
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch('/api/deploy-viz-metadata', {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-      });
-      const result = await response.json();
+    try {
+      async function fetchPackageCompatibility() {
+        const request = await fetch('/api/package_compatibilities', {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        });
+        const response = await request.json();
 
-      setIsOutdated(result.timestamp);
+        if (request.ok) {
+          setCompatibilityData(response);
+          setCanUseShareableUrls(response?.is_compatible || false);
+
+          // User's fsspec package version isn't compatible, so set
+          // the necessary state to reflect that in the UI.
+          if (!response.is_compatible) {
+            setDeploymentState(!response.is_compatible && 'incompatible');
+          }
+        }
+      }
+
+      if (isRunningLocally()) {
+        fetchPackageCompatibility();
+      }
+    } catch (error) {
+      console.log('package_compatibilities fetch error: ', error);
     }
-
-    fetchData();
   }, []);
 
   const onChange = (key, value) => {
@@ -104,34 +163,51 @@ const ShareableUrlModal = ({ onToggle, visible }) => {
     <Modal
       className="shareable-url-modal"
       closeModal={() => onToggle(false)}
-      message={modalMessages[deploymentState]}
+      message={modalMessages(
+        deploymentState,
+        compatibilityData.package_version
+      )}
       title={
         deploymentState === 'success'
           ? 'Kedro-Viz Hosted and Deployed'
-          : 'Deploy and Share'
+          : 'Deploy and Share Kedro-Viz'
       }
       visible={visible.shareableUrlModal}
     >
-      {!isLoading && !responseUrl ? (
+      {!isLoading && !responseUrl && canUseShareableUrls ? (
         <>
           <div className="shareable-url-modal__input-wrapper">
             <div className="shareable-url-modal__input-label">
               AWS Bucket Region
             </div>
-            <Input
-              defaultValue={inputValues.region}
-              onChange={(value) => onChange('region', value)}
-              placeholder="Enter details"
-              resetValueTrigger={visible}
-              size="large"
-            />
+            <Dropdown
+              defaultText={inputValues?.region || 'Choose a region...'}
+              onChanged={(selectedRegion) => {
+                onChange('region', selectedRegion.value);
+              }}
+              width={null}
+            >
+              {s3BucketRegions.map((region) => {
+                return (
+                  <MenuOption
+                    className={classnames({
+                      'pipeline-list__option--active':
+                        inputValues.region === region,
+                    })}
+                    key={region}
+                    primaryText={region}
+                    value={region}
+                  />
+                );
+              })}
+            </Dropdown>
           </div>
           <div className="shareable-url-modal__input-wrapper">
             <div className="shareable-url-modal__input-label">Bucket Name</div>
             <Input
               defaultValue={inputValues.bucket_name}
               onChange={(value) => onChange('bucket_name', value)}
-              placeholder="Enter details"
+              placeholder="s3://my-bucket-name"
               resetValueTrigger={visible}
               size="large"
             />
