2926f7f310d331fa90b684fb3baedca7e246189d
Tynan DeBold <thdebold@gmail.com>
2023-10-05 13:11:25 +0100

    Add back graphql subscription (removal moved to another PR)
    Signed-off-by: Tynan DeBold <thdebold@gmail.com>

--- src/apollo/config.js
+++ src/apollo/config.js
@@ -6,11 +6,23 @@ import {
   split,
 } from '@apollo/client';
 import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
 import { replaceMatches } from '../utils';
 
-const { pathname } = window.location;
+const { host, pathname, protocol } = window.location;
 const sanitizedPathname = replaceMatches(pathname, {
   'experiment-tracking': '',
+});
+
+const wsHost = process.env.NODE_ENV === 'development' ? 'localhost:4142' : host;
+
+const wsProtocol = protocol === 'https:' ? 'wss:' : 'ws:';
+
+const wsLink = new WebSocketLink({
+  uri: `${wsProtocol}//${wsHost}${sanitizedPathname}graphql`,
+  options: {
+    reconnect: true,
+  },
 });
 
 const httpLink = createHttpLink({
@@ -19,11 +31,18 @@ const httpLink = createHttpLink({
   fetch,
 });
 
-const splitLink = split(({ query }) => {
-  const definition = getMainDefinition(query);
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
 
-  return definition.kind === 'OperationDefinition';
-}, httpLink);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 export const client = new ApolloClient({
   connectToDevTools: true,
