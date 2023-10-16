6dc9cb2c06bcd155670c8bda2165f38c7788b11f
mehdinv <mehdinv@hotmail.com>
2023-10-16 16:57:30 +0100

    Adjusted setup.py file to incl. only test dependencies
    Signed-off-by: mehdinv <mehdinv@hotmail.com>

--- package/setup.py
+++ package/setup.py
@@ -1,53 +1,47 @@
 # type: ignore
-import json
-import os
-from os import path
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-name = "kedro-viz"
-here = path.abspath(path.dirname(__file__))
-
-
-jsbuild = [
-    os.path.join(dirpath, f)
-    for dirpath, dirnames, files in os.walk("kedro_viz/html/")
-    for f in files
+extras_require["test"] = [
+    "kedro >=0.17.0",
+    "kedro-datasets[pandas.ParquetDataSet, pandas.CSVDataSet, pandas.ExcelDataSet, plotly.JSONDataSet]>=1.0, <1.7.1",
+    "kedro-telemetry>=0.1.1",  # for testing telemetry integration
+    "bandit~=1.7",
+    "behave~=1.2",
+    "black~=23.3",
+    "boto3~=1.26",
+    "flake8~=5.0",
+    "fastapi[all]>=0.73.0, <0.96.0",
+    "isort~=5.11",
+    "matplotlib~=3.5",
+    "mypy~=1.0",
+    "moto~=4.1.14",
+    "psutil==5.9.5",  # same as Kedro for now
+    "pylint~=2.17",
+    "pytest~=7.4",
+    "pytest-asyncio~=0.21",
+    "pytest-mock~=3.11",
+    "pytest-cov~=4.1",
+    "sqlalchemy-stubs~=0.4",
+    "strawberry-graphql[cli]>=0.99.0, <1.0",
+    "trufflehog~=2.2",
+    "types-aiofiles==0.1.3",
+    "types-cachetools==0.1.6",
+    "types-click==0.1.14",
+    "types-futures==0.1.3",
+    "types-Jinja2==2.11.2",
+    "types-orjson==0.1.0",
+    "types-pkg-resources==0.1.2",
+    "types-protobuf==0.1.10",
+    "types-PyYAML==0.1.5",
+    "types-requests==0.1.8",
+    "types-toml==0.1.1",
+    "types-ujson==0.1.0",
+    "typing_extensions~=4.7.0; python_version < '3.9'",
 ]
 
-files = map(lambda x: x.replace("kedro_viz/", "", 1), jsbuild)
-
-with open(path.join(here, path.pardir, "package.json")) as data:
-    obj = json.load(data)
-    version = obj["version"]
-
-with open("requirements.txt", "r", encoding="utf-8") as f:
-    requires = [x.strip() for x in f if x.strip()]
-
-with open(path.join(here, "../README.md"), encoding="utf-8") as f:
-    readme = f.read()
-
-
 setup(
-    name=name,
-    version=version,
-    description="Kedro-Viz helps visualise Kedro data and analytics pipelines",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    license="Apache Software License (Apache 2.0)",
-    url="https://github.com/kedro-org/kedro-viz",
-    python_requires=">=3.7",
-    install_requires=requires,
-    keywords="pipelines, machine learning, data pipelines, data science, data engineering, visualisation",
-    author="Kedro",
-    packages=find_packages(exclude=["tests*", "features*"]),
-    package_data={"kedro_viz": list(files)},
-    zip_safe=False,
-    entry_points={
-        "kedro.global_commands": ["kedro-viz = kedro_viz.launchers.cli:commands"],
-        "kedro.line_magic": ["line_magic = kedro_viz.launchers.jupyter:run_viz"],
-        "kedro.hooks": [
-            "kedro-dataset-stats = kedro_viz.integrations.kedro.hooks:dataset_stats_hook"
-        ],
+    package_data={
+        "kedro-viz"
     },
-)
+    extras_require=extras_require,
+)x
