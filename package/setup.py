1a286925aa38431a71e652ee2b15bc88c04712e5
mehdinv <mehdinv@hotmail.com>
2023-10-17 17:19:49 +0100

    Add correct bundling logic for package_data param
    Signed-off-by: mehdinv <mehdinv@hotmail.com>

--- package/setup.py
+++ package/setup.py
@@ -1,8 +1,21 @@
 # type: ignore
+import json
+import os
+from os import path
+
 from setuptools import setup
 
+name = "kedro-viz"
+here = path.abspath(path.dirname(__file__))
+
+jsbuild = [
+    os.path.join(dirpath, f)
+    for dirpath, dirnames, files in os.walk("kedro_viz/html/")
+    for f in files
+]
+
+files = map(lambda x: x.replace("kedro_viz/", "", 1), jsbuild)
+
 setup(
-    package_data={
-        "kedro-viz"
-    }
+    package_data={"kedro_viz": list(files)},
 )
