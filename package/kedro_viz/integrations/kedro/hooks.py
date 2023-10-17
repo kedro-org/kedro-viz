ae0c6577ecacd86e93c3031b03305f6c33a2f9bd
Merel Theisen <merel.theisen@quantumblack.com>
2023-10-17 18:42:28 +0100

    Fix linting and coverage
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- package/kedro_viz/integrations/kedro/hooks.py
+++ package/kedro_viz/integrations/kedro/hooks.py
@@ -30,10 +30,11 @@ class DatasetStatsHook:
         Args:
             catalog: The catalog that was created.
         """
+        # Temporary try/except block so the Kedro develop branch can work with Viz.
         try:
             self.datasets = catalog._data_sets
-        except:
-            self.datasets = catalog._datasets
+        except Exception:  # pragma: no cover
+            self.datasets = catalog._datasets  # type: ignore[attr-defined]
 
     @hook_impl
     def after_dataset_loaded(self, dataset_name: str, data: Any):
