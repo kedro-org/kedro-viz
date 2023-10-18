5c73b5bf28fdb62023c87c35031985ff97a9452a
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2023-10-18 08:58:16 +0200

    Add missing dependencies
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

--- package/setup.py
+++ package/setup.py
@@ -53,6 +53,8 @@ setup(
     extras_require={
         "docs": [
             "sphinx~=5.3.0",
+            "sphinx_copybutton==0.3.1",
+            "sphinx-notfound-page",
             "sphinx_rtd_theme==1.2.0",
             "myst-parser~=1.0.0",
         ],
