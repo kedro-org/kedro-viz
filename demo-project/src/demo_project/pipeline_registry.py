b285e98c3cfe8c0a9d7d2c9d85a5c6a528ee7346
Rashida Kanchwala <rashida.kanchwala@quantumblack.com>
2023-10-20 14:36:36 +0100

    test

--- demo-project/src/demo_project/pipeline_registry.py
+++ demo-project/src/demo_project/pipeline_registry.py
@@ -1,12 +1,51 @@
 """Project pipelines."""
 from typing import Dict
 
-from kedro.pipeline import Pipeline
 
 from demo_project.pipelines import data_ingestion as di
 from demo_project.pipelines import feature_engineering as fe
 from demo_project.pipelines import modelling as mod
 from demo_project.pipelines import reporting as rep
+
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+def create_pipeline(**kwargs) -> Pipeline:
+
+    sub_pipeline = pipeline(
+        [
+            node(lambda x: x,
+                 inputs="dataset_1",
+                 outputs="dataset_2",
+                 name="step2"),
+            node(lambda x: x,
+                 inputs="dataset_2",
+                 outputs="dataset_3",
+                 name="step3"),
+        ],
+        inputs={"dataset_1"},
+        outputs={"dataset_3"},
+        namespace="sub_pipeline"
+    )
+
+    new_pipeline = pipeline(
+        [
+            node(lambda x: x,
+                 inputs="dataset_in",
+                 outputs="dataset_1",
+                 name="step1"),
+            sub_pipeline,
+            node(lambda x: x,
+                 inputs="dataset_3",
+                 outputs="dataset_out",
+                 name="step4"
+            )
+        ],
+        namespace="main_pipeline",
+        inputs=None,
+        outputs={"dataset_out"}
+    )
+    return new_pipeline
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -26,16 +65,21 @@ def register_pipelines() -> Dict[str, Pipeline]:
 
     reporting_pipeline = rep.create_pipeline()
 
+
     return {
-        "__default__": (
-            ingestion_pipeline
-            + feature_pipeline
-            + modelling_pipeline
-            + reporting_pipeline
-        ),
-        "Data ingestion": ingestion_pipeline,
-        "Modelling stage": modelling_pipeline,
-        "Feature engineering": feature_pipeline,
-        "Reporting stage": reporting_pipeline,
-        "Pre-modelling": ingestion_pipeline + feature_pipeline,
+        "__default__": create_pipeline()
     }
+
+    # return {
+    #     "__default__": (
+    #         ingestion_pipeline
+    #         + feature_pipeline
+    #         + modelling_pipeline
+    #         + reporting_pipeline
+    #     ),
+    #     "Data ingestion": ingestion_pipeline,
+    #     "Modelling stage": modelling_pipeline,
+    #     "Feature engineering": feature_pipeline,
+    #     "Reporting stage": reporting_pipeline,
+    #     "Pre-modelling": ingestion_pipeline + feature_pipeline,
+    # }
