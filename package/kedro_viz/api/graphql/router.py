15dc6859b8c62566dd7d19bf936941305506d430
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2023-11-02 19:17:06 +0000

    Lint error fix

--- package/kedro_viz/api/graphql/router.py
+++ package/kedro_viz/api/graphql/router.py
@@ -9,6 +9,8 @@ router = APIRouter()
 # graphiql=False can be removed if you wish to use the graphiql playground locally
 graphql_app: GraphQL = GraphQL(schema, graphiql=False)
 router.add_route("/graphql", graphql_app)
-router.add_route("/{subpath:path}/graphql", graphql_app) # {subpath:path} is to handle urls with subpath e.g. demo.kedro.org/web
 router.add_websocket_route("/graphql", graphql_app)
+
+# {subpath:path} is to handle urls with subpath e.g. demo.kedro.org/web
+router.add_route("/{subpath:path}/graphql", graphql_app)
 router.add_websocket_route("/{subpath:path}/graphql", graphql_app)
