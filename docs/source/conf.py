4b8e91443a167ac132155fe4b4d9ea54b583e221
Jo Stichbury <jo_stichbury@mckinsey.com>
2023-11-10 10:31:01 +0000

    Suggested variable removal
    Signed-off-by: Jo Stichbury <jo_stichbury@mckinsey.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,8 +63,6 @@ intersphinx_mapping = {
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-here = Path(__file__).parent.absolute()
-
 
 # Theme options are theme-specific and customise the look and feel of a theme
 # further.  For a list of options available for each theme, see the
