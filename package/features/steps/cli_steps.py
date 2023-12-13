911f003b67ecb470451234d5cd6ed6d07f891f11
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2023-12-12 22:39:18 -0600

    adjust requirements path
    Signed-off-by: ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>

--- package/features/steps/cli_steps.py
+++ package/features/steps/cli_steps.py
@@ -76,8 +76,8 @@ def create_project_with_starter(context, starter):
 
 @given("I have installed the project's requirements")
 def install_project_requirements(context):
-    """Run ``pip install -r src/requirements.txt``."""
-    requirements_path = str(context.root_project_dir) + "/src/requirements.txt"
+    """Run ``pip install -r requirements.txt``."""
+    requirements_path = str(context.root_project_dir) + "/requirements.txt"
     cmd = [context.pip, "install", "-r", requirements_path]
     res = run(cmd, env=context.env)
 
