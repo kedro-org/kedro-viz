2984d4dc3206709688efb334262e048233204fbe
rashidakanchwala <rashida_kanchwala@mckinsey.com>
2023-12-18 13:05:25 +0000

    update highlights

--- src/components/update-reminder/update-reminder-content.js
+++ src/components/update-reminder/update-reminder-content.js
@@ -1,19 +1,21 @@
 export const updateContent = {
-  date: '16 November 2023',
+  date: '18 December 2023',
   features: [
     {
-      title: 'Deprecation warning for Kedro-Viz',
+      title: 'Update CLI command `kedro viz run` which starts Kedro-viz',
       image: '',
-      copy: 'The `kedro viz` command will be deprecated with the release of Kedro-Viz 7.0.0. `kedro viz run` will be the new way to start Kedro Viz',
-      buttonLink: '',
-      buttonText: '',
+      copy: '`kedro viz run` is the new way to start Kedro Viz. The old command `kedro viz` is no longer supported',
+      buttonLink:
+        'https://docs.kedro.org/projects/kedro-viz/en/latest/share_kedro_viz.html',
+      buttonText: 'View the docs',
     },
     {
-      title: 'Fixed Kedro-Viz when hosted via URL Subpaths',
+      title: 'Deploy Kedro-viz to AWS using cli `kedro viz deploy`',
       image: '',
-      copy: 'Kedro-Viz now works properly when hosted via a URL subpath.',
-      buttonLink: '',
-      buttonText: '',
+      copy: 'Kedro-Viz can now be deployed directly to AWS by using the command `kedro viz deploy`',
+      buttonLink:
+        'https://docs.kedro.org/projects/kedro-viz/en/latest/kedro-viz_visualisation.html',
+      buttonText: 'View the docs',
     },
   ],
 };
