71cad5165ef94db9b5666988c95679f5751ed1e7
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2024-01-04 19:23:22 +0530

    add mypy ignore for entire file
    Signed-off-by: ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>

--- package/kedro_viz/api/graphql/router.py
+++ package/kedro_viz/api/graphql/router.py
@@ -1,4 +1,5 @@
 """`kedro_viz.api.graphql.router` defines GraphQL routes."""
+# mypy: ignore-errors
 from fastapi import APIRouter
 from strawberry.asgi import GraphQL
 
@@ -8,9 +9,9 @@ router = APIRouter()
 
 # graphiql=False can be removed if you wish to use the graphiql playground locally
 graphql_app: GraphQL = GraphQL(schema, graphiql=False)
-router.add_route("/graphql", graphql_app)  # type: ignore
-router.add_websocket_route("/graphql", graphql_app)  # type: ignore
+router.add_route("/graphql", graphql_app)
+router.add_websocket_route("/graphql", graphql_app)
 
 # {subpath:path} is to handle urls with subpath e.g. demo.kedro.org/web
-router.add_route("/{subpath:path}/graphql", graphql_app)  # type: ignore
-router.add_websocket_route("/{subpath:path}/graphql", graphql_app)  # type: ignore
+router.add_route("/{subpath:path}/graphql", graphql_app)
+router.add_websocket_route("/{subpath:path}/graphql", graphql_app)
