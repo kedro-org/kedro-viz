ecb6a0586ad67ffb6c3cf498b35eac48623bbb55
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2024-01-15 10:17:32 +0530

    remove circular dependency
    Signed-off-by: ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>

--- package/kedro_viz/integrations/deployment/__init__.py
+++ package/kedro_viz/integrations/deployment/__init__.py
@@ -1,12 +1,2 @@
 """`kedro_viz.integrations.deployment` provides interface to
 integrate Kedro viz with deployers."""
-
-# from .base_deployer import BaseDeployer
-# from .deployer_factory import DeployerFactory
-# from .local_deployer import LocalDeployer
-# from .s3_deployer import S3Deployer
-
-# # Import other deployers as needed
-
-# # List the classes you want to make available when using `from deployment import *`
-# __all__ = ["BaseDeployer", "LocalDeployer", "S3Deployer", "DeployerFactory"]
