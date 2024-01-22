d39f75672165794d82bad4355498f569e8fc1d58
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2024-01-22 11:38:29 +0000

    Doc link updated

--- src/components/update-reminder/update-reminder-content.js
+++ src/components/update-reminder/update-reminder-content.js
@@ -2,11 +2,12 @@ export const updateContent = {
   date: '22 January 2024',
   features: [
     {
-      title: 'Build Kedro-viz using cli `kedro viz build`',
+      title:
+        'Publish Kedro-viz on any hosting platform using cli `kedro viz build`',
       image: '',
       copy: '`kedro viz build` command that enables you to publish and share Kedro-Viz to any static website hosting platform.',
       buttonLink:
-        'https://docs.kedro.org/projects/kedro-viz/en/latest/share_kedro_viz.html',
+        'https://docs.kedro.org/projects/kedro-viz/en/latest/share_kedro_viz.html#platform-agnostic-sharing-with-kedro-viz',
       buttonText: 'View the docs',
     },
   ],
