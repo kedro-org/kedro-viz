a860080407409e24449c14212ed3f527460e39ef
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2024-01-24 16:16:38 +0530

    fix lint
    Signed-off-by: ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>

--- package/tests/test_integrations/test_deployer_factory.py
+++ package/tests/test_integrations/test_deployer_factory.py
@@ -1,5 +1,4 @@
 import re
-from kedro_viz.integrations.deployment.gcp_deployer import GCPDeployer
 
 import pytest
 
@@ -7,6 +6,7 @@ from kedro_viz.constants import SHAREABLEVIZ_SUPPORTED_PLATFORMS
 from kedro_viz.integrations.deployment.aws_deployer import AWSDeployer
 from kedro_viz.integrations.deployment.azure_deployer import AzureDeployer
 from kedro_viz.integrations.deployment.deployer_factory import DeployerFactory
+from kedro_viz.integrations.deployment.gcp_deployer import GCPDeployer
 from kedro_viz.integrations.deployment.local_deployer import LocalDeployer
 
 
