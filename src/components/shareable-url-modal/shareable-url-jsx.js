3debad86585a48f58d7bb22357156aa678e042a4
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2024-02-21 20:51:35 +0000

    Disclaimer note added and shareable-url-modal component refactored by moving major jsx to other file
    Signed-off-by: <>

---
+++ src/components/shareable-url-modal/shareable-url-jsx.js
@@ -0,0 +1,208 @@
+import Button from '../ui/button';
+import CopyIcon from '../icons/copy';
+import IconButton from '../ui/icon-button';
+import LoadingIcon from '../icons/loading';
+import Tooltip from '../ui/tooltip';
+import { KEDRO_VIZ_DOCS_URL, KEDRO_VIZ_PUBLISH_URL } from '../../config';
+
+/**
+ * Renders a compatibility message based on the availability of shareable URLs.
+ * @param {boolean} canUseShareableUrls - Indicates whether shareable URLs can be used.
+ * @param {function} handleModalClose - The function to handle the modal close event.
+ * @returns {JSX.Element|null} - The rendered compatibility message or null if shareable URLs can be used.
+ */
+export const renderCompatibilityMessage = (
+  canUseShareableUrls,
+  handleModalClose
+) => {
+  return !canUseShareableUrls ? (
+    <div className="shareable-url-modal__button-wrapper shareable-url-modal__button-wrapper--right">
+      <Button mode="secondary" onClick={() => handleModalClose()} size="small">
+        Cancel
+      </Button>
+      <a
+        href="https://docs.kedro.org/en/latest/visualisation/share_kedro_viz.html"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <Button size="small">View documentation</Button>
+      </a>
+    </div>
+  ) : null;
+};
+
+/**
+ * Renders the success content for the shareable URL modal.
+ *
+ * @param {string} responseUrl - The response URL.
+ * @param {boolean} showCopied - Indicates whether the "Copied!" tooltip should be shown.
+ * @param {function} onCopyClick - The function to be called when the copy button is clicked.
+ * @param {function} handleResponseUrl - The function to handle the response URL.
+ * @param {function} handleModalClose - The function to handle the modal close event.
+ * @param {function} handleLinkSettingsClick - The function to handle the link settings click event.
+ * @returns {JSX.Element|null} The rendered success content.
+ */
+export const renderSuccessContent = (
+  responseUrl,
+  showCopied,
+  onCopyClick,
+  handleResponseUrl,
+  handleModalClose,
+  handleLinkSettingsClick
+) => {
+  return responseUrl ? (
+    <>
+      <div className="shareable-url-modal__result">
+        <div className="shareable-url-modal__label">Hosted link</div>
+        <div className="shareable-url-modal__url-wrapper">
+          <a
+            className="shareable-url-modal__result-url"
+            href={handleResponseUrl()}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {responseUrl}
+          </a>
+          {window.navigator.clipboard && (
+            <div className="shareable-url-modal__result-action">
+              <IconButton
+                ariaLabel="Copy run command to clipboard."
+                className="copy-button"
+                dataHeapEvent={`clicked.run_command`}
+                icon={CopyIcon}
+                onClick={onCopyClick}
+              />
+              <Tooltip
+                text="Copied!"
+                visible={showCopied}
+                noDelay
+                centerArrow
+                arrowSize="small"
+              />
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="shareable-url-modal__button-wrapper ">
+        <Button
+          mode="secondary"
+          onClick={() => handleLinkSettingsClick()}
+          size="small"
+        >
+          Link Settings
+        </Button>
+        <Button
+          mode="secondary"
+          onClick={() => handleModalClose()}
+          size="small"
+        >
+          Close
+        </Button>
+      </div>
+    </>
+  ) : null;
+};
+
+/**
+ * Renders the error content for the shareable URL modal.
+ *
+ * @param {string} responseError - The error message to display.
+ * @param {function} handleGoBackClick - The function to handle the "Go back" button click.
+ * @returns {JSX.Element|null} The rendered error content or null if there is no error.
+ */
+export const renderErrorContent = (responseError, handleGoBackClick) => {
+  return responseError ? (
+    <div className="shareable-url-modal__error">
+      <p>Error message: {responseError}</p>
+      <Button mode="primary" onClick={() => handleGoBackClick()} size="small">
+        Go back
+      </Button>
+    </div>
+  ) : null;
+};
+
+/**
+ * Renders the disclaimer content for the shareable URL modal.
+ *
+ * @param {Function} clearDisclaimerMessage - The function to clear the disclaimer message.
+ * @param {Function} handleModalClose - The function to handle the modal close event.
+ * @returns {JSX.Element} The JSX element representing the disclaimer content.
+ */
+export const renderDisclaimerContent = (
+  clearDisclaimerMessage,
+  handleModalClose
+) => {
+  return (
+    <div>
+      <div className="shareable-url-modal__content-wrapper shareable-url-modal__content-description">
+        Disclaimer: Please note that Kedro-Viz contains preview data for
+        multiple datasets. If you wish to disable the preview when publishing
+        Kedro-Viz, please refer to the documentation on how to do so.
+      </div>
+      <div className="shareable-url-modal__button-wrapper shareable-url-modal__button-wrapper--right">
+        <Button
+          mode="secondary"
+          onClick={() => handleModalClose()}
+          size="small"
+        >
+          Cancel
+        </Button>
+        <Button size="small" onClick={clearDisclaimerMessage}>
+          Okay
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+/**
+ * Renders the loading content based on the isLoading flag.
+ *
+ * @param {boolean} isLoading - Flag indicating whether the content is loading or not.
+ * @returns {JSX.Element|null} - The loading content or null if isLoading is false.
+ */
+export const renderLoadingContent = (isLoading) => {
+  return isLoading ? (
+    <div className="shareable-url-modal__loading">
+      <LoadingIcon visible={isLoading} />
+    </div>
+  ) : null;
+};
+
+/**
+ * Renders the text content for the shareable URL modal.
+ * @returns {JSX.Element} The rendered JSX element.
+ */
+export const renderTextContent = () => {
+  return (
+    <div className="shareable-url-modal__content-wrapper">
+      <div className="shareable-url-modal__content-title">
+        Publish and Share Kedro-Viz
+      </div>
+      <p className="shareable-url-modal__content-description shareable-url-modal__paregraph-divider">
+        Prerequisite: Deploying and hosting Kedro-Viz requires access keys or
+        user credentials, depending on the chosen cloud provider. To use this
+        feature, please add your access keys or credentials as environment
+        variables in your Kedro project. More information can be found in{' '}
+        <a target="_blank" rel="noopener noreferrer" href={KEDRO_VIZ_DOCS_URL}>
+          docs
+        </a>
+        .
+      </p>
+      <p className="shareable-url-modal__content-description">
+        Enter the required information and a hosted link will be generated.
+      </p>
+      <p className="shareable-url-modal__content-description shareable-url-modal__content-note">
+        Find out how to obtain the endpoint link for the selected platform,{' '}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={KEDRO_VIZ_PUBLISH_URL}
+        >
+          here
+        </a>
+        .
+      </p>
+    </div>
+  );
+};
--- src/components/shareable-url-modal/shareable-url-modal.js
+++ src/components/shareable-url-modal/shareable-url-modal.js
@@ -3,22 +3,22 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
 import { toggleShareableUrlModal } from '../../actions';
+import { hostingPlatform, inputKeyToStateKeyMap } from '../../config';
+
 import {
-  hostingPlatform,
-  inputKeyToStateKeyMap,
-  KEDRO_VIZ_DOCS_URL,
-  KEDRO_VIZ_PUBLISH_URL,
-} from '../../config';
+  renderCompatibilityMessage,
+  renderSuccessContent,
+  renderErrorContent,
+  renderDisclaimerContent,
+  renderLoadingContent,
+  renderTextContent,
+} from './shareable-url-jsx';
 
 import Button from '../ui/button';
-import CopyIcon from '../icons/copy';
 import Dropdown from '../ui/dropdown';
-import IconButton from '../ui/icon-button';
 import Input from '../ui/input';
-import LoadingIcon from '../icons/loading';
 import Modal from '../ui/modal';
 import MenuOption from '../ui/menu-option';
-import Tooltip from '../ui/tooltip';
 
 import './shareable-url-modal.scss';
 
@@ -49,6 +49,7 @@ const ShareableUrlModal = ({ onToggleModal, visible }) => {
   const [isLinkSettingsClick, setIsLinkSettingsClick] = useState(false);
   const [compatibilityData, setCompatibilityData] = useState({});
   const [canUseShareableUrls, setCanUseShareableUrls] = useState(true);
+  const [isDisclaimerViewed, setIsDisclaimerViewed] = useState(false);
 
   useEffect(() => {
     async function fetchPackageCompatibility() {
@@ -141,6 +142,7 @@ const ShareableUrlModal = ({ onToggleModal, visible }) => {
     setResponseUrl(null);
     setIsLinkSettingsClick(false);
     setInputValues({});
+    setIsDisclaimerViewed(false);
     setIsFormDirty({
       hasBucketName: false,
       hasPlatform: false,
@@ -148,11 +150,6 @@ const ShareableUrlModal = ({ onToggleModal, visible }) => {
     });
   };
 
-  /**
-   * Returns the modal title/message based on the given type and deployment state.
-   * @param {string} type - The type of the modal heading.
-   * @returns {string|null} The modal title/message text or null if deployment state is 'default'.
-   */
   const getDeploymentStateByType = (type) => {
     if (deploymentState === 'default') {
       return null;
@@ -176,6 +173,105 @@ const ShareableUrlModal = ({ onToggleModal, visible }) => {
     return responseUrl;
   };
 
+  const handleLinkSettingsClick = () => {
+    setDeploymentState('default');
+    setIsLoading(false);
+    setResponseUrl(null);
+    setIsLinkSettingsClick(true);
+  };
+
+  const handleGoBackClick = () => {
+    setDeploymentState('default');
+    setIsLoading(false);
+    setResponseUrl(null);
+    setResponseError(null);
+  };
+
+  const clearDisclaimerMessage = () => setIsDisclaimerViewed(true);
+
+  const renderMainContent = () => {
+    return !isLoading &&
+      !responseUrl &&
+      canUseShareableUrls &&
+      !responseError ? (
+      <>
+        <div className="shareable-url-modal__content-form-wrapper">
+          {renderTextContent()}
+          <div className="shareable-url-modal__form-wrapper">
+            <div className="shareable-url-modal__input-wrapper">
+              <div className="shareable-url-modal__input-label">
+                Hosting platform
+              </div>
+              <Dropdown
+                defaultText={platform && hostingPlatform[platform]}
+                placeholderText={!platform ? 'Select a hosting platform' : null}
+                onChanged={(selectedPlatform) => {
+                  onChange('platform', selectedPlatform.value);
+                }}
+                width={null}
+              >
+                {Object.entries(hostingPlatform).map(([value, label]) => (
+                  <MenuOption
+                    className={classnames({
+                      'pipeline-list__option--active': platform === value,
+                    })}
+                    key={value}
+                    primaryText={label}
+                    value={value}
+                  />
+                ))}
+              </Dropdown>
+            </div>
+            <div className="shareable-url-modal__input-wrapper">
+              <div className="shareable-url-modal__input-label">
+                Bucket Name
+              </div>
+              <Input
+                defaultValue={bucket_name}
+                onChange={(value) => onChange('bucket_name', value)}
+                placeholder="Enter name"
+                resetValueTrigger={visible}
+                size="small"
+                type="input"
+                dataTest={'bucket_name'}
+              />
+            </div>
+            <div className="shareable-url-modal__input-wrapper">
+              <div className="shareable-url-modal__input-label">
+                Endpoint Link
+              </div>
+              <Input
+                defaultValue={endpoint}
+                onChange={(value) => onChange('endpoint', value)}
+                placeholder="Enter url"
+                resetValueTrigger={visible}
+                size="small"
+                type="input"
+                dataTest={'endpoint_name'}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="shareable-url-modal__button-wrapper shareable-url-modal__button-wrapper--right">
+          <Button
+            mode="secondary"
+            onClick={() => handleModalClose()}
+            size="small"
+          >
+            Cancel
+          </Button>
+          <Button
+            disabled={!Object.values(isFormDirty).every((value) => value)}
+            size="small"
+            onClick={handleSubmit}
+          >
+            {isLinkSettingsClick ? 'Republish' : 'Publish'}
+          </Button>
+        </div>
+      </>
+    ) : null;
+  };
+
   const { platform, bucket_name, endpoint } = inputValues || {};
 
   return (
@@ -189,216 +285,24 @@ const ShareableUrlModal = ({ onToggleModal, visible }) => {
       title={getDeploymentStateByType('title')}
       visible={visible.shareableUrlModal}
     >
-      {!isLoading && !responseUrl && canUseShareableUrls && !responseError ? (
+      {!isDisclaimerViewed ? (
+        renderDisclaimerContent(clearDisclaimerMessage, handleModalClose)
+      ) : (
         <>
-          <div className="shareable-url-modal__content-form-wrapper">
-            <div className="shareable-url-modal__content-wrapper">
-              <div className="shareable-url-modal__content-title">
-                Publish and Share Kedro-Viz
-              </div>
-              <p className="shareable-url-modal__content-description shareable-url-modal__paregraph-divider">
-                Prerequisite: Deploying and hosting Kedro-Viz requires access
-                keys or user credentials, depending on the chosen cloud
-                provider. To use this feature, please add your access keys or
-                credentials as environment variables in your Kedro project. More
-                information can be found in{' '}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={KEDRO_VIZ_DOCS_URL}
-                >
-                  docs
-                </a>
-                .
-              </p>
-              <p className="shareable-url-modal__content-description">
-                Enter the required information and a hosted link will be
-                generated.
-              </p>
-              <p className="shareable-url-modal__content-description shareable-url-modal__content-note">
-                Find out how to obtain the endpoint link for the selected
-                platform,{' '}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={KEDRO_VIZ_PUBLISH_URL}
-                >
-                  here
-                </a>
-                .
-              </p>
-            </div>
-            <div className="shareable-url-modal__form-wrapper">
-              <div className="shareable-url-modal__input-wrapper">
-                <div className="shareable-url-modal__input-label">
-                  Hosting platform
-                </div>
-                <Dropdown
-                  defaultText={platform && hostingPlatform[platform]}
-                  placeholderText={
-                    !platform ? 'Select a hosting platform' : null
-                  }
-                  onChanged={(selectedPlatform) => {
-                    onChange('platform', selectedPlatform.value);
-                  }}
-                  width={null}
-                >
-                  {Object.entries(hostingPlatform).map(([value, label]) => (
-                    <MenuOption
-                      className={classnames({
-                        'pipeline-list__option--active': platform === value,
-                      })}
-                      key={value}
-                      primaryText={label}
-                      value={value}
-                    />
-                  ))}
-                </Dropdown>
-              </div>
-              <div className="shareable-url-modal__input-wrapper">
-                <div className="shareable-url-modal__input-label">
-                  Bucket Name
-                </div>
-                <Input
-                  defaultValue={bucket_name}
-                  onChange={(value) => onChange('bucket_name', value)}
-                  placeholder="Enter name"
-                  resetValueTrigger={visible}
-                  size="small"
-                  type="input"
-                  dataTest={'bucket_name'}
-                />
-              </div>
-              <div className="shareable-url-modal__input-wrapper">
-                <div className="shareable-url-modal__input-label">
-                  Endpoint Link
-                </div>
-                <Input
-                  defaultValue={endpoint}
-                  onChange={(value) => onChange('endpoint', value)}
-                  placeholder="Enter url"
-                  resetValueTrigger={visible}
-                  size="small"
-                  type="input"
-                  dataTest={'endpoint_name'}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="shareable-url-modal__button-wrapper shareable-url-modal__button-wrapper--right">
-            <Button
-              mode="secondary"
-              onClick={() => handleModalClose()}
-              size="small"
-            >
-              Cancel
-            </Button>
-            <Button
-              disabled={!Object.values(isFormDirty).every((value) => value)}
-              size="small"
-              onClick={handleSubmit}
-            >
-              {isLinkSettingsClick ? 'Republish' : 'Publish'}
-            </Button>
-          </div>
+          {renderMainContent()}
+          {renderLoadingContent(isLoading)}
+          {renderErrorContent(responseError, handleGoBackClick)}
+          {renderSuccessContent(
+            responseUrl,
+            showCopied,
+            onCopyClick,
+            handleResponseUrl,
+            handleModalClose,
+            handleLinkSettingsClick
+          )}
+          {renderCompatibilityMessage(canUseShareableUrls, handleModalClose)}
         </>
-      ) : null}
-      {isLoading ? (
-        <div className="shareable-url-modal__loading">
-          <LoadingIcon visible={isLoading} />
-        </div>
-      ) : null}
-      {responseError ? (
-        <div className="shareable-url-modal__error">
-          <p>Error message: {responseError}</p>
-          <Button
-            mode="primary"
-            onClick={() => {
-              setDeploymentState('default');
-              setIsLoading(false);
-              setResponseUrl(null);
-              setResponseError(null);
-            }}
-            size="small"
-          >
-            Go back
-          </Button>
-        </div>
-      ) : null}
-      {responseUrl ? (
-        <>
-          <div className="shareable-url-modal__result">
-            <div className="shareable-url-modal__label">Hosted link</div>
-            <div className="shareable-url-modal__url-wrapper">
-              <a
-                className="shareable-url-modal__result-url"
-                href={handleResponseUrl()}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {responseUrl}
-              </a>
-              {window.navigator.clipboard && (
-                <div className="shareable-url-modal__result-action">
-                  <IconButton
-                    ariaLabel="Copy run command to clipboard."
-                    className="copy-button"
-                    dataHeapEvent={`clicked.run_command`}
-                    icon={CopyIcon}
-                    onClick={onCopyClick}
-                  />
-                  <Tooltip
-                    text="Copied!"
-                    visible={showCopied}
-                    noDelay
-                    centerArrow
-                    arrowSize="small"
-                  />
-                </div>
-              )}
-            </div>
-          </div>
-          <div className="shareable-url-modal__button-wrapper ">
-            <Button
-              mode="secondary"
-              onClick={() => {
-                setDeploymentState('default');
-                setIsLoading(false);
-                setResponseUrl(null);
-                setIsLinkSettingsClick(true);
-              }}
-              size="small"
-            >
-              Link Settings
-            </Button>
-            <Button
-              mode="secondary"
-              onClick={() => handleModalClose()}
-              size="small"
-            >
-              Close
-            </Button>
-          </div>
-        </>
-      ) : null}
-      {!canUseShareableUrls ? (
-        <div className="shareable-url-modal__button-wrapper shareable-url-modal__button-wrapper--right">
-          <Button
-            mode="secondary"
-            onClick={() => handleModalClose()}
-            size="small"
-          >
-            Cancel
-          </Button>
-          <a
-            href="https://docs.kedro.org/en/latest/visualisation/share_kedro_viz.html"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <Button size="small">View documentation</Button>
-          </a>
-        </div>
-      ) : null}
+      )}
     </Modal>
   );
 };
