d57a27f771b76bcdc2c65cbf2d7a867fbcb6b172
Sajid Alam <sajid_alam@mckinsey.com>
2024-04-25 15:45:44 +0100

    update colors from rgb to hex
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- src/config.js
+++ src/config.js
@@ -69,7 +69,7 @@ export const settings = {
   isPrettyName: {
     name: 'Pretty name',
     description: 'Display a formatted name for the kedro nodes',
-    default: false,
+    default: true,
   },
   showFeatureHints: {
     name: 'New feature hints',
