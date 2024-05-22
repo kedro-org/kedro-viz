218427ca576419b4645df8e6b751c8d566e7b24e
huongg <huongg1409@gmail.com>
2024-05-22 17:02:28 +0100

    first draft of the copy
    Signed-off-by: huongg <huongg1409@gmail.com>

--- src/components/update-reminder/update-reminder-content.js
+++ src/components/update-reminder/update-reminder-content.js
@@ -3,32 +3,20 @@ export const updateContent = {
   features: [
     {
       title: 'Add "Expand All Pipelines" toggle to the utility bar',
-      image: '',
       copy: 'This is to quickly expand or collapse all pipeline sections from the utility bar with a single click, enhancing navigation and visibility across complex pipeline structures',
-      buttonLink: '',
-      buttonText: '',
     },
     {
       title:
         'Allow Kedro-Viz commands to run from any sub directory within Kedro project',
-      image: '',
-      copy: '',
-      buttonLink: '',
-      buttonText: '',
+      copy: 'With this update, you can now run Kedro-Viz commands from any subdirectory within a Kedro project, providing greater ease of use. Additionally, two new methods, _is_project and _find_kedro_project, have been implemented. These methods are designed to address issues where the Kedro project could not be found.',
     },
     {
       title: 'Add support for JSONDataset preview',
-      image: '',
-      copy: '',
-      buttonLink: '',
-      buttonText: '',
+      copy: 'This introduces support for previewing JSONDatasets in the preview data panel',
     },
     {
       title: 'Fix bug related to tag filtering and sharing via stateful URL',
-      image: '',
       copy: '',
-      buttonLink: '',
-      buttonText: '',
     },
   ],
 };
