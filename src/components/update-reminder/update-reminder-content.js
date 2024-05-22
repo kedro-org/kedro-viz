c72883302b797324eb927ec84561e011f84fee65
huongg <huongg1409@gmail.com>
2024-05-22 19:11:51 +0100

    updae copy
    Signed-off-by: huongg <huongg1409@gmail.com>

--- src/components/update-reminder/update-reminder-content.js
+++ src/components/update-reminder/update-reminder-content.js
@@ -2,21 +2,33 @@ export const updateContent = {
   date: '22 May 2024',
   features: [
     {
-      title: 'Add "Expand All Pipelines" toggle to the utility bar',
-      copy: 'This is to quickly expand or collapse all pipeline sections from the utility bar with a single click, enhancing navigation and visibility across complex pipeline structures',
+      title: 'Toggle to expand and collapse all pipelines in the utility bar.',
+      image: '',
+      copy: 'You can now quickly expand or collapse all pipeline sections from the utility bar with a single click, enhancing navigation and visibility across complex pipeline structures.',
+      buttonLink: '',
+      buttonText: '',
     },
     {
       title:
-        'Allow Kedro-Viz commands to run from any sub directory within Kedro project',
+        'Allow Kedro-Viz commands to run from any sub directory within a Kedro project.',
+      image: '',
       copy: 'With this update, you can now run Kedro-Viz commands from any subdirectory within a Kedro project, providing greater ease of use. Additionally, two new methods, _is_project and _find_kedro_project, have been implemented. These methods are designed to address issues where the Kedro project could not be found.',
+      buttonLink: '',
+      buttonText: '',
     },
     {
       title: 'Add support for JSONDataset preview',
-      copy: 'This introduces support for previewing JSONDatasets in the preview data panel',
+      image: '',
+      copy: 'Support for previewing JSONDatasets in the metadata panel is now available',
+      buttonLink: '',
+      buttonText: '',
     },
     {
       title: 'Fix bug related to tag filtering and sharing via stateful URL',
-      copy: '',
+      image: '',
+      copy: 'With this bug fix, tag filtering and sharing via stateful URLs should now function as expected',
+      buttonLink: '',
+      buttonText: '',
     },
   ],
 };
