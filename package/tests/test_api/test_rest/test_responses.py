73394e063bcf5fb1fa98b7f4eb877c3e1482c4b5
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2024-06-05 23:24:19 -0500

    adjust pytest for responses
    Signed-off-by: ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>

--- package/tests/test_api/test_rest/test_responses.py
+++ package/tests/test_api/test_rest/test_responses.py
@@ -613,7 +613,7 @@ class TestNodeMetadataEndpoint:
         metadata = response.json()
         assert (
             metadata["code"].replace(" ", "")
-            == "defprocess_data(raw_data,train_test_split):...\n"
+            == "defprocess_data(raw_data,train_test_split):\n...\n"
         )
         assert metadata["parameters"] == {"uk.data_processing.train_test_split": 0.1}
         assert metadata["inputs"] == [
