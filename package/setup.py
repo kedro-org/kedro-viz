9d0f52043a9954874d21e5de8a8a0e58b4705f49
Huong Nguyen <huongg1409@gmail>
2024-06-24 16:54:08 +0100

    use different  kedro-sphinx-them for testing
    Signed-off-by: Huong Nguyen <huongg1409@gmail>

--- package/setup.py
+++ package/setup.py
@@ -52,7 +52,7 @@ setup(
     },
     extras_require={
         "docs": [
-            "kedro-sphinx-theme==2024.4.0",
+            "kedro-sphinx-theme @ git+https://github.com/kedro-org/kedro-sphinx-theme/tree/fix/favicon-and-active-tab",
         ],
         "aws": ["s3fs>=2021.4"],
         "azure": ["adlfs>=2021.4"],
