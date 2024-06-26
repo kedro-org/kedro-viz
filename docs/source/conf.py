c11bbc935642b902122cb52c6b253e009526dc5b
Huong Nguyen <huongg1409@gmail>
2024-06-26 16:26:52 +0100

    include favicon in kedro-viz
    Signed-off-by: Huong Nguyen <huongg1409@gmail>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,6 +69,14 @@ favicons = [
 #
 html_theme = "kedro-sphinx-theme"
 
+extensions = [
+    'sphinx_favicon',
+]
+
+favicons = [
+    "https://kedro.org/images/favicon.ico",
+]
+
 # Theme options are theme-specific and customise the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
