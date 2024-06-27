218689c92fe9f1102931a7d89096d36d2541e3e5
Huong Nguyen <huongg1409@gmail>
2024-06-27 15:00:44 +0100

    revert back to normal sphinx-theme
    Signed-off-by: Huong Nguyen <huongg1409@gmail>

--- package/setup.py
+++ package/setup.py
@@ -52,7 +52,7 @@ setup(
     },
     extras_require={
         "docs": [
-            "kedro-sphinx-theme @ git+https://github.com/kedro-org/kedro-sphinx-theme.git@fix-favicon",
+            "kedro-sphinx-theme==2024.4.0",
         ],
         "aws": ["s3fs>=2021.4"],
         "azure": ["adlfs>=2021.4"],
