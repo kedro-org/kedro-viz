16c7794b02a7da4b02f072a86d4767a5301e807b
Sajid Alam <sajid_alam@mckinsey.com>
2024-06-28 13:34:24 +0100

    Update app.test.js
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- tools/test-lib/react-app/app.test.js
+++ tools/test-lib/react-app/app.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App, { dataSources } from './app';
 
 const keys = Object.keys(dataSources).filter((key) => key !== 'random');
@@ -20,18 +20,17 @@ describe('lib-test', () => {
       .querySelector('.pipeline-nodelist__row')
       .querySelector('.pipeline-nodelist__row__text--tree')
       .querySelector('.pipeline-nodelist__row__label')
-      .textContent.trim().toLowerCase();
-
-    console.log('firstNodeName:', firstNodeName);
+      .textContent.trim();
 
     const modularPipelinesTree = dataSources[key]().modular_pipelines;
     const modularPipelineNames = Object.keys(modularPipelinesTree).map(
-      (modularPipelineID) => modularPipelinesTree[modularPipelineID].name.trim().toLowerCase()
+      (modularPipelineID) => modularPipelinesTree[modularPipelineID].name.trim()
     );
 
-    console.log('modularPipelineNames:', modularPipelineNames);
+    const expectedNodeName = modularPipelineNames.includes(firstNodeName.toLowerCase()) ? firstNodeName.toLowerCase() : firstNodeName;
+    const expectedNodeNames = modularPipelineNames.map(name => name.toLowerCase());
 
-    expect(modularPipelineNames).toContain(firstNodeName);
+    expect(expectedNodeNames).toContain(expectedNodeName.toLowerCase());
   };
 
   test.each(keys)(`uses %s dataset when provided as prop`, (key) => {
