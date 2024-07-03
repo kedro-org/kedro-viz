729872670534dfb1a816fea58432c20df9f4b1ad
Huong Nguyen <huongg1409@gmail>
2024-07-03 15:21:11 +0100

    general styling
    Signed-off-by: Huong Nguyen <huongg1409@gmail>

--- src/components/command/command.js
+++ src/components/command/command.js
@@ -1,19 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { getRunCommand, getVisibleRunCommand } from '../../selectors/command';
+import Button from '../ui/button';
+import Tooltip from '../ui/tooltip';
 import classnames from 'classnames';
 
 import './command.scss';
 
-const Command = ({ runCommand, theme, visible }) => {
+const Command = ({ runCommand, theme, visible, showSidebar }) => {
+  const [showCopied, setShowCopied] = useState(false);
+
+  const onCopyClick = () => {
+    window.navigator.clipboard.writeText(runCommand);
+    setShowCopied(true);
+
+    setTimeout(() => setShowCopied(false), 1500);
+  };
+
   return (
     <div
       className={classnames('command-wrapper', {
         'command-wrapper--visible': !visible,
+        'command-wrapper--with-sidebar': showSidebar,
       })}
     >
-      <p>Run command:</p>
-      <p>{runCommand}</p>
+      <h3 className="command-title">Run command:</h3>
+      <div className="command-copier-wrapper">
+        <div className="command-line">{runCommand}</div>
+        {/* {window.navigator.clipboard && ( */}
+        <Button
+          ariaLabel="Copy run command to clipboard."
+          dataTest={'run-command-copy-button'}
+          dataHeapEvent={`clicked.run_command`}
+          onClick={onCopyClick}
+          size="small"
+        >
+          Copy
+        </Button>
+        <Tooltip
+          text="Copied!"
+          visible={showCopied}
+          noDelay
+          centerArrow
+          arrowSize="small"
+        />
+        {/* )} */}
+      </div>
     </div>
   );
 };
@@ -22,6 +54,7 @@ export const mapStateToProps = (state, ownProps) => ({
   runCommand: getRunCommand(state),
   theme: state.theme,
   visible: getVisibleRunCommand(state),
+  showSidebar: state.visible.sidebar,
   ...ownProps,
 });
 
--- src/selectors/command.js
+++ src/selectors/command.js
@@ -1,22 +1,25 @@
 import { createSelector } from 'reselect';
 
 const getClickedNode = (state) => state.node.clicked;
+const getActivePipeline = (state) => state.pipeline.active;
 
 /**
  * Returns true if run command should be visible
  */
 export const getVisibleRunCommand = createSelector(
-  [getClickedNode],
-  (nodeClicked) => Boolean(nodeClicked)
+  [getClickedNode, getActivePipeline],
+  (nodeClicked, activePipeline) => {
+    return Boolean(nodeClicked) || Boolean(activePipeline);
+  }
 );
 
 export const getRunCommand = createSelector(
-  [getClickedNode, (state) => state.node.runCommand],
-  (nodeId, nodeRunCommand) => {
-    if (!nodeId) {
+  [getClickedNode, (state) => state.node.runCommand, getActivePipeline],
+  (nodeId, nodeRunCommand, activePipeline) => {
+    if (!nodeId && !activePipeline) {
       return null;
     }
 
-    return nodeRunCommand[nodeId];
+    return nodeRunCommand[nodeId] || `run run pipeline ${activePipeline}`;
   }
 );
