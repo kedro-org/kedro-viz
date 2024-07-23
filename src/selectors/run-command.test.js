6d7876a15a83b4754ba4d80184a4e5e65b03048e
Huong Nguyen <huongg1409@gmail>
2024-07-23 22:02:38 +0100

    test to cover the run-command selector
    Signed-off-by: Huong Nguyen <huongg1409@gmail>

---
+++ src/selectors/run-command.test.js
@@ -0,0 +1,25 @@
+import { mockState } from '../utils/state.mock';
+import reducer from '../reducers';
+
+import { FILTER_NODES } from '../actions/filters';
+import { getRunCommand } from './run-command';
+
+describe('Selectors', () => {
+  it('should generate a kedro run command with specified from and to nodes', () => {
+    const fromNodeId = '47b81aa6';
+    const toNodeId = '23c94afb';
+
+    const expectedFromNodeName = ['create_model_input_table_node'];
+    const expectedToNodeName = ['model_input_table'];
+    const expectedCommand = `kedro run --from-nodes=${expectedFromNodeName} --to-nodes=${expectedToNodeName}`;
+
+    const newState = reducer(mockState.spaceflights, {
+      type: FILTER_NODES,
+      filters: { from: fromNodeId, to: toNodeId },
+    });
+
+    const generatedCommand = getRunCommand(newState);
+
+    expect(generatedCommand).toEqual(expectedCommand);
+  });
+});
