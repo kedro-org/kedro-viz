dcebad11716feefb94f318219ab887ded87ccf82
Sajid Alam <sajid_alam@mckinsey.com>
2024-07-26 14:54:13 +0100

    reminder content with more descriptions
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- src/components/update-reminder/update-reminder-content.js
+++ src/components/update-reminder/update-reminder-content.js
@@ -1,10 +1,11 @@
 export const updateContent = {
-  date: '25 July 2024',
+  date: '29 July 2024',
   features: [
     {
-      title: 'Enable/disable dataset preview when publishing Kedro-Viz',
+      title:
+        'Enable/disable dataset preview in Kedro-Viz instance or when publishing',
       image: '',
-      copy: 'Users can now enable or disable the preview for all datasets when publishing Kedro-Viz from both the CLI and UI.',
+      copy: 'Users can now enable or disable the preview for all datasets in their local Kedro-Viz instance by using the toggle in the settings menu. Moreover, it can also be toggled when publishing Kedro-Viz from both the CLI and UI.',
       buttonLink: '',
       buttonText: '',
     },
@@ -12,21 +13,21 @@ export const updateContent = {
       title:
         'Move session store and stats file to .viz directory conditionally',
       image: '',
-      copy: 'The session store and stats file are now conditionally moved to the .viz directory.',
+      copy: 'The session store and stats file are now conditionally moved to the `.viz` directory. If the user does not provide `SESSION_STORE_ARGS` in their `settings.py` file, Kedro-Viz will use the default `.viz` directory to store session and stats files.',
       buttonLink: '',
       buttonText: '',
     },
     {
       title: 'Enhance documentation for Kedro-Viz standalone React component',
       image: '',
-      copy: 'The documentation for the Kedro-Viz standalone React component has been enhanced.',
+      copy: 'The documentation for the Kedro-Viz standalone React component has been enhanced. It now includes a detailed props explanation, UI component annotation screenshot, and a new section dedicated to using Kedro-Viz as a standalone React component.',
       buttonLink: '',
       buttonText: '',
     },
     {
       title: 'Display published URLs',
       image: '',
-      copy: 'Kedro-Viz now displays published URLs.',
+      copy: 'Kedro-Viz now displays published URLs, allowing users to easily access and share the locations where their Kedro-Viz instances are hosted. This feature stores the hosting platform, bucket name, and endpoint URL in localStorage.',
       buttonLink: '',
       buttonText: '',
     },
