4273c2654c915cebe3dcaff79fe4a15e90990098
Sajid Alam <sajid_alam@mckinsey.com>
2024-07-26 12:40:15 +0100

    Update update-reminder-content.js
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- src/components/update-reminder/update-reminder-content.js
+++ src/components/update-reminder/update-reminder-content.js
@@ -9,13 +9,6 @@ export const updateContent = {
       buttonText: '',
     },
     {
-      title: 'Display published URLs',
-      image: '',
-      copy: 'Kedro-Viz now displays published URLs.',
-      buttonLink: '',
-      buttonText: '',
-    },
-    {
       title:
         'Move session store and stats file to .viz directory conditionally',
       image: '',
@@ -24,25 +17,16 @@ export const updateContent = {
       buttonText: '',
     },
     {
-      title:
-        'Introduce `metadataPanel` in `display` prop in Kedro-Viz react component',
-      image: '',
-      copy: 'A new `metadataPanel` in the `display` prop has been introduced in the Kedro-Viz react component.',
-      buttonLink: '',
-      buttonText: '',
-    },
-    {
-      title:
-        'Expose internal Redux state through `options` prop in Kedro-Viz React component',
-      image: '',
-      copy: 'The internal Redux state is now exposed through the `options` prop while using Kedro-Viz as a React component.',
-      buttonLink: '',
-      buttonText: '',
-    },
-    {
       title: 'Enhance documentation for Kedro-Viz standalone React component',
       image: '',
       copy: 'The documentation for the Kedro-Viz standalone React component has been enhanced.',
+      buttonLink: '',
+      buttonText: '',
+    },
+    {
+      title: 'Display published URLs',
+      image: '',
+      copy: 'Kedro-Viz now displays published URLs.',
       buttonLink: '',
       buttonText: '',
     },
