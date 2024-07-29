6dfec6ec9045b1c1341c10a6acc5579ae2b9f3ca
Sajid Alam <sajid_alam@mckinsey.com>
2024-07-29 13:07:43 +0100

    changes based in review
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- src/components/update-reminder/update-reminder-content.js
+++ src/components/update-reminder/update-reminder-content.js
@@ -3,15 +3,16 @@ export const updateContent = {
   features: [
     {
       title:
-        'Enable/disable dataset preview in Kedro-Viz instance or when publishing',
+        '`Enable`/`disable` dataset preview in Kedro-Viz instance or when publishing',
       image: '',
-      copy: 'Users can now enable or disable the preview for all datasets in their local Kedro-Viz instance by using the toggle in the settings menu. Moreover, it can also be toggled when publishing Kedro-Viz from both the CLI and UI.',
-      buttonLink: '',
-      buttonText: '',
+      copy: 'Users can now `enable` or `disable` the preview for all datasets in their local Kedro-Viz instance by using the toggle in the settings menu. Moreover, it can also be toggled when publishing Kedro-Viz from both the CLI and UI.',
+      buttonLink:
+        'https://docs.kedro.org/projects/kedro-viz/en/latest/preview_datasets.html#disabling-previews',
+      buttonText: 'View the docs',
     },
     {
       title:
-        'Move session store and stats file to .viz directory conditionally',
+        'Move session store and stats file to `.viz` directory conditionally',
       image: '',
       copy: 'The session store and stats file are now conditionally moved to the `.viz` directory. If the user does not provide `SESSION_STORE_ARGS` in their `settings.py` file, Kedro-Viz will use the default `.viz` directory to store session and stats files.',
       buttonLink: '',
@@ -21,13 +22,13 @@ export const updateContent = {
       title: 'Enhance documentation for Kedro-Viz standalone React component',
       image: '',
       copy: 'The documentation for the Kedro-Viz standalone React component has been enhanced. It now includes a detailed props explanation, UI component annotation screenshot, and a new section dedicated to using Kedro-Viz as a standalone React component.',
-      buttonLink: '',
-      buttonText: '',
+      buttonLink: 'https://www.npmjs.com/package/@quantumblack/kedro-viz',
+      buttonText: 'View the docs',
     },
     {
       title: 'Display published URLs',
       image: '',
-      copy: 'Kedro-Viz now displays published URLs, allowing users to easily access and share the locations where their Kedro-Viz instances are hosted. This feature stores the hosting platform, bucket name, and endpoint URL in localStorage.',
+      copy: 'Kedro-Viz now displays published URLs, allowing users to easily access and share the locations where their Kedro-Viz instances are hosted. This feature stores the hosting platform, bucket name, and endpoint URL in `localStorage`.',
       buttonLink: '',
       buttonText: '',
     },
