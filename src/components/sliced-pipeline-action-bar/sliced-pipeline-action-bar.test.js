c7e212ca3ca98235d56957743f25b9979fc5ee72
Huong Nguyen <huongg1409@gmail>
2024-08-15 13:25:16 +0100

    include test for notification
    Signed-off-by: Huong Nguyen <huongg1409@gmail>

--- src/components/sliced-pipeline-action-bar/sliced-pipeline-action-bar.test.js
+++ src/components/sliced-pipeline-action-bar/sliced-pipeline-action-bar.test.js
@@ -4,28 +4,6 @@ import { render, screen } from '@testing-library/react';
 import { SlicedPipelineActionBar } from './sliced-pipeline-action-bar';
 
 describe('SlicedPipelineActionBar', () => {
-  it('displays notification message when notification prop is true', () => {
-    const ref = {
-      current: {
-        firstChild: { getBoundingClientRect: () => ({ width: 100 }) },
-      },
-    };
-    render(
-      <SlicedPipelineActionBar
-        chartSize={{ outerWidth: 800 }}
-        slicedPipeline={[]}
-        visibleSidebar={true}
-        notification={true}
-        displayMetadataPanel={false}
-        ref={ref}
-      />
-    );
-    const notification = screen.getByText(
-      /Hold Shift \+ Click on another node to slice pipeline/i
-    );
-    expect(notification).toBeInTheDocument();
-  });
-
   it('displays "Slice" button when slicedPipeline is not empty', () => {
     const ref = {
       current: {
