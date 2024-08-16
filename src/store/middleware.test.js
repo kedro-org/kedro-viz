8272a488cdbc7c635c50232b598a0faa31a735e0
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2024-08-16 15:00:54 +0100

    Test fix
    Signed-off-by: Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>

--- src/store/middleware.test.js
+++ src/store/middleware.test.js
@@ -18,6 +18,7 @@ describe('createCallbackMiddleware', () => {
   it('should call the callback with the correct node when action type is TOGGLE_NODE_CLICKED and node is found', () => {
     const nodeClicked = '123';
     const node = { id: nodeClicked, name: 'Node 123' };
+    const nodeClickAction = { type: 'TOGGLE_NODE_CLICKED', payload: node };
     const action = { type: 'TOGGLE_NODE_CLICKED', nodeClicked };
 
     store.getState.mockReturnValue({
@@ -28,7 +29,7 @@ describe('createCallbackMiddleware', () => {
 
     middleware(action);
 
-    expect(callback).toHaveBeenCalledWith(node);
+    expect(callback).toHaveBeenCalledWith(nodeClickAction);
     expect(next).toHaveBeenCalledWith(action);
   });
 
