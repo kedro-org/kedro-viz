423e84749d9e7040e981c3b48140d4532be78652
Huong Nguyen <huongg1409@gmail>
2024-08-22 15:14:07 +0100

    update run command to get from metadata
    Signed-off-by: Huong Nguyen <huongg1409@gmail>

--- src/selectors/run-command.js
+++ src/selectors/run-command.js
@@ -1,28 +1,19 @@
 import { createSelector } from 'reselect';
 
-const getSlicedPipeline = (state) => state.slice;
-const getNodesNames = (state) => state.node.fullName;
-const getModularPipeline = (state) => state.node.modularPipelines;
-const getNodesTypes = (state) => state.node.type;
+const getSlicedPipelineState = (state) => state.slice;
+const getNodesRunCommand = (state) => state.node.runCommand;
 
 export const getRunCommand = createSelector(
-  [getSlicedPipeline, getNodesNames, getNodesTypes],
-  (slicedPipeline, nodesNames, nodesTypes) => {
-    const { from, to } = slicedPipeline;
+  [getSlicedPipelineState, getNodesRunCommand],
+  (slicedPipelineState, nodeRunCommand) => {
+    const { from, to } = slicedPipelineState;
 
     if (!from || !to) {
       return null;
     }
 
-    const fromNodeName = nodesNames[from];
-    const toNodeName = nodesNames[to];
-    const fromNodeType = nodesTypes[from];
-    const toNodeType = nodesTypes[to];
-
-    // Determine the correct flag based on the node type
-    const fromFlag = fromNodeType === 'data' ? '--from-inputs' : '--from-nodes';
-    const toFlag = toNodeType === 'data' ? '--to-outputs' : '--to-nodes';
-
-    return `kedro run ${fromFlag}=${fromNodeName} ${toFlag}=${toNodeName}`;
+    const slicingPipelineCommand =
+      nodeRunCommand[to] || 'please define a run command for this node';
+    return slicingPipelineCommand;
   }
 );
