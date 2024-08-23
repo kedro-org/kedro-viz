a83e87894ada3028bff86e1a69392802c898867b
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2024-08-23 11:04:00 -0500

    address PR comments

--- package/kedro_viz/launchers/cli/main.py
+++ package/kedro_viz/launchers/cli/main.py
@@ -5,19 +5,13 @@ import click
 from kedro_viz.launchers.cli.lazy_default_group import LazyDefaultGroup
 
 
-@click.group(
-    name="Kedro-Viz",
-    cls=LazyDefaultGroup,
-    lazy_subcommands={
-        "viz": "kedro_viz.launchers.cli.main.viz",
-    },
-)
+@click.group(name="Kedro-Viz")
 def viz_cli():  # pylint: disable=missing-function-docstring
     pass
 
 
 @viz_cli.group(
-    name="Kedro-Viz",
+    name="viz",
     cls=LazyDefaultGroup,
     lazy_subcommands={
         "run": "kedro_viz.launchers.cli.run.run",
