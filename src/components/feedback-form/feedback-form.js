131f1381f5c4820c584fa1e70cd5deeedba28d82
Huong Nguyen <huongg1409@gmail>
2024-09-06 16:11:46 +0100

    star component
    Signed-off-by: Huong Nguyen <huongg1409@gmail>

--- src/components/feedback-form/feedback-form.js
+++ src/components/feedback-form/feedback-form.js
@@ -3,11 +3,8 @@ import Modal from '../ui/modal';
 import { Stars } from './stars';
 
 export const FeedbackForm = ({ title, onClose }) => {
-  return (
-    <div className="feedback-form-wrapper">
-      <Modal closeModal={onClose} title={title} visible={true}>
-        <Stars />
-      </Modal>
-    </div>
-  );
+            <Stars 
+                selectedRating={starNumber}
+                onClick={setStars}
+            />
 };
--- src/components/feedback-form/stars.js
+++ src/components/feedback-form/stars.js
@@ -7,27 +7,21 @@ import { List } from './list';
 import './stars.scss';
 
 export const Stars = ({
-  onClick,
-  selectedRating = -1,
-  name,
-  className,
-  size = 24,
-  isActive = true,
-  starsCount = 5,
-}) => {
-  return (
-    <ul className={'stars-wrapper'} data-element-name={name}>
-      <List
-        data={range(1, starsCount + 1).reverse()}
-        render={({ item, index }) => (
-          <IconButton
-            key={index}
-            aria-disabled={!isActive}
-            onClick={() => isActive && onClick && onClick(item)}
-            icon={StarIcon}
-          />
-        )}
-      />
-    </ul>
-  );
-};
+    onClick,
+    selectedRating = -1,
+    className,
+    starsCount = 5,
+  }) => {
+    return (
+      <section className={'stars-wrapper ' + className}>
+        {Array.from({ length: starsCount }, (_, index) => (
+            <IconButton
+             key={index} 
+             className={index < selectedRating ? 'active' : ''}
+              onClick={() => onClick && onClick(index + 1)}
+              icon={StarIcon}
+            />
+        ))}
+      </section>
+    );
+  };
