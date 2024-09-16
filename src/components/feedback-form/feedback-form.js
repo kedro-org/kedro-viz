075b03f169c009c2f3ffae0a866eb1547c8815e2
Huong Nguyen <huongg1409@gmail>
2024-09-16 11:33:32 +0100

    move mood to its own component
    Signed-off-by: Huong Nguyen <huongg1409@gmail>

--- src/components/feedback-form/feedback-form.js
+++ src/components/feedback-form/feedback-form.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classnames from 'classnames';
 import Button from '../ui/button';
 import CloseIcon from '../icons/close';
-import { Mood } from './mood';
+import { Mood } from '../mood/mood';
 
 import './feedback-form.scss';
 
