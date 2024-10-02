2f82ea8a3696e0547fec82a717020799a032059e
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2024-10-02 11:42:31 -0500

    fix lint

--- package/kedro_viz/api/apps.py
+++ package/kedro_viz/api/apps.py
@@ -1,7 +1,6 @@
 """`kedro_viz.api.app` defines the FastAPI app to serve Kedro data in a RESTful API.
 This data could either come from a real Kedro project or a file.
 """
-
 import json
 import os
 import time
@@ -43,7 +42,7 @@ def _create_base_api_app() -> FastAPI:
         @app.middleware("http")
         async def set_secure_headers(request, call_next):
             response = await call_next(request)
-            secure_headers.framework.fastapi(response)
+            secure_headers.framework.fastapi(response)  # pylint: disable=no-member
             return response
 
     return app
