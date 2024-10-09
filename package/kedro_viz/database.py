5516b5cccf90326df13214262053543f4fb50e20
Sajid Alam <sajid_alam@mckinsey.com>
2024-10-09 11:46:17 +0100

    Update database.py
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- package/kedro_viz/database.py
+++ package/kedro_viz/database.py
@@ -10,6 +10,13 @@ def make_db_session_factory(session_store_location: str) -> sessionmaker:
     """SQLAlchemy connection to a SQLite DB"""
     database_url = f"sqlite:///{session_store_location}"
     engine = create_engine(database_url, connect_args={"check_same_thread": False})
+
+    # Enable Write-Ahead Logging (WAL)
+    with engine.begin() as conn:
+        conn.execute("PRAGMA journal_mode=WAL;")
+        conn.execute("PRAGMA synchronous=NORMAL;")
+        conn.execute("PRAGMA busy_timeout=30000;")  # 30 seconds timeout for locks
+
     session_class = sessionmaker(engine)
     # TODO: making db session factory shouldn't depend on models.
     # So want to move the table creation elsewhere ideally.
