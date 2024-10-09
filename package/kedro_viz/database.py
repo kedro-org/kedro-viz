5b0def112ed2acb6e692761f3d0a6e61f46e11fa
Sajid Alam <sajid_alam@mckinsey.com>
2024-10-09 12:57:56 +0100

    lint
    Signed-off-by: Sajid Alam <sajid_alam@mckinsey.com>

--- package/kedro_viz/database.py
+++ package/kedro_viz/database.py
@@ -1,25 +1,25 @@
 """Database management layer based on SQLAlchemy"""
 
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
 import os
 
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
 from kedro_viz.models.experiment_tracking import Base
+
 
 def make_db_session_factory(session_store_location: str) -> sessionmaker:
     """SQLAlchemy connection to a SQLite DB with WAL mode enabled."""
     database_url = f"sqlite:///{session_store_location}"
-    engine = create_engine(
-        database_url,
-        connect_args={"check_same_thread": False}
-    )
+    engine = create_engine(database_url, connect_args={"check_same_thread": False})
 
     # Check if we are running in an Azure ML environment.
     is_azure_ml = any(
-        var in os.environ for var in [
+        var in os.environ
+        for var in [
             "AZUREML_ARM_SUBSCRIPTION",
             "AZUREML_ARM_RESOURCEGROUP",
-            "AZUREML_RUN_ID"
+            "AZUREML_RUN_ID",
         ]
     )
 
