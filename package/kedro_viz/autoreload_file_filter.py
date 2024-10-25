2785250ada66b97c5fb9a195cbf224fe6a43ca49
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2024-10-25 20:34:53 +0100

    Extra import removed
    Signed-off-by: Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>

--- package/kedro_viz/autoreload_file_filter.py
+++ package/kedro_viz/autoreload_file_filter.py
@@ -7,7 +7,6 @@ import logging
 from pathlib import Path
 from typing import Optional, Set
 
-import pathspec
 from pathspec import PathSpec
 from watchfiles import Change, DefaultFilter
 
@@ -40,7 +39,7 @@ class AutoreloadFileFilter(DefaultFilter):
         try:
             with open(gitignore_path, "r", encoding="utf-8") as gitignore_file:
                 ignore_patterns = gitignore_file.read().splitlines()
-            self.gitignore_spec: Optional[PathSpec] = pathspec.PathSpec.from_lines(
+            self.gitignore_spec: Optional[PathSpec] = PathSpec.from_lines(
                 "gitwildmatch", ignore_patterns
             )
         except FileNotFoundError:
