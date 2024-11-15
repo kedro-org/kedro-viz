1ab761de0c68e6d8d50c65337c52a0a2b0c78b7f
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2024-11-14 22:59:43 -0600

    add support for python 3.13

--- package/kedro_viz/__init__.py
+++ package/kedro_viz/__init__.py
@@ -10,7 +10,7 @@ class KedroVizPythonVersionWarning(UserWarning):
     """Custom class for warnings about incompatibilities with Python versions."""
 
 
-if sys.version_info >= (3, 13):
+if sys.version_info >= (3, 14):
     warnings.warn(
         """Please be advised that Kedro Viz is not yet fully
         compatible with the Python version you are currently using.""",
