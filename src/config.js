4c31689c86955721098addf990eadec836ccec0d
Huong Nguyen <huongg1409@gmail>
2024-11-19 12:29:45 +0000

    update content for lite banner
    Signed-off-by: Huong Nguyen <huongg1409@gmail>

--- src/config.js
+++ src/config.js
@@ -212,7 +212,7 @@ export const NODE_TYPES = {
 export const BANNER_METADATA = {
   liteModeWarning: {
     title: 'Missing dependencies',
-    body: 'For the best experience with full functionality, please install the missing Kedro project dependencies.',
+    body: 'Some features might be disabled in --lite mode due to missing dependencies',
     docsLink:
       'https://docs.kedro.org/projects/kedro-viz/en/latest/kedro-viz_visualisation.html#visualise-a-kedro-project-without-installing-project-dependencies',
   },
