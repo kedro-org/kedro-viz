9a17a2e07b368a50916f47ed371e50f0034fc6d3
ravi-kumar-pilla <ravi_kumar_pilla@mckinsey.com>
2024-11-19 14:10:58 -0600

    merged main

--- package/kedro_viz/__init__.py
+++ package/kedro_viz/__init__.py
@@ -10,11 +10,7 @@ class KedroVizPythonVersionWarning(UserWarning):
     """Custom class for warnings about incompatibilities with Python versions."""
 
 
-<<<<<<< HEAD
 if sys.version_info >= (3, 14):
-=======
-if sys.version_info >= (3, 13):
->>>>>>> e91d156e6b37bd6f38aa4ce2bfd08095a4134239
     warnings.warn(
         """Please be advised that Kedro Viz is not yet fully
         compatible with the Python version you are currently using.""",
