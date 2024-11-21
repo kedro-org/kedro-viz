5e33b5c10272273484d285cd5dfb091b5120e4de
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2024-11-21 11:42:42 +0000

    Release note and update reminder updated
    Signed-off-by: Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>

--- src/components/update-reminder/update-reminder-content.js
+++ src/components/update-reminder/update-reminder-content.js
@@ -2,23 +2,37 @@ export const updateContent = {
   date: '21 November 2024',
   features: [
     {
-      title: 'Moved to an opt-out model for telemetry',
+      title: 'Support for Python 3.12',
       image: '',
-      copy: 'Telemetry in Kedro-Viz has been updated to support an opt-out model, giving users more control over data sharing preferences.',
-      buttonLink: '',
-    },
-    {
-      title: 'Support for Python 3.12 and 3.13',
-      image: '',
-      copy: 'Kedro-Viz now supports Python 3.12 and 3.13, allowing users to leverage the latest features and improvements in the Python ecosystem.',
+      copy: 'Kedro-Viz now supports Python 3.12, ensuring compatibility with the latest Python version.',
       buttonLink:
         'https://github.com/kedro-org/kedro-viz?tab=readme-ov-file#usage',
       buttonText: 'Learn more',
     },
     {
-      title: 'Enhanced metadata display in Kedro-Viz',
+      title: 'Consistent Flowchart Positioning',
       image: '',
-      copy: 'The metadata panel in Kedro-Viz now displays the full dataset type with library prefixes, improving clarity for users exploring their pipelines.',
+      copy: 'Flowchart positions now remain consistent across renders. Previously, the flowchart layout would change every time users made changes to their Kedro project, especially when using the` --autoreload` function. This issue has been resolved, and the layout will only change for significant node or pipeline updates.',
+      buttonLink: '',
+    },
+    {
+      title: 'Smarter `--autoreload` File Watcher',
+      image: '',
+      copy: 'The `--autoreload` watcher has been optimized to monitor only relevant files, reducing unnecessary reloads. It now excludes files listed in the user’s .gitignore and focuses on key files like `.py` and `.yaml`.',
+      buttonLink:
+        'https://docs.kedro.org/projects/kedro-viz/en/latest/kedro-viz_visualisation.html#automatic-visualisation-updates',
+      buttonText: 'View the docs',
+    },
+    {
+      title: 'Improved Port Management',
+      image: '',
+      copy: 'Kedro-Viz will now open on a new port if the default port 4141 is already in use. This fixes an issue where running multiple instances of Kedro-Viz caused one to overwrite the other. For example, if 4141 is occupied, the next instance will use 4142, and so on.',
+      buttonLink: '',
+    },
+    {
+      title: 'Fixed Tag Undefined Issue',
+      image: '',
+      copy: 'Resolved a bug where the tag was occasionally undefined when pipelines were ordered differently in the pipeline registry. This was a user-reported issue, and it’s now fully addressed.',
       buttonLink: '',
     },
   ],
