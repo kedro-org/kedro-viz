11989df50cac6736b46a34ec55b8aafe2a5e3cc0
Huong Nguyen <huong.nguyen@mckinsey.com>
2024-12-02 21:28:12 +0000

    remove session store
    Signed-off-by: Huong Nguyen <huong.nguyen@mckinsey.com>

--- demo-project/src/demo_project/settings.py
+++ demo-project/src/demo_project/settings.py
@@ -2,25 +2,6 @@
 # List the installed plugins for which to disable auto-registry
 # DISABLE_HOOKS_FOR_PLUGINS = ("kedro-viz",)
 
-from pathlib import Path
-
-# Define where to store data from a KedroSession. Defaults to BaseSessionStore.
-# from kedro.framework.session.store import ShelveStore
-from kedro_viz.integrations.kedro.sqlite_store import SQLiteStore
-
-SESSION_STORE_CLASS = SQLiteStore
-SESSION_STORE_ARGS = {"path": str(Path(__file__).parents[2] / "data")}
-
-# Setup for collaborative experiment tracking.
-# SESSION_STORE_ARGS = {"path": str(Path(__file__).parents[2] / "data"),
-#                       "remote_path": "s3://{path-to-session_store}" }
-
-# Define custom context class. Defaults to `KedroContext`
-# CONTEXT_CLASS = KedroContext
-
-# Define the configuration folder. Defaults to `conf`
-# CONF_ROOT = "conf"
-
 from kedro.config import OmegaConfigLoader  # NOQA
 
 CONFIG_LOADER_CLASS = OmegaConfigLoader
--- package/kedro_viz/server.py
+++ package/kedro_viz/server.py
@@ -4,7 +4,6 @@ for Kedro pipeline visualisation."""
 from pathlib import Path
 from typing import Any, Dict, Optional
 
-from kedro.framework.session.store import BaseSessionStore
 from kedro.io import DataCatalog
 from kedro.pipeline import Pipeline
 
@@ -13,7 +12,6 @@ from kedro_viz.constants import DEFAULT_HOST, DEFAULT_PORT
 from kedro_viz.data_access import DataAccessManager, data_access_manager
 from kedro_viz.database import make_db_session_factory
 from kedro_viz.integrations.kedro import data_loader as kedro_data_loader
-from kedro_viz.integrations.kedro.sqlite_store import SQLiteStore
 from kedro_viz.launchers.utils import _check_viz_up, _wait_for, display_cli_message
 
 DEV_PORT = 4142
@@ -23,17 +21,11 @@ def populate_data(
     data_access_manager: DataAccessManager,
     catalog: DataCatalog,
     pipelines: Dict[str, Pipeline],
-    session_store: BaseSessionStore,
     stats_dict: Dict,
 ):
     """Populate data repositories. Should be called once on application start
     if creating an api app from project.
     """
-
-    if isinstance(session_store, SQLiteStore):
-        session_store.sync()
-        session_class = make_db_session_factory(session_store.location)
-        data_access_manager.set_db_session(session_class)
 
     data_access_manager.add_catalog(catalog, pipelines)
 
@@ -56,7 +48,7 @@ def load_and_populate_data(
     """Loads underlying Kedro project data and populates Kedro Viz Repositories"""
 
     # Loads data from underlying Kedro Project
-    catalog, pipelines, session_store, stats_dict = kedro_data_loader.load_data(
+    catalog, pipelines, stats_dict, _ = kedro_data_loader.load_data(
         path, env, include_hooks, package_name, extra_params, is_lite
     )
 
@@ -67,7 +59,7 @@ def load_and_populate_data(
     )
 
     # Creates data repositories which are used by Kedro Viz Backend APIs
-    populate_data(data_access_manager, catalog, pipelines, session_store, stats_dict)
+    populate_data(data_access_manager, catalog, pipelines, stats_dict)
 
 
 def run_server(
