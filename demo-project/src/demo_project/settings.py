57cba0b126add5a6b8f0ad5a66331143ee782ae8
Huong Nguyen <huong.nguyen@mckinsey.com>
2024-12-03 16:36:02 +0000

    remove database.py entirely
    Signed-off-by: Huong Nguyen <huong.nguyen@mckinsey.com>

--- demo-project/src/demo_project/settings.py
+++ demo-project/src/demo_project/settings.py
@@ -2,6 +2,12 @@
 # List the installed plugins for which to disable auto-registry
 # DISABLE_HOOKS_FOR_PLUGINS = ("kedro-viz",)
 
+# Define custom context class. Defaults to `KedroContext`
+# CONTEXT_CLASS = KedroContext
+
+# Define the configuration folder. Defaults to `conf`
+# CONF_ROOT = "conf"
+
 from kedro.config import OmegaConfigLoader  # NOQA
 
 CONFIG_LOADER_CLASS = OmegaConfigLoader
