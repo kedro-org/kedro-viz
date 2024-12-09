8b0aab0ec7881107d0e92520a0d3ec9767f46723
Huong Nguyen <huong.nguyen@mckinsey.com>
2024-12-09 11:23:45 +0000

    remove experiment-tracking.py, and runs and tracking datatset from repos
    Signed-off-by: Huong Nguyen <huong.nguyen@mckinsey.com>

--- package/kedro_viz/data_access/managers.py
+++ package/kedro_viz/data_access/managers.py
@@ -40,9 +40,7 @@ from .repositories import (
     GraphNodesRepository,
     ModularPipelinesRepository,
     RegisteredPipelinesRepository,
-    RunsRepository,
     TagsRepository,
-    TrackingDatasetsRepository,
 )
 
 logger = logging.getLogger(__name__)
@@ -68,8 +66,7 @@ class DataAccessManager:
         self.node_dependencies: Dict[str, Dict[str, Set]] = defaultdict(
             lambda: defaultdict(set)
         )
-        self.runs = RunsRepository()
-        self.tracking_datasets = TrackingDatasetsRepository()
+        self.runs = {}
         self.dataset_stats = {}
 
     def set_db_session(self, db_session_class: sessionmaker):
@@ -108,9 +105,6 @@ class DataAccessManager:
 
         self.catalog.set_catalog(catalog)
 
-        for dataset_name, dataset in self.catalog.as_dict().items():
-            if self.tracking_datasets.is_tracking_dataset(dataset):
-                self.tracking_datasets.add_tracking_dataset(dataset_name, dataset)
 
     def add_pipelines(self, pipelines: Dict[str, KedroPipeline]):
         """Extract objects from all registered pipelines from a Kedro project
