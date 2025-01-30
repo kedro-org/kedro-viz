a5594c0173136b45fad8be16dd5e204ddd8bc4e2
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2025-01-30 10:56:56 +0000

    First attempt at adding Dev Containers config
    Signed-off-by: Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>

---
+++ .devcontainer/onCreateCommand.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+uv venv $VIRTUAL_ENV
+
+uv pip install -r package/test_requirements.txt -r demo-project/src/docker_requirements.txt
+uv pip install -e package/
