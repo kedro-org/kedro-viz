e7519452757691184f6ce4427b869ee68f86504d
rashidakanchwala <rashida_kanchwala@mckinsey.com>
2025-02-03 11:55:01 +0000

    done
    Signed-off-by: rashidakanchwala <rashida_kanchwala@mckinsey.com>

--- package/kedro_viz/launchers/cli/run.py
+++ package/kedro_viz/launchers/cli/run.py
@@ -139,11 +139,11 @@ def run(  # noqa: PLR0915
             )
             return
 
-        display_cli_message(
-            "WARNING: Experiment Tracking on Kedro-viz will be deprecated in Kedro-Viz 11.0.0. "
-            "Please refer to the Kedro documentation for migration guidance.",
-            "yellow",
-        )
+    display_cli_message(
+        "WARNING: Experiment Tracking on Kedro-viz will be deprecated in Kedro-Viz 11.0.0. "
+        "Please refer to the Kedro documentation for migration guidance.",
+        "yellow",
+    )
 
     installed_version = parse(__version__)
     latest_version = get_latest_version()
