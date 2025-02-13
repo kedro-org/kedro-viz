44e64f63471014fa6f5997bf878c6a439f212304
Juan Luis Cano Rodríguez <juan_luis_cano@mckinsey.com>
2025-02-13 15:57:35 +0100

    Improve disk performance

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -4,6 +4,23 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# Install build tools for node-gyp and pixman
+sudo apt-get update && \
+sudo apt-get install -y --no-install-recommends \
+    build-essential \
+    python3 \
+    make \
+    g++ \
+    libpixman-1-dev \
+    libcairo2-dev \
+    libjpeg-dev \
+    libgif-dev \
+    pkg-config \
+    libcairo2-dev \
+    libpango1.0-dev \
+    libpangocairo-1.0-0 && \
+sudo apt-get clean && sudo rm -rf /var/lib/apt/lists/*
+
 # Install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
@@ -13,4 +30,5 @@ uv pip install -r package/test_requirements.txt -r demo-project/src/docker_requi
 uv pip install -e package/
 
 # Now the NPM dependencies too
+sudo chown vscode:vscode node_modules
 npm install
