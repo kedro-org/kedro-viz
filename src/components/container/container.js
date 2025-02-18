7991af2797791f5952d8b3ac8d7e1de9075488d8
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2025-02-18 12:29:50 +0000

    Revert container.js
    Signed-off-by: Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -7,51 +7,10 @@ import './container.scss';
  * Top-level component for the use-case where Kedro-Viz is run as a standalone
  * app rather than imported as a library/package into a larger application.
  */
-const Container = () => {
-  // const [tags, setTags] = React.useState({
-  //   enabled: { companies: true, shuttles: true },
-  // });
-  // const [nodeTypes, setNodeTypes] = React.useState({
-  //   disabled: {
-  //     parameters: false,
-  //     task: false,
-  //     data: false,
-  //   },
-  // });
-  // const [theme, setTheme] = React.useState('dark');
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setTags({
-  //       enabled: { evaluate: true, companies: false },
-  //     });
-  //     setNodeTypes({
-  //       disabled: {
-  //         parameters: true,
-  //         task: true,
-  //         data: false,
-  //       },
-  //     });
-  //     setTheme('light');
-  //   }, 10000);
-  // }, [theme, tags, nodeTypes]);
-
-  return (
-    <>
-      <App
-        data={getPipelineData()}
-        options={{
-          display: {
-            globalToolbar: false,
-            miniMap: true,
-            expandAllPipelines: true,
-            sidebar: true,
-            filterBtn: true,
-          },
-        }}
-      />
-    </>
-  );
-};
+const Container = () => (
+  <>
+    <App data={getPipelineData()} />
+  </>
+);
 
 export default Container;
