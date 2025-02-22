d55211a4a6ea54d50e4dc1f232a179e51f5aee09
rashidakanchwala <rashida_kanchwala@mckinsey.com>
2025-02-22 23:13:22 +0000

    remove catalog as dict
    Signed-off-by: rashidakanchwala <rashida_kanchwala@mckinsey.com>

--- package/kedro_viz/data_access/repositories/catalog.py
+++ package/kedro_viz/data_access/repositories/catalog.py
@@ -134,10 +134,3 @@ class CatalogRepository:
 
     def get_layer_for_dataset(self, dataset_name: str) -> Optional[str]:
         return self.layers_mapping.get(_strip_transcoding(dataset_name))
-
-    def as_dict(self) -> Dict[str, Optional["AbstractDataset"]]:
-        return {
-            dataset_name: self.get_dataset(dataset_name)
-            for dataset_name in self._catalog.list()
-            if self.get_dataset(dataset_name) is not None
-        }
