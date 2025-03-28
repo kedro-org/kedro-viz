74ae8492172cd25c58b052eb562337437b5b3222
ravi_kumar_pilla <ravi_kumar_pilla@mckinsey.com>
2025-03-28 15:30:19 -0500

    tested and fixed
    Signed-off-by: ravi_kumar_pilla <ravi_kumar_pilla@mckinsey.com>

--- src/utils/index.js
+++ src/utils/index.js
@@ -227,15 +227,12 @@ export const isRunningLocally = () => {
  * @returns {string} Sanitized pathname
  */
 export const sanitizedPathname = () => {
-  // const { pathname } = window.location;
-  // const pathnameWithTrailingSlash = pathname.endsWith('/')
-  //   ? pathname
-  //   : `${pathname}/`; // the `pathname` will have a trailing slash if it didn't initially
+  const { pathname } = window.location;
+  const pathnameWithTrailingSlash = pathname.endsWith('/')
+    ? pathname
+    : `${pathname}/`; // the `pathname` will have a trailing slash if it didn't initially
 
-  // return pathnameWithTrailingSlash;
-
-  // Testing again if this broke things :(
-  return '/';
+  return pathnameWithTrailingSlash;
 };
 
 /**
