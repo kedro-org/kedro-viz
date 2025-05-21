3b59dd918047e223394f1110ad2d4379db33263f
Ravi Kumar <ravi_kumar_pilla@mckinsey.com>
2025-05-21 15:29:24 +0530

    address PR comments
    Signed-off-by: Ravi Kumar <ravi_kumar_pilla@mckinsey.com>

--- package/kedro_viz/integrations/kedro/data_loader.py
+++ package/kedro_viz/integrations/kedro/data_loader.py
@@ -82,23 +82,12 @@ def _load_data_helper(
     Returns:
         A tuple containing the data catalog, pipeline dictionary and dataset stats dictionary.
     """
-
-    try:
-        kedro_session = KedroSession.create(
-            project_path=project_path,
-            env=env,
-            save_on_close=False,
-            runtime_params=extra_params,  # type: ignore[call-arg]
-        )
-    except TypeError:
-        kedro_session = KedroSession.create(
-            project_path=project_path,
-            env=env,
-            save_on_close=False,
-            extra_params=extra_params,
-        )
-
-    with kedro_session as session:
+    with KedroSession.create(
+        project_path,
+        env,
+        False,
+        extra_params,
+    ) as session:
         # check for --include-hooks option
         if not include_hooks:
             session._hook_manager = _VizNullPluginManager()  # type: ignore
