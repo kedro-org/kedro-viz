700224ec47e92c3060a33ee72276ba52ec9b06a0
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2025-06-18 13:52:18 +0100

    Mock data removed

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -2,14 +2,13 @@ import React from 'react';
 import App from '../app';
 import getPipelineData from '../../utils/data-source';
 import './container.scss';
-import runData from './run-data.json';
 /**
  * Top-level component for the use-case where Kedro-Viz is run as a standalone
  * app rather than imported as a library/package into a larger application.
  */
 const Container = () => (
   <>
-    <App data={getPipelineData()} runData={runData} />
+    <App data={getPipelineData()} />
   </>
 );
 
