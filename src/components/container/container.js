497f0dcbe3e0fae5fc514471449429315fddcf6e
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2025-06-18 17:00:48 +0100

    User testing: Run status for slow run use case

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -2,13 +2,14 @@ import React from 'react';
 import App from '../app';
 import getPipelineData from '../../utils/data-source';
 import './container.scss';
+import runData from './run-data.json';
 /**
  * Top-level component for the use-case where Kedro-Viz is run as a standalone
  * app rather than imported as a library/package into a larger application.
  */
 const Container = () => (
   <>
-    <App data={getPipelineData()} />
+    <App data={getPipelineData()} runData={runData} />
   </>
 );
 
