cff38faca2f108b817a9749c766e9f1c743b512e
Huong Nguyen <huong_nguyen@mckinsey.com>
2025-06-25 14:37:48 +0100

    removed files that are not related to this PR

--- src/actions/run-status.js
+++ src/actions/run-status.js
@@ -1,8 +1,3 @@
-import {
-  fetchRunStatus,
-  processRunStatus,
-} from '../utils/normalizeRunStatus.js';
-
 export const UPDATE_RUN_STATUS_DATA = 'UPDATE_RUN_STATUS_DATA';
 
 /**
@@ -13,17 +8,5 @@ export function updateRunStatusData(data) {
   return {
     type: UPDATE_RUN_STATUS_DATA,
     data,
-  };
-}
-
-/**
- * Load run status data
- * @returns {Function} Thunk that loads run status data
- */
-export function loadRunStatusData() {
-  return async function (dispatch) {
-    const runData = await fetchRunStatus();
-    const processedData = processRunStatus(runData);
-    dispatch(updateRunStatusData(processedData));
   };
 }
