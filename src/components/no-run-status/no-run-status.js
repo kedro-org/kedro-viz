7a9bb87d10121207c8b0a12bf2d7ab2c3697eddb
Huong Nguyen <huong_nguyen@mckinsey.com>
2025-06-30 15:12:20 +0100

    simplify the no-run-status component

--- src/components/no-run-status/no-run-status.js
+++ src/components/no-run-status/no-run-status.js
@@ -1,37 +1,18 @@
-import React from 'react';
 import { connect } from 'react-redux';
 
-import ExportModal from '../export-modal';
-import MetaData from '../metadata';
-import MetadataModal from '../metadata-modal';
 import Sidebar from '../sidebar';
-import SadSmiley from '../icons/sad-smiley';
+import { PipelineWarningContent } from '../pipeline-warning/pipeline-warning';
 
-import './no-run-status.scss';
-
-export const NoRunStatus = ({
-  displaySidebar,
-  displayMetadataPanel,
-  displayExportBtn,
-}) => {
+export const NoRunStatus = ({ displaySidebar, sidebarVisible }) => {
   return (
     <div className="kedro-pipeline">
       {displaySidebar && <Sidebar />}
-      {displayMetadataPanel && <MetaData />}
-
-      <div className="pipeline-wrapper">
-        <div className="pipeline-workflow kedro">
-          <div className={'pipeline-no-run-status'}>
-            <SadSmiley />
-            <h2 className="pipeline-no-run-status__title">
-              Kedro run not found
-            </h2>
-            <p>Please view CLI logs for more details.</p>
-          </div>
-        </div>
-      </div>
-      {displayExportBtn && <ExportModal />}
-      <MetadataModal />
+      <PipelineWarningContent
+        isVisible={true}
+        title="Kedro run not found"
+        subtitle="Please view CLI logs for more details."
+        sidebarVisible={sidebarVisible}
+      />
     </div>
   );
 };
@@ -39,8 +20,6 @@ export const NoRunStatus = ({
 export const mapStateToProps = (state) => ({
   displaySidebar: state.display.sidebar,
   sidebarVisible: state.visible.sidebar,
-  displayMetadataPanel: state.display.metadataPanel,
-  displayExportBtn: state.display.exportBtn,
 });
 
 export default connect(mapStateToProps)(NoRunStatus);
