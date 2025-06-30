1bbf42e61d311fcef05b3a98ca1080c73f93b3e9
Huong Nguyen <huong_nguyen@mckinsey.com>
2025-06-30 16:03:19 +0100

    set the visible to be true if no props is passing through parent

--- src/components/nodes-panel/index.js
+++ src/components/nodes-panel/index.js
@@ -7,7 +7,7 @@ import { NodesPanelContextProvider } from './utils/nodes-panel-context';
  * Acts as a wrapper component that provides the AppContext to the NodesPanel component.
  * This ensures that NodesPanel has access to the necessary context values and functions.
  */
-const NodesPanelProvider = ({ faded, visible }) => {
+const NodesPanelProvider = ({ faded, visible = true }) => {
   return (
     <NodesPanelContextProvider>
       <NodesPanel visible={visible} faded={faded} />
