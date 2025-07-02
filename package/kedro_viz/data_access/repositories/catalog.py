f15c32b7388f078c1977707d56e91a65321a364a
rashidakanchwala <rashida_kanchwala@mckinsey.com>
2025-07-02 17:22:30 +0100

    update
    Signed-off-by: rashidakanchwala <rashida_kanchwala@mckinsey.com>

--- package/kedro_viz/data_access/repositories/catalog.py
+++ package/kedro_viz/data_access/repositories/catalog.py
@@ -130,22 +130,22 @@ class CatalogRepository:
         return self._layers_mapping
 
     def get_dataset(self, dataset_name: str) -> Optional["AbstractDataset"]:
-        dataset_obj: Optional["AbstractDataset"]
+        dataset_obj: Optional["AbstractDataset"] = None
         try:
-            if hasattr(self._catalog, "get") and callable(self._catalog.get):
+            if hasattr(self._catalog, "keys") and callable(self._catalog.keys):
                 dataset_obj = self._catalog.get(dataset_name)
             elif KEDRO_VERSION >= parse("0.18.1"):
-                dataset_obj = self._catalog._get_dataset(dataset_name, suggest=False)  # type: ignore[attr-defined]
+                dataset_obj = self._catalog._get_dataset(dataset_name, suggest=False)
             else:  # pragma: no cover
-                dataset_obj = self._catalog._get_dataset(dataset_name)  # type: ignore[attr-defined]
+                dataset_obj = self._catalog._get_dataset(dataset_name)
         except DatasetNotFoundError:
-            dataset_obj = MemoryDataset()
+            pass  # dataset_obj stays None
 
-        # TODO: Remove this for Kedro-Viz 12.0.0 release
         if dataset_obj is None:
             dataset_obj = MemoryDataset()
 
         return dataset_obj
+
 
     def get_layer_for_dataset(self, dataset_name: str) -> Optional[str]:
         return self.layers_mapping.get(_strip_transcoding(dataset_name))
