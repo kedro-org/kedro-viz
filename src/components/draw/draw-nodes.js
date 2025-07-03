455b657bda05e8efa3cb6d149d8de99e3896c0ee
Huong Nguyen <huong_nguyen@mckinsey.com>
2025-07-03 14:52:27 +0100

    remove nodesActice as dependencies to avoid duplicated in rendering

--- src/components/draw/draw-nodes.js
+++ src/components/draw/draw-nodes.js
@@ -257,13 +257,7 @@ export function DrawNodes({
       .text((node) => node.name)
       .style('transition-delay', (node) => (node.showText ? '200ms' : '0ms'))
       .style('opacity', (node) => (node.showText ? 1 : 0));
-  }, [
-    nodes,
-    nodeTypeDisabled.parameters,
-    nodesWithInputParams,
-    orientation,
-    nodeActive,
-  ]);
+  }, [nodes, orientation]);
 
   return <g id="nodes" className="pipeline-flowchart__nodes" ref={groupRef} />;
 }
