ed417494401b1473ec0d73aed036c2df0cadec00
Huong Nguyen <huong_nguyen@mckinsey.com>
2025-07-17 16:24:30 +0100

    update reminder-content for the upcoming release

--- src/components/update-reminder/update-reminder-content.js
+++ src/components/update-reminder/update-reminder-content.js
@@ -1,12 +1,20 @@
 export const updateContent = {
-  date: '8 July 2025',
+  date: '23 July 2025',
   features: [
     {
-      title: 'Last Version Supporting Kedro 0.19',
+      title: 'Workflow run status',
       image: '',
-      copy: 'Kedro-Viz 11.1.0 is compatible with both the Kedro 0.19 series and Kedro 1.0.0. Please note: future versions (starting from 12.0.0) will drop support for Kedro 0.19.',
-      buttonLink: '',
-      buttonText: '',
+      copy: 'With Kedro-Viz 12.0, We are proud to introduce our latest feature Workflow. It gives you a clear, visual summary of your pipeline run—showing whether it succeeded or failed, which nodes were executed, and exactly where issues occurred. With this insight, you can debug faster and optimize your workflows with confidence.',
+      buttonLink:
+        'https://docs.kedro.org/projects/kedro-viz/en/unreleased/workflow-view/',
+      buttonText: 'Read more',
+    },
+    {
+      title: 'Brand new documentation using MKdocs',
+      image: '',
+      copy: 'We’ve revamped our documentation with MkDocs. Enjoy a cleaner, and more intuitive experience with improved structure, better navigation, and easier access to the information you need.',
+      buttonLink: 'https://docs.kedro.org/projects/kedro-viz/',
+      buttonText: 'Check it out',
     },
   ],
 };
