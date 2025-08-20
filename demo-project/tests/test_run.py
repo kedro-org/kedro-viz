a5e8cd953245f14a2877462286ece86baf60e5a2
Ravi Kumar <ravi_kumar_pilla@mckinsey.com>
2025-08-20 11:25:11 -0500

    lint fix
    Signed-off-by: Ravi Kumar <ravi_kumar_pilla@mckinsey.com>

--- demo-project/tests/test_run.py
+++ demo-project/tests/test_run.py
@@ -3,6 +3,7 @@ This module contains example tests for a Kedro project.
 Tests should be placed in ``src/tests``, in modules that mirror your
 project's structure, and in files named test_*.py.
 """
+
 from pathlib import Path
 
 from kedro.framework.session import KedroSession
@@ -11,6 +12,7 @@ from kedro.framework.startup import bootstrap_project
 # The tests below are here for the demonstration purpose
 # and should be replaced with the ones testing the project
 # functionality
+
 
 class TestKedroRun:
     def test_kedro_run(self):
