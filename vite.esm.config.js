bde849862488372d49c5acccaf139bc0fa21de47
Ravi Kumar <ravi_kumar_pilla@mckinsey.com>
2025-09-03 16:41:23 -0500

    testing vite for notebook
    Signed-off-by: Ravi Kumar <ravi_kumar_pilla@mckinsey.com>

---
+++ vite.esm.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  publicDir: false,
+  build: {
+    outDir: 'esmt',
+    emptyOutDir: true,
+    lib: {
+      entry: path.resolve(__dirname, 'src/utils/viz-entry.js'),
+      formats: ['es'],
+      fileName: () => 'kedro-viz.production.mjs',
+    },
+    rollupOptions: {
+      external: [
+        'plotly.js-dist-min',
+      ],
+      output: {
+        format: 'es',
+        entryFileNames: 'kedro-viz.production.mjs',
+        inlineDynamicImports: true,
+      },
+    },
+    cssCodeSplit: false,
+    minify: 'esbuild',
+    sourcemap: false,
+  },
+  define: {
+    'process.env.NODE_ENV': '"production"',
+  },
+});
