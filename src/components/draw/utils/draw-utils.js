ca4e56275a6e0630c2f655d12bb16c2808adf96d
Ravi Kumar <ravi_kumar_pilla@mckinsey.com>
2025-09-22 00:14:20 -0500

    accomodate frontend to backend node_extras changes
    Signed-off-by: Ravi Kumar <ravi_kumar_pilla@mckinsey.com>

--- src/components/draw/utils/draw-utils.js
+++ src/components/draw/utils/draw-utils.js
@@ -15,3 +15,21 @@ export const toSinglePoint = (value) => parseFloat(value).toFixed(1);
  */
 export const limitPrecision = (path) =>
   path.replace(matchFloats, toSinglePoint);
+
+export const processNodeStyles = (nodeStyles, currentTheme) => {
+  const processedStyles = {};
+
+  // Apply root-level styles first (common styles)
+  Object.keys(nodeStyles).forEach((key) => {
+    if (key !== 'themes') {
+      processedStyles[key] = nodeStyles[key];
+    }
+  });
+
+  // Apply theme-specific styles (overwrites common if conflicts)
+  if (nodeStyles.themes && nodeStyles.themes[currentTheme]) {
+    Object.assign(processedStyles, nodeStyles.themes[currentTheme]);
+  }
+
+  return Object.keys(processedStyles).length > 0 ? processedStyles : null;
+};
