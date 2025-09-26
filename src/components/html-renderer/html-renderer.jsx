9408924150ae2a6a18c354234222214be3f05f89
Huong Nguyen <huong_nguyen@mckinsey.com>
2025-09-26 10:00:49 +0100

    use ReactMarkdown and sanitise content before rendering

--- src/components/html-renderer/html-renderer.jsx
+++ src/components/html-renderer/html-renderer.jsx
@@ -1,18 +1,47 @@
 import React from 'react';
-import { renderMarkdownToHTML } from './utils';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import DOMPurify from 'isomorphic-dompurify';
+
 import './html-renderer.scss';
 
 const HTMLRenderer = ({ content, className = '', fontSize }) => {
   const style = fontSize ? { fontSize } : {};
 
+  const sanitizedContent = DOMPurify.sanitize(content || '');
+
   return (
-    <div
-      className={`html-renderer ${className}`}
-      style={style}
-      dangerouslySetInnerHTML={{
-        __html: renderMarkdownToHTML(content),
-      }}
-    />
+    <div className={`html-renderer ${className}`} style={style}>
+      <ReactMarkdown
+        // Enable GitHub Flavored Markdown features (tables, strikethrough, task lists, autolinks, footnotes)
+        remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ children, ...props }) => (
+            <table className="markdown-table" {...props}>
+              {children}
+            </table>
+          ),
+          code: ({ inline, className, children, ...props }) => {
+            if (inline) {
+              return (
+                <code className="inline-code" {...props}>
+                  {children}
+                </code>
+              );
+            }
+            return (
+              <pre className="code-block">
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              </pre>
+            );
+          },
+        }}
+      >
+        {sanitizedContent}
+      </ReactMarkdown>
+    </div>
   );
 };
 
