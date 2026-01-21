d453a44ccb265a187cf0fcf518542834a5476f45
Ravi Kumar <ravi_kumar_pilla@mckinsey.com>
2026-01-20 18:14:58 -0600

    add text renderer
    Signed-off-by: Ravi Kumar <ravi_kumar_pilla@mckinsey.com>

---
+++ src/components/text-renderer/text-renderer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import classnames from 'classnames';
+import { connect } from 'react-redux';
+import MetaDataCode from '../metadata/metadata-code';
+import './text-renderer.scss';
+
+/**
+ * TextRenderer component for displaying plain text or code with syntax highlighting
+ * @param {string} content - The text content to display
+ * @param {Object} meta - Optional metadata (e.g., language for syntax highlighting)
+ * @param {string} theme - Current theme (light/dark)
+ * @param {string} view - View mode ('preview' or 'modal')
+ */
+const TextRenderer = ({ content, meta = {}, theme, view = 'preview' }) => {
+  const { language } = meta;
+  const isCode = Boolean(language);
+
+  return (
+    <div
+      className={classnames(
+        'pipeline-text-renderer',
+        `pipeline-text-renderer--${view}`
+      )}
+    >
+      {isCode ? (
+        <MetaDataCode
+          value={content}
+          visible={true}
+          theme={theme}
+          inline={true}
+        />
+      ) : (
+        <pre className="pipeline-text-renderer__content">{content}</pre>
+      )}
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  theme: state.theme,
+});
+
+export default connect(mapStateToProps)(TextRenderer);
